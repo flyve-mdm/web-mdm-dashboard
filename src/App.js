cfac2534928fa735dc17b06c4273f706bb0d6b1d
Hector Rondon <hrondon@teclib.com>
2017-11-06 10:35:02 -0400

    feat(dashboard): add style sheets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import ContactBook from './ContactBook'
+
 import './App.css'
+import '../node_modules/winjs/css/ui-light.css'
 
 function App() {
   return <ContactBook />
