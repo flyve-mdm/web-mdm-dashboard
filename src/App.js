56c6d1343137b96a04ba4d85a5d43cddb9b7abce
Hector Rondon <hrondon@teclib.com>
2017-11-07 15:44:45 -0400

    docs(app): add documentation to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+/**
+ * Dependencies
+ */
 import * as React from 'react'
 import ContactBook from './ContactBook'
-
+/**
+ * Assets
+ */
 import './App.css'
 import '../node_modules/winjs/css/ui-light.css'
-
+/**
+ * Main component
+ * @returns ContactBook component
+ */
 function App() {
   return <ContactBook />
 }
