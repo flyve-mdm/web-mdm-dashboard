01a636efd8a260b775282f41d2090f295178f51c
Hector Rondon <hrondon@teclib.com>
2017-11-07 11:52:29 -0400

    ci(docs): send code documentation to gh-pages

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -2,6 +2,13 @@
 
 if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
 
-yarn run deploy
+    # Generate CHANGELOG.md and increment version
+    yarn run standard-version -- -t ''
+    # Get version number from package.json
+    export GIT_TAG=$(jq -r ".version" package.json)
+    # Generate documentation with jsdoc
+    yarn run jsdoc src -r -d docs
+    # Copy documentation to gh-pages branch
+    yarn run gh-pages -d docs -e docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
 
 fi
