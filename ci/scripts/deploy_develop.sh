8cf768a204ba990d688609e1a770c1ad536dc473
Hector Rondon <hrondon@teclib.com>
2017-11-08 11:31:23 -0400

    ci(dependencies): install deploy dependencies

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -3,12 +3,12 @@
 if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
 
     # Generate CHANGELOG.md and increment version
-    yarn run standard-version -- -t ''
+    yarn release
     # Get version number from package.json
     export GIT_TAG=$(jq -r ".version" package.json)
     # Generate documentation with jsdoc
-    yarn run jsdoc src -r -d docs
+    jsdoc src -r -d docs 
     # Copy documentation to gh-pages branch
-    yarn run gh-pages -d docs -e docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
+    gh-pages --dist docs --dest docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
 
 fi
--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -2,4 +2,6 @@
 
 # Install jq for json parse
 sudo apt-get install jq
+yarn global add jsdoc
+yarn global add gh-pages
 yarn install
