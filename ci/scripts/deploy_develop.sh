d45bdf8a1b7223c5c4f961a3e98d8d0d233886ef
Hector Rondon <hrondon@teclib.com>
2017-11-09 16:48:14 -0400

    ci(deploy): run deploy with yarn

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -7,8 +7,20 @@ if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version
     # Get version number from package.json
     export GIT_TAG=$(jq -r ".version" package.json)
     # Generate documentation with jsdoc
-    jsdoc src -r -d docs 
+    yarn jsdoc 
+    # Add docs folder
+    git add docs -f
+    # Create commit, NOTICE: this commit is not sent
+    git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
     # Copy documentation to gh-pages branch
-    gh-pages --dist docs --dest docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
+    yarn gh-pages-docs -- -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
+    # Genarate coverage
+    yarn coverage
+    # Add docs folder
+    git add coverage -f
+    # Create commit, NOTICE: this commit is not sent
+    git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
+    # Copy code coverage to gh-pages branch
+    yarn gh-pages-coverage -- -m "ci(docs): generate code coverage for version ${GIT_TAG}"
 
 fi
