e6ab04f0c7fa135f6e8da0a482e6a358d0958458
Hector Rondon <hrondon@teclib.com>
2017-11-10 11:29:09 -0400

    ci(release): upload artifact to release

--- ci/scripts/deploy_master.sh
+++ ci/scripts/deploy_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
+if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* ]]; then
 
     # Generate CHANGELOG.md and increment version
     yarn release -- -m "ci(release): generate CHANGELOG.md for version %s"
@@ -12,5 +12,4 @@ if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version
     git push --follow-tags origin $TRAVIS_BRANCH
     # Create release with conventional-github-releaser
     yarn conventional-github-releaser -- -p angular -t $GITHUB_TOKEN
-
 fi
