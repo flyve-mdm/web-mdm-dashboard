0af3911c6570ad30b28dd818ff76f1716446d63d
Hector Rondon <hrondon@teclib.com>
2017-11-10 12:41:05 -0400

    ci(deploy): remove validation of commits

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
-
-    # Generate CHANGELOG.md and increment version
-    yarn release
-    # Get version number from package.json
-    export GIT_TAG=$(jq -r ".version" package.json)
-    # Generate documentation with jsdoc
-    yarn jsdoc 
-    # Add docs folder
-    git add docs -f
-    # Create commit, NOTICE: this commit is not sent
-    git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
-    # Copy documentation to gh-pages branch
-    yarn gh-pages-docs -- -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
-    # Genarate coverage
-    yarn coverage
-    # Add docs folder
-    git add coverage -f
-    # Create commit, NOTICE: this commit is not sent
-    git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
-    # Copy code coverage to gh-pages branch
-    yarn gh-pages-coverage -- -m "ci(docs): generate code coverage for version ${GIT_TAG}"
-
-fi
+# Generate CHANGELOG.md and increment version
+yarn release
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+# Generate documentation with jsdoc
+yarn jsdoc 
+# Add docs folder
+git add docs -f
+# Create commit, NOTICE: this commit is not sent
+git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
+# Copy documentation to gh-pages branch
+yarn gh-pages-docs -- -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
+# Genarate coverage
+yarn coverage
+# Add docs folder
+git add coverage -f
+# Create commit, NOTICE: this commit is not sent
+git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
+# Copy code coverage to gh-pages branch
+yarn gh-pages-coverage -- -m "ci(docs): generate code coverage for version ${GIT_TAG}"
