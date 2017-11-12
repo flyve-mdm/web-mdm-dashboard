0e72967bfcfae23442613275bf473717ed65eeb9
Hector Rondon <hrondon@teclib.com>
2017-11-12 01:17:23 -0400

    feat(pages): create dynamic components for menu section

--- src/AdminDashboard/BodyAdminDashboard.js
+++ src/AdminDashboard/BodyAdminDashboard.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import ReactWinJS from 'react-winjs'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { closePane, changeLocation, changeListItem } from './DuckController'
+import { closePane, changeLocation, changeIndex, changeListItem } from './DuckController'
 import ListItemPage from './ListItemPage'
 
 function mapStateToProps(state, props) {
@@ -10,6 +10,8 @@ function mapStateToProps(state, props) {
         splitViewId: state.AdminDashboard.splitViewId,
         paneOpened: state.AdminDashboard.paneOpened,
         location: state.AdminDashboard.location,
+        index: state.AdminDashboard.index,
+        router: state.AdminDashboard.router,
         splitViewConfigs: state.AdminDashboard.splitViewConfigs,
         mode: state.AdminDashboard.mode,
         listItem: state.AdminDashboard.listItem
@@ -20,6 +22,7 @@ function mapDispatchToProps(dispatch) {
     const actions = {
         closePane: bindActionCreators(closePane, dispatch),
         changeLocation: bindActionCreators(changeLocation, dispatch),
+        changeIndex: bindActionCreators(changeIndex, dispatch),
         changeListItem: bindActionCreators(changeListItem, dispatch)
     }
     return { actions }
@@ -27,14 +30,15 @@ function mapDispatchToProps(dispatch) {
 
 class BodyAdminDashboard extends React.Component {
 
-    handleCommandInvoked(newLocation) {
+    handleCommandInvoked(newLocation, newIndex) {
         this.props.actions.changeLocation(newLocation)
+        this.props.actions.changeIndex(newIndex)
         this.props.actions.closePane()
     }
 
     render() {
         let contentComponent
-        if (this.props.location[0] === 'Devices' || this.props.location[0] === 'Fleets' || this.props.location[0] === 'Files' || this.props.location[0] === 'Applications' || this.props.location[0] === 'Users') {
+        if (this.props.router[this.props.index].type === 'list') {
             contentComponent = <ListItemPage mode={this.props.mode} location={this.props.location} listItem={this.props.listItem} onNavigate={this.props.actions.changeLocation} changeListItem={this.props.actions.changeListItem} />
         } else {
             contentComponent = <h2 className="win-h2" style={{ marginLeft: '10px' }}> {this.props.location} </h2>
@@ -42,51 +46,15 @@ class BodyAdminDashboard extends React.Component {
 
         let pane = (
             <div>
+                {this.props.router.map((item, index) =>
                 <ReactWinJS.SplitView.Command
-                    label="Dashboard"
-                    icon="home"
-                    onInvoked={() => this.handleCommandInvoked(['Dashboard'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="Devices"
-                    icon="cellphone"
-                    onInvoked={() => this.handleCommandInvoked(['Devices'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="Fleets"
-                    icon="gotostart"
-                    onInvoked={() => this.handleCommandInvoked(['Fleets'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="Files"
-                    icon="copy"
-                    onInvoked={() => this.handleCommandInvoked(['Files'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="Applications"
-                    icon="switchapps"
-                    onInvoked={() => this.handleCommandInvoked(['Applications'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="Users"
-                    icon="people"
-                    onInvoked={() => this.handleCommandInvoked(['Users'])}
-                />
-
-                <ReactWinJS.SplitView.Command
-                    label="Settings"
-                    icon="settings"
-                    style={{ position: 'absolute', bottom: 48, width: '100%' }}
-                    onInvoked={() => this.handleCommandInvoked(['Settings'])}
-                />
-                <ReactWinJS.SplitView.Command
-                    label="About"
-                    icon="contactinfo"
-                    style={{ position: 'absolute', bottom: 0, width: '100%' }}
-                    onInvoked={() => this.handleCommandInvoked(['About'])}
-                />
+                    key={index}
+                    label={item.label}
+                    icon={item.icon}
+                    style={item.style}
+                    onInvoked={() => this.handleCommandInvoked([item.label], index)}
+                />)}
             </div>
-
         )
 
         return (
