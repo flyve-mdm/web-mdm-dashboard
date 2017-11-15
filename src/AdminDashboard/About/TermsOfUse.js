4ffaf0bee21ffd519ff03747239e61312129ccaa
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-15 12:02:34 -0400

    feat(about): create component of 'terms of use'

---
+++ src/AdminDashboard/About/TermsOfUse.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+export default class TermsOfUse extends Component {
+    render() {
+        return (
+            <div>
+                <p>By using our website, you agree to be bound by the full Terms &amp; Conditions posted on our website.</p>
+                <p>These terms and conditions apply to the business relation between the Client and the Editor (as identified further below), arising from the Client requesting the Editor to provide certain corporate and management services. Any order placed in any of our offices, by internet or by any other means, shall constitute a binding agreement of these terms &amp; conditions between the Client and the Editor.</p>
+                <p><strong>DEFINITIONS</strong></p>
+                <p><strong>&#8220;The Editor&#8221;</strong> means Teclib&#8217; Spain, a company, registered in Spain and it also includes any subsidiary or branch of Teclib&#8217; Group, which provides services to the client or the company.</p>
+                <p><strong>&#8220;Services&#8221;</strong> means any or several of the following: GLPi Network, Kimios DMS, Uhuru Mobile, Armadito Antivirus, Flyve MDM, Training sessions, any IT related services and support, Partnership agreements, Registered Member agreements, Marketing agreements, administrative and managerial work and any other corporate services of similar nature that the Editor agrees to provide.</p>
+                <p><strong>&#8220;Company&#8221;</strong> means any company beneficially owned by the client for which services are provided by the Editor.</p>
+                <p><strong>&#8220;Client&#8221;</strong> means the beneficial owner(s) of the company and/or the person(s) who has(ve) requested the Editor to provide services. It also means any person the client may represent in the case of a group (all such persons jointly and separately).</p>
+                <p><strong>&#8220;Contact Form&#8221;</strong> means the standard contact form posted on the Editor&#8217;s website http://www.teclib-edition.com/en/contact-us/ or other format of the same form submitted by the client to the Editor before beginning and providing services.</p>
+                <p><strong>&#8220;Communication&#8221; </strong>means any communication between the Editor and the client by any of the following methods: (a) email or other message or form posted through the internet; (b) fax; (c) courier or mail delivery, any of the above being addressed to the last known or notified address of the recipient.</p>
+                <p><u><strong>I. GENERAL NOTES</strong></u></p>
+                <p><strong>Complaints Procedure</strong><br />
+                The management of the company wishes to remain open to well-founded complaints in order not to only resolve them efficiently, but also to improve our service at large. Complaints are usually handled by the office manager, however if you have a complaint that cannot be resolved, you can write an email to our complaint resolution department.<br />
+                Complaints can be lodged in writing to the office with the envelope marked personal and confidential.<br />
+                Please substantiate any complaints with precise facts, dates and names if you want us to be able to act on your complaint. Any vague complaints or unconstructive insults will be discarded without follow-up. We will attempt to answer or resolve any complaint within a maximum of 3 days.</p>
+                <p><u><strong>II. OUR SERVICES</strong></u></p>
+                <p><strong>GLPi Network</strong><br />
+                Distribution of a complete pack based on GLPi project and other open source technologies, that helps you manage and control your IT infrastructure, with asset management, ticketing and loads of features and plugins.</p>
+                <p><strong>Kimios DMS</strong><br />
+                Java-based, Open Source Knowledge Management Solution also Document Management System. It helps you produce, store, organize, manage and research any kind of document, media and file in your repository</p>
+                <p><strong>Uhuru Mobile</strong><br />
+                Secure operating system for Android-based smartphones, tablets, PCs and applications.<br />
+                Uhuru analyzes, controls and protects against malware and vulnerability exploits.</p>
+                <p><strong>Armadito Antivirus</strong><br />
+                Open source antivirus and anti-malware software for desktop (computers) and servers. It analyses your system, detects viruses, identify malware codes, and last but not least: it protects and strengthens your network.</p>
+                <p><strong>Flyve MDM</strong><br />
+                Mobile Device Management that allows you to: configure, deploy, secure, monitor, integrate and manage any mobile devices within the organization, protecting your corporate network and data.</p>
+                <p><strong>Teclib&#8217; Training Courses</strong><br />
+                Follow a training program, get certified and become an expert of our technologies</p>
+                <p><strong>Become a Partner</strong><br />
+                Our Partnership Program gives access to a portfolio of subscription-based services, letting Partners and customers receive technical support and trainings, to answer the fast evolving business’ needs.</p>
+                <p><strong>Become a Registered Member</strong><br />
+                Becoming a Teclib’ Registered Member is a great way to help improve your profit potential and ensure your business is recognized by the industry and amongst end-users as being professional and technically competent.</p>
+                <p><u><strong>III. GENERAL CONDITIONS</strong></u></p>
+                <p><strong>Your obligations under this agreement</strong><br />
+                We provide you services with the understanding that you have supplied us with true and accurate instructions and that you have given us the authority to lawfully carry out those instructions. You warrant that you are acting on your own authority or have the authority of your client to instruct us in this regards.<br />
+                While identifying officers to the company, you fully warrant that you have the legal consent of those officers, and you agree to indemnify us in the case of a conflict with the directors. You will ensure the accuracy and completeness of the information you provide us.</p>
+                <p><strong>Our obligations under this agreement</strong><br />
+                We will take reasonable steps to insure that our website is as complete and precise as possible and to protect your privacy. We will protect your private data and will never stock any information without strong encryption and following PCI-DSS guidelines.</p>
+                <p><strong>Force Majeure</strong><br />
+                We shall in no case be liable to you for any breach of the terms and conditions or any failure to provide or delay in providing our services through our site resulting from any event or circumstance beyond our reasonable control including, without limitation, breakdown of systems or network access, fire, explosion or accident or any &#8220;acts of God&#8221;.</p>
+                <p><strong>Exclusions and Limitations</strong><br />
+                We make no representations or warranties about the accuracy, completeness, or suitability for any specific purpose of the information and related graphics published on our website.<br />
+                The information published may contain technical inaccuracies, out-dated material or typographical errors and is only intended to be a general indication of our services. Except for claims related to death or personal injury resulting from negligence or as otherwise prescribed by law, our liability for any loss or damage (compensatory, direct, indirect or consequential damages, loss of data, income or profit, loss or damage to property and claims of third parties) arising out of any single claim will be limited to the value of re-supplying the relevant product or service.</p>
+                <p>You agree that this limitation is reasonable due to the nature of our site and given that when you require information or services through our site you start a separate contract with us in each case.</p>
+                <p><strong>Changes in Content and Services</strong><br />
+                At any time and without notice, we reserve the right to revise our terms and conditions, our bouquet of services and the content of our site.</p>
+                <p>Outgoing Web Links<br />
+                Outgoing links to third party websites are provided for your convenience and for general information. We do not warrant any of the content, services or accuracy of these third parties.</p>
+                <p><strong>Ownership and Rights to the materials on this website</strong><br />
+                All rights to the design, text &amp; graphics and any other material on our website, layout and style are our own copyright or the ones of related third parties. Permission is granted to electronically copy and print hard copy portions of our site solely for the purpose of studying offers in connection with the acquisition of goods or services from our website. Any other use of the materials on our website (including reproduction for purposes other than those above mentioned and any alteration, modification, distribution, or publication) without our prior written permission is strictly prohibited and will be prosecuted to the full extend authorized by law.</p>
+                <p><strong>Third Party Rights</strong><br />
+                Our terms and conditions are not intended to be enforceable by any third party.</p>
+                <p><strong>Competent Jurisdiction and Tribunals</strong><br />
+                These Terms and Conditions and our relationship with you shall be governed by and construed in accordance with the laws of Spain and will be subject to the exclusive jurisdiction of the Spanish Courts.</p>
+                <p><strong>Privacy Policy</strong><br />
+                You agree that we may collect, store, and use information about you in accordance with our Privacy Policy. You acknowledge and agree to be bound by the terms of our Privacy Policy.</p>
+            </div>
+        )
+    }
+}
