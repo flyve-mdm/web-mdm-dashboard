5cb33892f686572e6604185ff1510476491ef64b
Hector Rondon <hrondon@teclib.com>
2017-11-17 16:14:53 -0400

    fix(lists): fix bug when delete an item

--- src/AdminDashboard/Fleets/FleetsList.js
+++ src/AdminDashboard/Fleets/FleetsList.js
@@ -34,7 +34,7 @@ export default class FleetsList extends Component {
 
     handleToggleSelectionMode = () => {
         this.setState({ selectionMode: !this.state.selectionMode })
-        this.props.onNavigate(this.props.location)
+        this.props.onNavigate([this.props.location[0], null])
         this.refs.listView.winControl.selection.clear()
     }
 
