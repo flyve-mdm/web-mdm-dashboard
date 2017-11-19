05e564f328435556fa75d041cee271713cee9d34
Hector Rondon <hrondon@teclib.com>
2017-11-19 19:00:27 -0400

    feat(fleets): add main content to create fleet

--- src/AdminDashboard/Fleets/FleetsAdd.js
+++ src/AdminDashboard/Fleets/FleetsAdd.js
@@ -6,13 +6,21 @@ export default class FleetsAdd extends Component {
         return (
             <ReactWinJS.Pivot>
                 <ReactWinJS.Pivot.Item key="main" header="Main">
-                    <div>Main Screen</div>
+                <div style={{ width: '320px', padding: '10px'}}>
+                    <input type="text" placeholder="Fleet Name"/>
+                </div>
                 </ReactWinJS.Pivot.Item>
                 <ReactWinJS.Pivot.Item key="policies" header="Policies">
                     <div>Policies </div>
                 </ReactWinJS.Pivot.Item>
                 <ReactWinJS.Pivot.Item key="applications" header="Applications">
-                    <div>Applications</div>
+                    <div>
+                        <p>Choose here the applications which you want automatically installed on the devices with this fleet</p>
+                        <h2>Current Applications for this Fleet</h2>
+                        <p>No application choosen to be installed for the devices that are/will-be in this Fleet</p>
+                        <h2>Add Applications for this fleet</h2>
+                        <p>No available applications to the fleet.</p>
+                    </div>
                 </ReactWinJS.Pivot.Item>
                 <ReactWinJS.Pivot.Item key="files" header="Files">
                     <div>Files</div>
