416ede83dcbb8f61cc6fbee7d74f349939dd000a
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-23 10:29:32 -0400

    refactor(fleets): create the component of PoliciesAdd

---
+++ src/AdminDashboard/Fleets/PoliciesAdd.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactWinJS from 'react-winjs'
+
+export default class FleetsAddPolicies extends Component {
+    render() {
+        return (
+            <div>Policies</div>
+        )
+    }
+}
