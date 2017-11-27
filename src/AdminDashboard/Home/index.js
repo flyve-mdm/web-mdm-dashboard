ce369b6f204db18d3161dc2a53a1cdff159a8258
Hector Rondon <hrondon@teclib.com>
2017-11-27 01:14:01 -0400

    feat(dashboard): show count items on home screen

---
+++ src/AdminDashboard/Home/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import DashboardPage from './DashboardPage'
+import { Devices, Fleets, Files, Applications, Users } from '../Data'
+
+export default class Dashboard extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            pages: {
+                devices: Devices.length,
+                fleets: Fleets.length,
+                files: Files.length,
+                applications: Applications.length,
+                users: Users.length
+            }
+        }
+    }
+    render() {
+        
+        return (
+            <div>
+                <h2 className="win-h2" style={{ marginLeft: '10px' }}> {this.props.location} </h2>
+                <DashboardPage count={this.state.pages.devices} name="Devices"/>
+                <DashboardPage count={this.state.pages.fleets} name="Fleets"/>
+                <DashboardPage count={this.state.pages.files} name="Files"/>
+                <DashboardPage count={this.state.pages.applications} name="Applications"/>
+                <DashboardPage count={this.state.pages.users} name="Users"/>
+            </div>
+            
+        )
+    }
+}
