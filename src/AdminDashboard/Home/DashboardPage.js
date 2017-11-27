9e89034180f07ebe2f36f46881f33d847b719b57
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-27 10:10:43 -0400

    refactor(home): improve screen design

--- src/AdminDashboard/Home/DashboardPage.js
+++ src/AdminDashboard/Home/DashboardPage.js
@@ -3,15 +3,14 @@ import React, { Component } from 'react'
 export default class DashboardPage extends Component {
     
     render() {
-        console.log(this.props)
         return (
-            <div style={{ padding: '20px', display: 'inline-block', width: '100%', height: '100px' }}>
+            <div className="info-box">
                 <div>
                     <h1>
                         {this.props.count}
                     </h1>
                 </div>
-                <div>
+                <div className="title">
                     <h3>
                         {this.props.name}
                     </h3>
