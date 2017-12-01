c1b39f84f95218a61d0bad80ea996e5ea27faab4
Hector Rondon <hrondon@teclib.com>
2017-12-01 17:28:26 -0400

    refactor(routers): use react-router-redux to handler routers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-import Reducers from './reducers'
+import { store, history } from './store'
+import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
 import registerServiceWorker from './registerServiceWorker'
+import Login from './Login'
+import AdminDashboard from './AdminDashboard'
 
-const store = createStore(Reducers)
+/**
+ * Assets
+ */
+import './styles/App.scss'
+import './styles/ui-light.css'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path='/app' component={AdminDashboard} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>), 
+    document.getElementById('app')
 )
 registerServiceWorker()
--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import AdminDashboard from './AdminDashboard/DuckController'
 import Login from './Login/DuckController'
 
 export default combineReducers ({
     AdminDashboard,
-    Login
+    Login,
+    router: routerReducer
 })
