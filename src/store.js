b78cddf7eab89ba350b8882d36f3cbc9f53019b4
Hector Rondon <hrondon@teclib.com>
2017-12-01 17:28:26 -0400

    refactor(routers): use react-router-redux to handler routers

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+import Reducers from './reducers'
+
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+
+export const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const routerReduxMiddleware = routerMiddleware(history)
+
+const getMiddleware = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return applyMiddleware(routerReduxMiddleware)
+    } else {
+        // Enable additional logging in non-production environments.
+        return applyMiddleware(routerReduxMiddleware)
+    }
+};
+
+export const store = createStore(
+    Reducers, 
+    composeWithDevTools(getMiddleware())
+)
