00ddaf72e853d0379ece45cfaf325b2204dc33d7
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-04 18:28:26 -0400

    feat(utils): create a function to validate the data displayed

---
+++ src/Utils/validateData.js
@@ -0,0 +1,10 @@
+export default (data, specialReturn) => {
+
+    let value
+
+    if (!data && specialReturn) value = specialReturn
+    else if (!data && data !== 0) value = "not available"
+    else value = data
+
+    return value
+}
