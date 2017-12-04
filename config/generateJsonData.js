4e309dcf70dcd6eb0c79612daf311f31c89ecded
Hector Rondon <hrondon@teclib.com>
2017-12-04 14:55:11 -0400

    build(data): create server to handler api rest

---
+++ config/generateJsonData.js
@@ -0,0 +1,25 @@
+
+module.exports = function() {
+    var Agents = require('../src/AdminDashboard/data/agents.json')
+    var Files = require('../src/AdminDashboard/data/files.json')
+    var Fleets = require('../src/AdminDashboard/data/fleets.json')
+    var HelpCenter = require('../src/AdminDashboard/data/helpCenter.json')
+    var Invitations = require('../src/AdminDashboard/data/invitations.json')
+    var InvitationsLog = require('../src/AdminDashboard/data/invitationsLog.json')
+    var Packages = require('../src/AdminDashboard/data/invitations.json')
+    var Plugins = require('../src/AdminDashboard/data/plugins.json')
+    var Policies = require('../src/AdminDashboard/data/policies.json')
+    var Users = require('../src/AdminDashboard/data/users.json')
+    return {
+        devices: Agents,
+        files: Files,
+        fleets: Fleets,
+        helpCenter: HelpCenter,
+        invitations: Invitations,
+        invitationsLog: InvitationsLog,
+        packages: Packages,
+        plugins: Plugins,
+        policies: Policies,
+        users: Users
+    }
+}
