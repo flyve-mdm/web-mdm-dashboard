e233357e30cc2e2bb62a4f32a0efdc6267fc9a5e
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-05 18:31:19 -0400

    refactor(EmptyList): simplify and consider the header size

--- src/Utils/EmptyList.js
+++ src/Utils/EmptyList.js
@@ -3,25 +3,24 @@ import PropTypes from 'prop-types'
 
 class EmptyList extends Component {
     render () {
+        const headerSize = this.props.headerSize ? this.props.headerSize : 0        
         const icon = this.props.icon ? this.props.icon : "fileIcon"
 
         return (
-            <div className="listPane" style={{ height: '100%', width: this.props.itemListPaneWidth, display: 'inline-block', verticalAlign: 'top'  }}>
-                <div style={{ display: 'flex', height: '100%', justifyContent: 'center', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                    <h4 className="win-h4" style={{ color: 'grey' }}>
-                        <span className={icon} style={{fontSize: "40px", lineHeight: "60px"}}/> <br/>
-                        { this.props.message } 
-                    </h4>
-                </div>
+            <div style={{ marginTop: - headerSize, display: 'flex', height: '100%', justifyContent: 'center', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <h4 className="win-h4" style={{ color: 'grey' }}>
+                    <span className={icon} style={{fontSize: "40px", lineHeight: "60px"}}/> <br/>
+                    { this.props.message } 
+                </h4>
             </div>
         )
     }
 }
 
 EmptyList.propTypes = {
-    itemListPaneWidth: PropTypes.number.isRequired,
     message: PropTypes.string.isRequired,
-    icon: PropTypes.string
+    icon: PropTypes.string,
+    headerSize: PropTypes.number    
 }
 
 export default EmptyList
