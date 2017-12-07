18ef74bb8944e5b6ab40a22af813190ad727b00e
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-07 18:40:21 -0400

    feat(Profiles): create a component for the different inputs

---
+++ src/AdminDashboard/Settings/Profiles/Input.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Input extends Component {
+    render() {
+        return (
+            <div className="list-col">
+                <p>{this.props.label}</p>
+                <input 
+                    type={this.props.type} 
+                    className="win-textbox" 
+                    name={this.props.name}
+                    value={this.props.value1}
+                    placeholder={this.props.placeholder}
+                    onChange={this.props.function}
+                    disabled={this.props.disabled}
+                    style={this.props.style}
+                />
+            </div>            
+        )
+    }
+}
+
+Input.propTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value1: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    function: PropTypes.func,
+    disabled: PropTypes.bool,
+    style: PropTypes.object
+}
+
+export default Input
+    
