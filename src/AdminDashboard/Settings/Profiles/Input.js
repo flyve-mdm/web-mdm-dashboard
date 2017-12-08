247e82e947cc18ec525dd005df3e1d86c707c320
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-08 18:25:22 -0400

    feat(Input): create button to delete

--- src/AdminDashboard/Settings/Profiles/Input.js
+++ src/AdminDashboard/Settings/Profiles/Input.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types'
 
 class Input extends Component {
     render() {
+        let clearIcon 
+        if (this.props.delete) clearIcon = <span className="clearIcon" style={{ margin: 10 }} onClick={() => this.props.delete(this.props.name)}/>
         return (
             <div className="list-col">
                 <p>{this.props.label}</p>
+                { clearIcon }
                 <input 
                     type={this.props.type} 
                     className="win-textbox" 
@@ -24,12 +27,16 @@ class Input extends Component {
 Input.propTypes = {
     label: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
+    name: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]).isRequired,
     value: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
     function: PropTypes.func,
     disabled: PropTypes.bool,
-    style: PropTypes.object
+    style: PropTypes.object,
+    delete: PropTypes.func
 }
 
 export default Input
