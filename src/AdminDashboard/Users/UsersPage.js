89cdb2b14c18ab1dec2427a39c7bf0745fc1bc41
Hector Rondon <hrondon@teclib.com>
2017-12-11 12:58:02 -0400

    feat(users): allow edit multiple users

--- src/AdminDashboard/Users/UsersPage.js
+++ src/AdminDashboard/Users/UsersPage.js
@@ -2,38 +2,56 @@ import React, { Component } from "react"
 import PropTypes from 'prop-types'
 import UsersContent from './UsersContent'
 import UsersEdit from './UsersEdit'
+import UserEditOne from './UsersEditOne'
 import EmptyMessage from '../../Utils/EmptyMessage'
 
 export default class UsersPage extends Component {
     
     render() {
-        if (this.props.selectedIndex === null) {
-            return (
-                <EmptyMessage message="No Selection" itemListPaneWidth={this.props.itemListPaneWidth} />
-            )
+        if (this.props.selectedIndex === null || this.props.actionList === 'Edit') {
+            switch (this.props.actionList) {
+                case "Edit":
+                    return (
+                        <UsersEdit
+                            itemListPaneWidth={this.props.itemListPaneWidth}
+                            dataSource={this.props.dataSource}
+                            changeDataSource={this.props.changeDataSource}
+                            location={this.props.location}
+                            onNavigate={this.props.onNavigate}
+                            changeSelectionMode={this.props.changeSelectionMode}
+                            changeActionList={this.props.changeActionList} />
+                    )
+                default:
+                    return (
+                        <EmptyMessage
+                            message="No Selection"
+                            itemListPaneWidth={this.props.itemListPaneWidth} />
+                    )
+            }
         } else {
-            if (this.props.actionList !== null) {
-                switch (this.props.actionList) {
-                    
-                    case "Edit":
-                        return (
-                            <UsersEdit
-                                itemListPaneWidth={this.props.itemListPaneWidth}
-                                dataSource={this.props.dataSource}
-                                selectedIndex={this.props.selectedIndex}
-                                changeActionList={this.props.changeActionList} />
-                        )
-                    default: 
-                        return (
-                            <EmptyMessage message="No Selection" itemListPaneWidth={this.props.itemListPaneWidth} />
-                        )
+            if (this.props.actionList === null) {
+                const selectedItemList = this.props.dataSource.itemList.getAt(this.props.selectedIndex)
+                if (selectedItemList !== undefined) {
+                    return (
+                        <UsersContent
+                            itemListPaneWidth={this.props.itemListPaneWidth}
+                            dataSource={this.props.dataSource}
+                            location={this.props.location}
+                            selectedIndex={this.props.selectedIndex}
+                            selectedItemList={selectedItemList}
+                            changeActionList={this.props.changeActionList} />
+                    )
+                } else {
+                    return (
+                        <EmptyMessage message="No Selection" itemListPaneWidth={this.props.itemListPaneWidth} />
+                    )
                 }
             } else {
-                
                 return (
-                    <UsersContent 
+                    <UserEditOne
                         itemListPaneWidth={this.props.itemListPaneWidth}
                         dataSource={this.props.dataSource}
+                        location={this.props.location}
                         selectedIndex={this.props.selectedIndex}
                         changeActionList={this.props.changeActionList} />
                 )
