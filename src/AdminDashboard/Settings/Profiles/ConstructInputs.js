fe2be966030fe9aa5ceac710410f22cde098e81f
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-12 18:39:24 -0400

    feat(profiles): create a component to textarea

--- src/AdminDashboard/Settings/Profiles/ConstructInputs.js
+++ src/AdminDashboard/Settings/Profiles/ConstructInputs.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Input from './Input'
 import Select from './Select'
 import DatePicker from './DatePicker'
+import TextArea from './TextArea'
 
 class ConstructInputs extends Component {
 
@@ -29,6 +30,20 @@ class ConstructInputs extends Component {
                                 name={element.name}
                                 value={element.value}
                                 function={element.function}
+                                key={element.name}/>
+                            )
+                        } else if (element.type === 'textArea') {
+                            renderElement = (
+                                <TextArea
+                                label={element.label}
+                                type={element.type}
+                                name={element.name}
+                                value={element.value}
+                                placeholder={element.placeholder}
+                                function={element.function}
+                                disabled={element.disabled}
+                                style={element.style}
+                                delete={element.delete}
                                 key={element.name}/>
                             )
                         } else {
