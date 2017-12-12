c43f9095418bc0f8246c45410e09c9363c1e0e1c
Hector Rondon <hrondon@teclib.com>
2017-12-12 14:09:28 -0400

    feat(fleets): create multiple fleets simultaneously

--- src/AdminDashboard/Fleets/FleetsNew.js
+++ src/AdminDashboard/Fleets/FleetsNew.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ContentPane from '../../Utils/ContentPane'
+import FleetsEditItemList from './FleetsEditItemList'
+import ItemList from '../ItemList'
 
 export default class FleetsNew extends Component {
     constructor(props) {
         super(props)
+
         this.state = {
-            name: ''
+            itemListArray: [this.item()]
         }
     }
 
+    item = () => {
+        return {
+            "PluginFlyvemdmFleet.name": '',
+            "PluginFlyvemdmFleet.id": 0,
+            "PluginFlyvemdmFleet.PluginFlyvemdmTask.items_id": null,
+            "PluginFlyvemdmFleet.PluginFlyvemdmTask.itemtype": null,
+            "PluginFlyvemdmFleet.is_default": 1
+        }
+    }
+
+    addFleet = () => {
+        this.setState({
+            itemListArray: [...this.state.itemListArray, this.item()]
+        })
+    }
+
+    updateItemList = (index, name) => {
+        var newItem = this.state.itemListArray
+  
+        var objIndex = newItem.findIndex((obj => obj["PluginFlyvemdmFleet.id"] === index));
+
+        newItem[objIndex]["PluginFlyvemdmFleet.name"] = name
+
+        this.setState({
+            itemListArray: newItem
+        })
+    }
+
     newFleet = () => {
+        if (this.state.itemListArray.length > 0) {
 
-        if(this.state.name !== '' && this.state.name !== undefined) {
+            let array = this.props.dataSource.itemList.map((value, index) => {
+                return value
+            })
+             
+            array = [
+                ...array,
+                ...this.state.itemListArray
+            ]
 
-            let item = this.props.dataSource.itemList
-            
-            let newItem = {
-                "PluginFlyvemdmFleet.name": this.state.name,
-                "PluginFlyvemdmFleet.id": 100,
-                "PluginFlyvemdmFleet.PluginFlyvemdmTask.items_id": null,
-                "PluginFlyvemdmFleet.PluginFlyvemdmTask.itemtype": null,
-                "PluginFlyvemdmFleet.is_default": 1
-            }
-    
-            item.push(newItem)
-            this.props.changeDataSource(this.props.location, { itemList: item, sort: true })
-            this.props.changeCurrentItem(newItem)
+            this.props.changeDataSource(this.props.location, { itemList: ItemList(this.props.location[0], array, this.props.dataSource.sort), sort: this.props.dataSource.sort })
+            this.props.changeCurrentItem(this.state.itemListArray[0])
             this.props.changeActionList('Add Tasks')
         }
     }
@@ -36,29 +64,34 @@ export default class FleetsNew extends Component {
     }
 
     render() {
+
+        let renderComponent = []
+
+        for (let index = 0; index < this.state.itemListArray.length; index++) {
+            renderComponent = [
+                ...renderComponent,
+                <FleetsEditItemList
+                    key={index}
+                    updateItemList={this.updateItemList}
+                    itemListPaneWidth={this.props.itemListPaneWidth}
+                    location={this.props.location}
+                    currentItem={this.state.itemListArray[index]}
+                    changeActionList={this.props.changeActionList} />
+            ]
+            
+        }
+
         return (
             <ContentPane itemListPaneWidth={this.props.itemListPaneWidth}>
-                <h2 className="win-h2 titleContentPane" onClick={() =>this.changeSelectItem([])}>
-                    New Fleet
-                </h2>
-                <input 
-                    type="text" 
-                    className="win-textbox" 
-                    placeholder="Fleet Name"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.changeInput}
-                    required
-                />
-                <br/>
-                <button className="win-button" onClick={() => this.props.changeActionList(null)}>Cancel</button>
-                <button 
-                    className="win-button win-button-primary" 
-                    style={{marginLeft: 10}}
-                    onClick={this.newFleet}
-                >
-                    Save
-                </button>
+                <div className="contentHeader">
+                    <h2 className="win-h2 titleContentPane" > New {this.props.location[0]} </h2>
+                    <button className="win-button win-button-primary" onClick={this.newFleet}>Save</button>
+                </div>
+                <div className="separator" />
+                <div style={{ float: 'right', textAlign: 'center', display: 'inline-block', marginTop: '20px' }}>
+                    <span className="addIcon" style={{ fontSize: '18px' }} onClick={this.addFleet}></span>
+                </div>
+                {renderComponent}
             </ContentPane>
         )
     }
