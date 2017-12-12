07815465034da152aec82137fe6d1317cf7266c4
Hector Rondon <hrondon@teclib.com>
2017-12-12 15:21:31 -0400

    feat(users): delete a single item

--- src/AdminDashboard/Users/UsersContent.js
+++ src/AdminDashboard/Users/UsersContent.js
@@ -4,7 +4,22 @@ import ContentPane from '../../Utils/ContentPane'
 import IconItemList from '../IconItemList'
 
 export default class UsersContent extends Component {
-  render() {
+
+    handleDelete = () => {
+
+        let item = this.props.dataSource.itemList
+        let index = this.props.selectedIndex
+        index.sort()
+        index.reverse()
+        index.forEach((i) => {
+            item.splice(i, 1)
+        })
+
+        this.props.changeDataSource(this.props.location, { itemList: item, sort: this.props.dataSource.sort })
+        this.props.onNavigate([this.props.location[0]])
+    }
+
+    render() {
         let selectedItemList = this.props.dataSource.itemList.getAt(this.props.selectedIndex)
         let imageProfile = selectedItemList['User.picture'] ? selectedItemList['User.picture'] : "profile.png"
         
@@ -25,7 +40,8 @@ export default class UsersContent extends Component {
                             </span>
                             <span className="source">Joined {selectedItemList['User.date_creation']}</span>
                             <br />
-                            <span className="editIcon" onClick={() => this.props.changeActionList('EditOne')} />
+                            <span className="editIcon" style={{ marginRight: '20px' }} onClick={() => this.props.changeActionList('EditOne')} />
+                            <span className="deleteIcon" onClick={this.handleDelete} />
                         </div>
                     </div>
                 </div>
@@ -65,6 +81,10 @@ UsersContent.propTypes = {
         PropTypes.number
     ]).isRequired,
     dataSource: PropTypes.object.isRequired,
+    changeDataSource: PropTypes.func.isRequired,
     selectedIndex: PropTypes.array,
+    location: PropTypes.array.isRequired,
+    onNavigate: PropTypes.func.isRequired,
+    selectedItemList: PropTypes.object.isRequired,
     changeActionList: PropTypes.func.isRequired
 }
