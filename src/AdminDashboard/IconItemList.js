cda63b96bce5f5c00e838e3212165a75de4f904d
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-12 13:55:27 -0400

    refactor(IconItemList): simplify component and add local images

--- src/AdminDashboard/IconItemList.js
+++ src/AdminDashboard/IconItemList.js
@@ -3,45 +3,23 @@ import PropTypes from 'prop-types'
 
 export default class IconItemList extends React.Component {
     render() {
-        let size = this.props.size
-        let backgroundColor = this.props.backgroundColor !== undefined ? this.props.backgroundColor : '#e6e6e6'
-        var image = ""
-        let style = {}
-        if (this.props.type === "base64") {
-            image = this.props.image
-        } else {
-            if (this.props.image !== undefined) {
-                image = "images/" + this.props.image
-            }
+        const size = this.props.size ? this.props.size : 100
+        const backgroundColor = this.props.backgroundColor ? this.props.backgroundColor : '#e6e6e6'
+        let image = this.props.image ? this.props.image : ''
+        if (this.props.type !== "base64" && this.props.type !== "localFile" && this.props.image) {
+            image = "images/" + this.props.image
         }
-        if (this.props.image !== undefined) {
-
-            style = {
-                lineHeight: size + 'px',
-                backgroundColor: backgroundColor,
-                width: size,
-                height: size,
-                WebkitBorderRadius: size,
-                MozBorderRadius: size,
-                borderRadius: size,
-                backgroundSize: 'cover',
-                display: 'inline-block'
-            }
-
-        } else {
-            style = {
-                lineHeight: size + 'px',
-                backgroundColor: backgroundColor,
-                width: size,
-                height: size,
-                WebkitBorderRadius: size,
-                MozBorderRadius: size,
-                borderRadius: size,
-                backgroundSize: 'cover',
-                display: 'inline-block'
-            }
+        const style = {
+            lineHeight: size + 'px',
+            backgroundColor: backgroundColor,
+            width: size,
+            height: size,
+            WebkitBorderRadius: size,
+            MozBorderRadius: size,
+            borderRadius: size,
+            backgroundSize: 'cover',
+            display: 'inline-block'
         }
-
 
         return (
             <div className="contentPicture" style={style}>
@@ -54,5 +32,5 @@ IconItemList.propTypes = {
     size: PropTypes.number,
     backgroundColor: PropTypes.string,
     image: PropTypes.string,
-    type: PropTypes.oneOf(["file", "base64"])
+    type: PropTypes.oneOf(["file", "base64", "localFile"])
 }
