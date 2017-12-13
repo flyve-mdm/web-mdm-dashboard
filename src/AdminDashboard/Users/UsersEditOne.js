2b0b65b310c582a172ed0662c2d7b618bf8e2dda
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-13 11:03:02 -0400

    refactor(user): change the form of edit one

--- src/AdminDashboard/Users/UsersEditOne.js
+++ src/AdminDashboard/Users/UsersEditOne.js
@@ -1,120 +1,183 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ContentPane from '../../Utils/ContentPane'
+import Calc100PercentMinus from '../../Utils/Calc100PercentMinus'
+import ConstructInputs from '../../Utils/Forms'
+import validateData from '../../Utils/validateData'
+import IconItemList from '../IconItemList'
+import { usersScheme } from '../../Utils/Forms/Schemes'
 
-export default class UsersEditOne extends Component {
-
+class Profiles extends Component {
+    
     constructor(props) {
         super(props)
+        const currentUser = this.props.dataSource.itemList.getAt(this.props.selectedIndex)
+        
         this.state = {
-            login: '',
-            surname: '',
-            firstName: '',
+            buttonSaveClassName: "win-button win-button-primary hidden",
+            login: validateData(currentUser["User.name"], undefined),
+            firstName: validateData(currentUser["User.firstname"]),
+            realName: validateData(currentUser["User.realname"]),
+            phone: validateData(currentUser["User.phone"]),
+            mobilePhone: validateData(currentUser["User.mobile"]),
+            phone2: validateData(currentUser["User.phone2"]),
+            administrativeNumber: validateData(currentUser["User.registration_number"]),
+            lastLogin: validateData(currentUser["User.last_login"], undefined),
+            created: validateData(currentUser["User.date_creation"], undefined),
+            modified: validateData(currentUser["User.date_mod"], undefined),
+            emails: validateData(currentUser["User.UserEmail.email"], []),
+            imageProfile: validateData(currentUser['User.picture'], "profile.png"),
+            authentication: 'GLPI internal database',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            category: '',
+            defaultEntity: '',
+            comments: '',
+            typeImageProfile: 'file',
+            title: '',
+            location: '',
+            defaultProfile: '',
+            validSince: new Date(),
+            validUntil: new Date()
         }
     }
 
-    componentDidMount() {
-        const selectedItemList = this.props.dataSource.itemList.getAt(this.props.selectedIndex)
-
+    saveChanges = () => {
         this.setState({
-            login: selectedItemList["User.name"],
-            surname: '',
-            firstName: '',
+            buttonSaveClassName: "win-button win-button-primary hidden"
         })
-    }
-
-    sendChanges = () => {
         this.props.changeActionList(null)
     }
 
-    changeInput = (e) => {
-        this.setState({[e.target.name]: e.target.value})
+    changeState = (name, value) => {
+        this.setState({
+            [name]: value
+        })
+        if (this.state.buttonSaveClassName === "win-button win-button-primary hidden") {
+            this.setState({
+                buttonSaveClassName: "win-button win-button-primary"
+            })
+        }
     }
 
-    render() {
+    changeEmail = (name, value) => {
+        let emails = [...this.state.emails]
+        emails[name] = value
+        this.changeState('emails', emails)
+    }
+
+    deleteEmail = (index) => {
+        this.setState({
+            emails: this.state.emails.slice(0,index).concat(this.state.emails.slice(index+1))
+        })
+    }
+
+    addEmail = () => {
+        this.setState({
+            emails: [
+                ...this.state.emails,
+                ''
+            ]
+        })
+    }
+
+    previewFile = (evt) => {
+
+        const file = evt.target.files[0]
+        if (file.type.match('image.*')) {
+            let reader = new FileReader()
+
+            reader.onload = ((theFile) => {
+                return (e) => {
+                this.setState({
+                    imageProfile: e.target.result,
+                    typeImageProfile: 'localFile'
+                })
+            }})(file)
+
+            reader.readAsDataURL(file)
+        }
+   }
+
+    openFileChooser = () => {
+        this.inputElement.value = null
+        this.inputElement.click()
+    }
+
+    render () {
+
+        const user = usersScheme({
+            state: this.state, 
+            changeState: this.changeState,
+            deleteEmail: this.deleteEmail
+        })
+
+        const inputAttributes = {
+            type: 'file',
+            accept: "image/*",
+            name: "imageProfile",
+            style: { display: 'none' },
+            ref: (element) => {
+                this.inputElement = element
+            },
+            onChange: this.previewFile
+        }
+
         return (
-            <ContentPane itemListPaneWidth={this.props.itemListPaneWidth}>
-                <h2 className="win-h2 titleContentPane" onClick={() =>this.changeSelectItem([])}>
-                    Edit User
-                </h2>
-                <p>Please insert an active email address.</p>
-                <p>An email will be sent with a QR code.</p>
-                <div>
-                    <b>Login </b> <br/>
-                    <input 
-                        type="text" 
-                        className="win-textbox" 
-                        placeholder="Login"
-                        name="login"
-                        value={this.state.login}
-                        onChange={this.changeInput}
-                        required
-                    />
-                </div>
-                <div>
-                    <b>Surname </b> <br/>
-                    <input 
-                        type="text" 
-                        className="win-textbox" 
-                        placeholder="Surname"
-                        name="surname"
-                        value={this.state.surname}
-                        onChange={this.changeInput}
-                    />
-                </div>
-                <div>
-                    <b>First name </b> <br/>
-                    <input 
-                        type="text" 
-                        className="win-textbox" 
-                        placeholder="First name"
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.changeInput}
-                    />
-                </div>
-                <div>
-                    <b>Password </b> <br/>
-                    <input 
-                        type="password" 
-                        className="win-textbox" 
-                        placeholder="Password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.changeInput}
-                        required
-                    />
-                </div>
-                <div>
-                    <b>Password (confirmation) </b> <br/>
-                    <input 
-                        type="password" 
-                        className="win-textbox" 
-                        placeholder="Password"
-                        name="passwordConfirmation"
-                        value={this.state.passwordConfirmation}
-                        onChange={this.changeInput}
-                        required
-                    />
+
+            <div className="contentPane list-content Profiles" style={{ width: Calc100PercentMinus(this.props.itemListPaneWidth) }}>
+
+                <div className="listElement listElementIcon">
+                    <span className="viewIcon"/>
                 </div>
                 
-                <br/>
 
-                <button className="win-button" onClick={() => this.props.changeActionList(null)}>Cancel</button>
-                <button 
-                    className="win-button win-button-primary" 
-                    style={{marginLeft: 10}}
-                    onClick={this.sendChanges}
-                >
+                <div className="listElement">
+
+                    <div style={{ overflow: 'hidden' }}>
+                        <input
+                            {...inputAttributes}
+                        />
+                        <IconItemList 
+                            image={this.state.imageProfile} 
+                            type={this.state.typeImageProfile}
+                            imgClick={this.openFileChooser}
+                            size={150}
+                            imgClass="clickable"/>
+                    </div>
+
+                </div>
+
+
+                <ConstructInputs data={user.personalInformation} icon="contactIcon" />
+
+                <ConstructInputs data={user.passwordInformation} icon="permissionsIcon" />
+               
+                <ConstructInputs data={user.validDatesInformation} icon="monthIcon" />
+
+                <ConstructInputs data={user.emailsInformation} icon="emailIcon" />
+                <div style={{ overflow: 'auto' }}>
+                    <button className="win-button" style={{ float: 'right'}} onClick={this.addEmail}>Add email</button>
+                </div>
+
+                <ConstructInputs data={user.contactInformation} icon="phoneIcon" />
+            
+                <ConstructInputs data={user.moreInformation} icon="detailsIcon" />
+
+                <ConstructInputs data={user.activityInformation} icon="documentIcon" />
+
+                <button className={ this.state.buttonSaveClassName } style={{ margin: "20px", float: "right" }} onClick={this.saveChanges}>
                     Save
                 </button>
-            </ContentPane>
+            
+                <br/>
+
+            </div>
+
         )
     }
 }
-UsersEditOne.propTypes = {
+
+Profiles.propTypes = {
     itemListPaneWidth: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.number
@@ -123,3 +186,5 @@ UsersEditOne.propTypes = {
     selectedIndex: PropTypes.array,
     changeActionList: PropTypes.func.isRequired
 }
+
+export default Profiles
