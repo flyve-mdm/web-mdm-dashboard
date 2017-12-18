29f5524322ab9c38162708b89c9972a31f5f17d5
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-18 16:55:40 -0400

    feat(login): create 'ForgotPassword' component

---
+++ src/Login/ForgotPassword/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ForgotPassword extends Component {
+    render() {
+        return (
+            <div className="forgot-password">
+            </div>
+        )
+    }
+}
+
+export default ForgotPassword
+    
