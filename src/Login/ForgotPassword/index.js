e2f97badad2eb7028c76b07e1bb07403ce711d44
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-18 18:14:40 -0400

    feat(ForgotPassword): add message and form to the screen

--- src/Login/ForgotPassword/index.js
+++ src/Login/ForgotPassword/index.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Input } from '../../Utils/Forms'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeEmail } from '../DuckController'
+
+
+function mapStateToProps(state, props) {
+    return {
+        email: state.Login.email
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    const actions = {
+        changeEmail: bindActionCreators(changeEmail, dispatch)
+    }
+    return { actions }
+}
 
 class ForgotPassword extends Component {
+
+    changeInput = (name, value) => {
+        this.props.actions.changeEmail(value)
+    }
+
     render() {
         return (
-            <div className="forgot-password">
+            <div className="LoginForm">
+                <img alt="" src="images/logo2.png" className="img-login"/>
+
+                <h2>
+                    Recover your <br/>
+                    account
+                </h2>
+                <p>
+                    We can help you reset password and security info.
+                    Please, enter your Flyve MDM account in the text box.
+                </p>
+                <Input 
+                    label="" 
+                    type="text" 
+                    name="email" 
+                    value={this.props.email} 
+                    placeholder="Flyve MDM account" 
+                    function={this.changeInput}
+                />
+
+                <button 
+                    className="win-button" 
+                    type="button" 
+                    onClick={() => this.props.history.push('/')}
+                >
+                    Back 
+                </button>
+
+                <button type="submit" className="win-button win-button-primary">Sing in</button>
+
+                <div className="credentials">
+                    <a href="https://flyve-mdm.com/privacy-policy/">Terms and Conditions</a>
+                    <br />
+                    <span>
+                        © 2017 Teclib'.
+                    </span>
+                </div>
             </div>
         )
     }
 }
 
-export default ForgotPassword
+ForgotPassword.propTypes = {
+    email: PropTypes.string.isRequired,
+    history: PropTypes.object.isRequired
+}
+
+export default connect (
+    mapStateToProps,
+    mapDispatchToProps
+)(ForgotPassword)
     
