02d1b3f35cb194f1a2f0f82b428336912a653082
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-21 17:36:02 -0400

    feat(dashboard): pass history to HeaderAdminDashboard

--- src/AdminDashboard/index.js
+++ src/AdminDashboard/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import HeaderAdminDashboard from './HeaderAdminDashboard'
 import BodyAdminDashboard from './BodyAdminDashboard'
+import PropTypes from 'prop-types'
 
-export default class App extends React.Component {
+class App extends React.Component {
 
     render () {
         return (
             <div style={{height: '100%'}}>
-                <HeaderAdminDashboard />
+                <HeaderAdminDashboard history={this.props.history}/>
                 <BodyAdminDashboard />
             </div>
         )
     }
 }
+
+App.propTypes = {
+    history: PropTypes.object.isRequired
+}
+
+export default App
