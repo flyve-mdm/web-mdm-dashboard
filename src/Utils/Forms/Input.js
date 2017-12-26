2de81f63654eabf847153d31f2537c2782fd0568
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-26 16:11:32 -0400

    feat(Input): validate the data in input onBlur or when there is an error

--- src/Utils/Forms/Input.js
+++ src/Utils/Forms/Input.js
@@ -5,9 +5,43 @@ import ErrorValidation from './ErrorValidation'
 
 class Input extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            isCorrect: true,
+            errors: [],
+            className: 'win-textbox'
+        }
+    }
+
     change = (eventObject) => {
         this.props.function(this.props.name, eventObject.target.value)
     }
+
+    componentWillReceiveProps = (newProps) => {
+        if (!this.state.isCorrect) {
+            this.validate(newProps.parametersToEvaluate, newProps.value)
+        }
+    }
+
+    validate = (parametersToEvaluate, value) => {
+        if (parametersToEvaluate) {
+            
+            const newParametersToEvaluate = {
+                ...parametersToEvaluate,
+                data: value
+            }
+            
+            const validation = ErrorValidation.validation(newParametersToEvaluate)
+
+            this.setState({
+                isCorrect: validation.isCorrect,
+                errors: validation.errors,
+                className: validation.isCorrect ? 'win-textbox' : 'win-textbox error-input'
+            })
+        }
+    }
+
 
     render() {
         let deleteIcon 
@@ -17,21 +51,23 @@ class Input extends Component {
         }
         if (this.props.delete) deleteIcon = <span className="deleteIcon" style={{ margin: 10, fontSize: 18 }} onClick={deleteEmail}/>
         const required = (this.props.required === true)
+
         return (
             <div className="list-col">
                 <p>{this.props.label}</p>
                 <input
                     type={this.props.type}
-                    className="win-textbox"
+                    className={this.state.className}
                     name={this.props.name}
                     value={this.props.value}
                     placeholder={this.props.placeholder}
                     onChange={this.change}
+                    onBlur={() => this.validate(this.props.parametersToEvaluate, this.props.value)}
                     disabled={this.props.disabled}
                     style={this.props.style}
                     required={required}
                 />
-                <ErrorValidation {...this.props.parametersToEvaluate} data={this.props.value} />
+                <ErrorValidation errors={this.state.errors} />
                 { deleteIcon }
                 {this.props.delete ? <Confirmation title={`Delete ${this.props.label}`} message={this.props.value} reference={el => this.contentDialog = el} /> : <span/>}
             </div>
