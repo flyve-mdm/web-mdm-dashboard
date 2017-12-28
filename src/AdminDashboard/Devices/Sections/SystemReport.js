0f441d70d66bc8a203c2d2380a4e8a93725aee6b
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-28 15:15:16 -0400

    feat(SystemReport): create the list of system reports

--- src/AdminDashboard/Devices/Sections/SystemReport.js
+++ src/AdminDashboard/Devices/Sections/SystemReport.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 class SystemReport extends Component {
+    
+    buildList = (title) => {
+        let elementList
+        if (Array.isArray(this.props.selectedItemList['PluginFlyvemdmAgent.systemReport'][title])) {
+            elementList = this.props.selectedItemList['PluginFlyvemdmAgent.systemReport'][title].map((x) => {
+                return Object.keys(x).map((element, key2) => {
+                    return (
+                        <div className="list-content" key={key2}>
+                            <div className="list-col">
+                                { element }
+                            </div>
+                            <div className="list-col">
+                                {x[element]}
+                            </div>
+                        </div>
+                    )
+                })
+            })
+        } else {
+            elementList = Object.keys(this.props.selectedItemList['PluginFlyvemdmAgent.systemReport'][title]).map((element, key2) => {
+                return (
+                    <div className="list-content" key={key2}>
+                        <div className="list-col">
+                            { element }
+                        </div>
+                        <div className="list-col">
+                            {this.props.selectedItemList['PluginFlyvemdmAgent.systemReport'][title][element]}
+                        </div>
+                    </div>
+                )
+            })
+        }
+
+        return elementList
+    }
+                
+
     render() {
         return ( 
-            <div>
+            <div className="system-report">
+                {
+                    Object.keys(this.props.selectedItemList['PluginFlyvemdmAgent.systemReport']).map((title, key) => {
+                        return (
+                            <div key={key}>
+                                <div className="title"> { title } </div>
+                                {
+                                    this.buildList(title) 
+                                }
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
