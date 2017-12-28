f158c59555c05af0266fb28dbe7c1d764c3097a5
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-12-28 11:06:51 -0400

    refactor(devices): separate 'Main' of DevicesContent

--- src/AdminDashboard/Devices/DevicesContent.js
+++ src/AdminDashboard/Devices/DevicesContent.js
@@ -1,78 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactWinJS from 'react-winjs'
-import Pluralize from 'pluralize'
-import IconItemList from '../IconItemList'
 import ContentPane from '../../Utils/ContentPane'
-import Confirmation from '../../Utils/Confirmation'
-import { DangerZone } from './Sections'
+import { DangerZone, Main } from './Sections'
 
-export default class DevicesContent extends Component {
-
-    handleEdit = () => {
-        this.props.changeActionList("EditOne")
-    }
-
-    handleDelete = async () => {
-        const isOK = await Confirmation.isOK(this.contentDialog)
-        if (isOK) {
-            let item = this.props.dataSource.itemList
-            let index = this.props.selectedIndex
-            index.sort()
-            index.reverse()
-            index.forEach((i) => {
-                item.splice(i, 1)
-            })
-
-            this.props.changeDataSource(this.props.location, { itemList: item, sort: this.props.dataSource.sort })
-            this.props.onNavigate([this.props.location[0]])
-        }
-    }
+class DevicesContent extends Component {
 
     render() {
         return (
             <ContentPane itemListPaneWidth={this.props.itemListPaneWidth} >
                 <ReactWinJS.Pivot>
                     <ReactWinJS.Pivot.Item key="main" header="Main">
-                        <div>
-                            <div className="contentHeader">
-                                <h2 className="win-h2 titleContentPane" > {Pluralize.singular(this.props.location[0])} </h2>
-                                <div className="itemInfo">
-                                    <IconItemList size={72} />
-                                    <div className="contentStatus">
-                                        <div className="name">{this.props.selectedItemList["PluginFlyvemdmAgent.Computer.User.realname"]}</div>
 
-                                        <div className="message">
-                                            {
-                                                this.props.selectedItemList["PluginFlyvemdmAgent.is_online"] === 1 ? 'Online' : 'Offline'
-                                            }
-                                        </div>
-                                        <div className="source">{this.props.selectedItemList["PluginFlyvemdmAgent.last_contact"]} last contact</div>
-                                        <br />
-                                        <span className="editIcon" style={{ marginRight: '20px' }} onClick={this.handleEdit} />
-                                        <span className="deleteIcon" onClick={this.handleDelete} />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="separator" />
-                            <div className="contentInfo">
-                                <ul>
-                                    <li>
-                                        <div className="callContent">
-                                            <div className="title">Version</div>
-                                            <div>{this.props.selectedItemList["PluginFlyvemdmAgent.version"]}</div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="callContent">
-                                            <div className="title">Fleets</div>
-                                            <div>{this.props.selectedItemList["PluginFlyvemdmAgent.PluginFlyvemdmFleet.name"]}</div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <Confirmation title={`Delete ` + this.props.location[0]} message={this.props.selectedItemList["PluginFlyvemdmAgent.Computer.User.realname"]} reference={el => this.contentDialog = el} /> 
-                        </div>
+                        <Main 
+                            dataSource={this.props.dataSource} 
+                            selectedIndex={this.props.selectedIndex} 
+                            changeActionList={this.props.changeActionList} 
+                            location={this.props.location} 
+                            selectedItemList={this.props.selectedItemList} 
+                            onNavigate={this.props.onNavigate} 
+                            changeDataSource={this.props.changeDataSource}
+                        />
+
                     </ReactWinJS.Pivot.Item>
                     <ReactWinJS.Pivot.Item key="systemReport" header="System Report">
                         <div>System Report</div>
@@ -93,6 +42,7 @@ export default class DevicesContent extends Component {
         )
     }
 }
+
 DevicesContent.propTypes = {
     itemListPaneWidth: PropTypes.oneOfType([
         PropTypes.string,
@@ -106,3 +56,5 @@ DevicesContent.propTypes = {
     selectedItemList: PropTypes.object.isRequired,
     changeActionList: PropTypes.func.isRequired
 }
+
+export default DevicesContent
