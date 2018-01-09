687e0cb6672a685aa4993c8793dafca4ebb1c32a
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-01-09 15:49:43 -0400

    feat(notifications): show and hide notifications
    notifications will be shown on the screen for two seconds or until the user clicks on the close button

--- src/AdminDashboard/index.js
+++ src/AdminDashboard/index.js
@@ -5,11 +5,21 @@ import PropTypes from 'prop-types'
 
 class App extends React.Component {
 
+    constructor (props) {
+        super(props)
+        this.state = {
+            title: '',
+            body: '',
+            type: ''
+        }
+    }
+
     render () {
         return (
             <div style={{height: '100%'}}>
                 <HeaderAdminDashboard history={this.props.history}/>
                 <BodyAdminDashboard />
+                {/* <ToastNotifications title={} body={} type={} /> */}
             </div>
         )
     }
--- src/Utils/ToastNotifications.js
+++ src/Utils/ToastNotifications.js
@@ -1,26 +1,80 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import WinJS from 'winjs'
 
 class ToastNotifications extends Component {
-    render () {
-        return (
-            <div className={`toast toast-${this.props.type}`}>
-                <div className="toast-title">
-                    { this.props.title }
-                </div>
-                <div className="toast-body">
-                    { this.props.body }
-                </div>
-            </div>
-        )
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            show: false,
+            timer: {}
+        }
     }
+
+    componentWillReceiveProps (newProps) {
+        this.setState({
+            show: true
+        }, () => {
+            WinJS.UI.Animation.enterContent(
+                document.getElementsByClassName('toast'), { top: '0px', left: '30px', rtlflip: true }, { mechanism: "transition" }
+            )
+            this.setState({
+                timer: setTimeout(() => {
+                    this.removeNotification()
+                }, 2000)
+            })
+        })
+    }
+
+    removeNotification = () => {
+        WinJS.UI.Animation.exitContent(
+            document.getElementsByClassName('toast'), { top: '0px', left: '20px' }
+        ).then(() => {
+            this.setState({
+                show: false
+            })
+        })
+    }
+
+    renderNotification = () => {
+        let render = <div/>
+        if (this.state.show) {
+            render = (
+                <div className={`toast toast-${this.props.type}`}>
+                    <span className="cancelIcon" style={{ float: 'right', cursor: 'pointer' }} onClick={()=> {
+                        clearTimeout(this.state.timer)
+                        this.removeNotification()
+                    }}/>
+                    <div className="toast-title">
+                        { this.props.title }
+                    </div>
+                    <div className="toast-body">
+                        { this.props.body }
+                    </div>
+                </div>
+            )
+        }
+        
+        return render
+    }
+ 
+    render () {
+        return this.renderNotification()
+    }
+}
+
+ToastNotifications.defaultProps = {
+    title: 'Title example',
+    body: 'body example',
+    type: 'info'
 }
 
 ToastNotifications.propTypes = {
     title: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired
+    type: PropTypes.oneOf(["alert", "warning", "success", "info"]).isRequired,
 }
 
 export default ToastNotifications
