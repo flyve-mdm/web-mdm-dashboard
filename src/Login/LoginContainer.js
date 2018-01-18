5f188b5c17561bba129258e3644493a2d7f72c39
Hector Rondon <hrondon@teclib.com>
2018-01-18 17:56:57 -0400

    feat(notifications): add notifications message

--- src/Login/LoginContainer.js
+++ src/Login/LoginContainer.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class LoginContainer extends Component {
-  render() {
+    render() {
 
-    const style = {
-        textAlign: this.props.centerContent ? 'center' : null,
-        width: this.props.width
-    }
-    return (
-        <div className="LoginFormContainer">
-            <div className="LoginForm" style={style}>
-                <div className="content">
-                    <div className="img-login">
-                        <img alt="" src="images/logo2.png"/>
+        const style = {
+            textAlign: this.props.centerContent ? 'center' : null,
+            width: this.props.width
+        }
+        return (
+            <div className="LoginFormContainer">
+                <div className="LoginForm" style={style}>
+                    <div className="content">
+                        <div className="img-login">
+                            <img alt="" src="images/logo2.png" />
+                        </div>
+
+                        {this.props.children}
                     </div>
 
-                    { this.props.children }
-                </div>
-
-                <div className="credentials">
-                    <a href="https://flyve-mdm.com/privacy-policy/">Terms and Conditions</a>
-                    <br />
-                    <span>
-                        © 2017 Teclib'.
+                    <div className="credentials">
+                        <a href="https://flyve-mdm.com/privacy-policy/">Terms and Conditions</a>
+                        <br />
+                        <span>
+                            © 2017 Teclib'.
                     </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
 
 LoginContainer.defaultProps = {
