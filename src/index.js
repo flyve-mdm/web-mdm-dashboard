7f617a4edf8f63c7eaef4c4fa466db8a6f7d9afc
Hector Rondon <hrondon@teclib.com>
2018-01-19 17:34:41 -0400

    feat(login): show dashboard when user is authenticated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store, history } from './store'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import registerServiceWorker from './registerServiceWorker'
 import AdminDashboard from './AdminDashboard'
@@ -14,12 +14,34 @@ import { SingUp, SignIn, ForgotPassword } from './Login'
 import './styles/App.scss'
 import './styles/ui-light.css'
 
+const auth = {
+    isAuthenticated: () => {
+        if (localStorage.getItem('sessionToken') && localStorage.getItem('sessionToken') !== undefined ) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        auth.isAuthenticated() ? (
+            <Component {...props} />
+        ) : (
+                <Redirect to={{
+                    pathname: '/'
+                }} />
+            )
+    )} />
+)
+
 ReactDOM.render((
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <Switch>
                 <Route exact path='/' component={SingUp} />
-                <Route path='/app' component={AdminDashboard} />
+                <PrivateRoute path="/app" component={AdminDashboard} />
                 <Route path='/signIn' component={SignIn} />
                 <Route path='/forgotPassword' component={ForgotPassword} />
             </Switch>
