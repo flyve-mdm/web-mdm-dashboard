1861490c426cc88e177463cbc80b9f45e50d2d81
Hector Rondon <hrondon@teclib.com>
2018-01-24 12:27:46 -0400

    refactor(glpi): create global variable for glpi api

---
+++ src/Utils/GlpiApi.js
@@ -0,0 +1,7 @@
+import Glpi from 'javascript-library-glpi'
+import config from '../config.json'
+
+let glpi = new Glpi({ url: config.URL_GLPI_API })
+glpi.sessionToken = localStorage.getItem('sessionToken')
+
+export default glpi
