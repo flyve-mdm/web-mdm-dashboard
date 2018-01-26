7f6806d338377662de3126d518a5166fa9ba35d9
Hector Rondon <hrondon@teclib.com>
2018-01-26 16:06:04 -0400

    refactor(agent): get an item of current agent

--- src/AdminDashboard/Devices/Sections/Main.js
+++ src/AdminDashboard/Devices/Sections/Main.js
@@ -3,8 +3,41 @@ import PropTypes from 'prop-types'
 import Pluralize from 'pluralize'
 import IconItemList from '../../IconItemList'
 import Confirmation from '../../../Utils/Confirmation'
+import Loading from '../../../Utils/Loading'
 
 class DangerZone extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: undefined
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, prevContext) {
+        if (this.props.selectedItemList !== prevProps.selectedItemList) {
+            this.setState({
+                data: undefined
+            })
+            this.handleRefresh()
+        }
+    }
+
+    componentDidMount() {
+        this.handleRefresh()
+    }
+
+    handleRefresh = () => {
+        this.props.glpi.getAnItem('PluginFlyvemdmAgent', this.props.selectedItemList[0], { expand_dropdowns: true })
+            .then((response) => {
+                this.setState({
+                    data: response
+                })
+            })
+            .catch((error) => {
+
+            })
+    }
 
     handleDelete = async () => {
         const isOK = await Confirmation.isOK(this.contentDialog)
@@ -28,21 +61,25 @@ class DangerZone extends Component {
     }
     
     render() {
-        return (
+        let renderComponent 
+        if (this.state.data === undefined) {
+            renderComponent = <Loading message="Loading..."/>
+        } else {
+            renderComponent = (
             <div>
                 <div className="contentHeader">
                     <h2 className="win-h2 titleContentPane" > {Pluralize.singular(this.props.location[0])} </h2>
                     <div className="itemInfo">
                         <IconItemList size={72} />
                         <div className="contentStatus">
-                            <div className="name">{this.props.selectedItemList["PluginFlyvemdmAgent.Computer.User.realname"]}</div>
+                            <div className="name">{this.state.data["name"]}</div>
 
                             <div className="message">
                                 {
-                                    this.props.selectedItemList["PluginFlyvemdmAgent.is_online"] === 1 ? 'Online' : 'Offline'
+                                    this.state.data["is_online"] === 1 ? 'Online' : 'Offline'
                                 }
                             </div>
-                            <div className="source">{this.props.selectedItemList["PluginFlyvemdmAgent.last_contact"]} last contact</div>
+                            <div className="source">{this.state.data["last_contact"]} last contact</div>
                             <br />
                             <span className="editIcon" style={{ marginRight: '20px' }} onClick={this.handleEdit} />
                             <span className="deleteIcon" onClick={this.handleDelete} />
@@ -55,30 +92,31 @@ class DangerZone extends Component {
                         <li>
                             <div className="callContent">
                                 <div className="title">Version</div>
-                                <div>{this.props.selectedItemList["PluginFlyvemdmAgent.version"]}</div>
+                                <div>{this.state.data["version"]}</div>
                             </div>
                         </li>
                         <li>
                             <div className="callContent">
                                 <div className="title">Fleets</div>
-                                <div>{this.props.selectedItemList["PluginFlyvemdmAgent.PluginFlyvemdmFleet.name"]}</div>
+                                <div>{this.state.data["plugin_flyvemdm_fleets_id"]}</div>
                             </div>
                         </li>
                     </ul>
                 </div>
-                <Confirmation title={`Delete ` + this.props.location[0]} message={this.props.selectedItemList["PluginFlyvemdmAgent.Computer.User.realname"]} reference={el => this.contentDialog = el} /> 
+                <Confirmation title={`Delete ` + this.props.location[0]} message={this.state.data["PluginFlyvemdmAgent.Computer.User.realname"]} reference={el => this.contentDialog = el} /> 
             </div>
-        )
+            )
+        }
+        return renderComponent
     }
 }
 
 DangerZone.propTypes = {
     dataSource: PropTypes.object.isRequired,
-    selectedIndex: PropTypes.array.isRequired,
     changeActionList: PropTypes.func.isRequired,
     changeDataSource: PropTypes.func.isRequired,
     location: PropTypes.array.isRequired,
-    selectedItemList: PropTypes.object.isRequired,
+    selectedItemList: PropTypes.array.isRequired,
     onNavigate: PropTypes.func.isRequired,
     showNotification: PropTypes.func.isRequired
 }
