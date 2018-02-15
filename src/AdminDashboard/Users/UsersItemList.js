8936bbeff81cd1e56024cb23b49ae03dcd8927e4
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-15 10:05:06 -0400

    refactor(users): show only the first email
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/AdminDashboard/Users/UsersItemList.js
+++ src/AdminDashboard/Users/UsersItemList.js
@@ -5,12 +5,17 @@ export default class UsersItemList extends Component {
     render() {
         console.log(this.props.itemList)
         let imageProfile = this.props.itemList['User.picture'] ? this.props.itemList['User.picture'] : "profile.png"
+        const email = this.props.itemList['User.UserEmail.email'] ? 
+            Array.isArray(this.props.itemList['User.UserEmail.email']) ? 
+                this.props.itemList['User.UserEmail.email'][0] 
+                    : this.props.itemList['User.UserEmail.email'] 
+                        : ''
         return (
             <div>
                 <IconItemList image={imageProfile} size={42} />
                 <div style={{ display: 'inline-block' }}>
                     <div className="name">{this.props.itemList['User.name']}</div>
-                    <div className="detail">{this.props.itemList['User.UserEmail.email']}</div>
+                    <div className="detail">{ email }</div>
                 </div>
             </div>
         )
