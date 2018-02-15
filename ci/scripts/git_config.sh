ab7e0f1e06cebd35805afe1145ca238d8d3ab227
Hector Rondon <hrondon@teclib.com>
2018-02-15 12:47:29 -0400

    ci(deploy): configure git
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/git_config.sh
+++ ci/scripts/git_config.sh
@@ -4,5 +4,5 @@ if [[ -n $GITHUB_TOKEN ]]; then
     git config --global user.email $GITHUB_EMAIL
     git config --global user.name "Flyve MDM"
     git remote remove origin
-    git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
+    git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
 fi
