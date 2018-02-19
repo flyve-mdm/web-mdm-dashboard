e8a22838be0348bf89ccbd2c42b2163be5c13b29
Hector Rondon <hrondon@teclib.com>
2018-02-19 15:35:27 -0400

    refactor(enroll): show loading indicator
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/AdminDashboard/Devices/Enroll.js
+++ src/AdminDashboard/Devices/Enroll.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ContentPane from '../../Utils/ContentPane'
+import Loading from '../../Utils/Loading'
 
 export default class Enroll extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            isLoading: false,
             email: ''
         }
     }
 
     inviteDevice = () => {
         if(this.state.email.trim() !== "") {
+            this.setState({
+                isLoading: true
+            })
             this.props.glpi.addItem({ itemtype: 'PluginFlyvemdmInvitation', input: { _useremails: this.state.email.trim()} })
             .then((response) => {
-                this.props.changeActionList(null)
+                
+                this.setState({
+                    isLoading: false
+                })
                 this.props.showNotification('Success', 'invitation sent')
+                this.props.changeActionList(null)
+                
             })
             .catch((error) => {
                 if (error.length > 1) {
@@ -31,33 +41,39 @@ export default class Enroll extends Component {
     }
 
     render() {
-        return (
-            <ContentPane itemListPaneWidth={this.props.itemListPaneWidth}>
-                <h2 className="win-h2 titleContentPane">
-                    Enroll new device
-                </h2>
-                <p>Please insert an active email address.</p>
-                <p>An email will be sent with a QR code.</p>
-                <input 
-                    type="email" 
-                    className="win-textbox" 
-                    placeholder="Email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.changeInput}
-                    required
-                />
-                <br/>
-                <button className="win-button" onClick={() => this.props.changeActionList(null)}>Cancel</button>
-                <button 
-                    className="win-button win-button-primary" 
-                    style={{marginLeft: 10}}
-                    onClick={this.inviteDevice}
-                >
-                    Save
-                </button>
-            </ContentPane >
-        )
+        let renderComponent
+        if (this.state.isLoading) {
+            renderComponent = <Loading message="Loading..." />
+        } else {
+            renderComponent = (
+                <ContentPane itemListPaneWidth={this.props.itemListPaneWidth}>
+                    <h2 className="win-h2 titleContentPane">
+                        Enroll new device
+                    </h2>
+                    <p>Please insert an active email address.</p>
+                    <p>An email will be sent with a QR code.</p>
+                    <input
+                        type="email"
+                        className="win-textbox"
+                        placeholder="Email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.changeInput}
+                        required
+                    />
+                    <br />
+                    <button className="win-button" onClick={() => this.props.changeActionList(null)}>Cancel</button>
+                    <button
+                        className="win-button win-button-primary"
+                        style={{ marginLeft: 10 }}
+                        onClick={this.inviteDevice}>
+                        Save
+                    </button>
+                </ContentPane >
+            )
+        }
+        
+        return renderComponent
     }
 }
 Enroll.propTypes = {
