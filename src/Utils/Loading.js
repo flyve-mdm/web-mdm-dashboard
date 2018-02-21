eea265b68f456f8806d1db6ecca0d4a904cbb55f
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-21 15:05:57 -0400

    feat(loading): create small loading component
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/Utils/Loading.js
+++ src/Utils/Loading.js
@@ -6,7 +6,19 @@ class Loading extends Component {
     render () {
         const headerSize = this.props.headerSize ? this.props.headerSize : 0
 
-        return (
+        const loadComponent = this.props.small ? (
+            <div className="loading" style={{display: 'inline-block', verticalAlign: 'middle', marginLeft: '10px'}}>
+                <div>
+                    <div className='small-loader'>
+                        <div className='circle'></div>
+                        <div className='circle'></div>
+                        <div className='circle'></div>
+                        <div className='circle'></div>
+                        <div className='circle'></div>
+                    </div>   
+                </div>
+            </div>
+        ) : (
             <div className="loading" style={{ marginTop: - headerSize, display: 'flex', height: '100%', justifyContent: 'center', textAlign: 'center', alignItems: 'center', flexDirection: 'column' }}>
                 <div>
                     <div className='loader'>
@@ -20,12 +32,15 @@ class Loading extends Component {
                 </div>
             </div>
         )
+
+        return loadComponent
     }
 }
 
 Loading.propTypes = {
     message: PropTypes.string,
-    headerSize: PropTypes.number
+    headerSize: PropTypes.number,
+    small: PropTypes.bool
 }
 
 export default Loading
