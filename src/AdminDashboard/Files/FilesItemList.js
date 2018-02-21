517f66ed6b0e27c3b8da97bbc7f72a9ce5580de0
Hector Rondon <hrondon@teclib.com>
2018-02-21 17:44:17 -0400

    refactor(files): change field to show on list
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/AdminDashboard/Files/FilesItemList.js
+++ src/AdminDashboard/Files/FilesItemList.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import BytesToSize from '../../Utils/BytesToSize'
 
 export default class FilesItemList extends Component {
     render() {
         return (
             <div style={{ display: 'inline-block' }}>
                 <div className="name">{this.props.itemList["PluginFlyvemdmFile.name"]}</div>
-                <div className="detail">{BytesToSize(this.props.itemList["PluginFlyvemdmFile.filesize"])}</div>
             </div>
         )
     }
