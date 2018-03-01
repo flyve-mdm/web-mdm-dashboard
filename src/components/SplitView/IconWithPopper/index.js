d98df4520d61b469b460ff7f9018146ccefa40d9
charlyjazz <carlosjazzc1@gmail.com>
2018-03-01 11:25:47 -0400

    feat(react-architecture): add logout icon to splitview
    Signed-off-by: charlyjazz <carlosjazzc1@gmail.com>

--- src/components/SplitView/IconWithPopper/index.js
+++ src/components/SplitView/IconWithPopper/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const iconWithPopper = ({to, iconName, title, disabled}) => (
-  <div>
-    <NavLink to={to} activeClassName="selected">
-      <span className={iconName}/>
-    </NavLink>
-  </div>
-)
+const iconWithPopper = ({to, iconName, title, disabled, click}) => {
+  if (typeof(to) === "string") {
+    return (
+      <div>
+        <NavLink to={to} activeClassName="selected">
+          <span className={iconName}/>
+        </NavLink>
+      </div>
+    )
+  } else {
+    return (
+      <div onClick={click}>
+        <a>
+          <span className={iconName}/>
+        </a>
+      </div>
+    )
+  }
+}
  
 export default iconWithPopper;
