efc0cc0f5f4a3247a3b023cbb27c96eb5ad4b9e9
Hector Rondon <hrondon@teclib.com>
2018-03-01 18:39:56 -0400

    fix(dashboard): fix integration components
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -2,18 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 
-const InfoBox = ({to, icon, count, name}) => {
+const InfoBox = ({ to, icon, count, name }) => {
     return (
         <Link to={to}>
-          <div className="info-box">
-              <span className="content-box">
-                  {count}
-              </span>
-              <span className={'icon-box ' + icon} />
-              <span className="title-box">
-                  { name.toUpperCase() }
-              </span>
-          </div>
+            <div className="info-box">
+                <span className="content-box">
+                    {count}
+                </span>
+                <span className={'icon-box ' + icon} />
+                <span className="title-box">
+                    {name.toUpperCase()}
+                </span>
+            </div>
         </Link>
     )
 }
