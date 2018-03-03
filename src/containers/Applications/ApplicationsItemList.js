ade3d526da39a194f7d44ab186b30c5d4e07803b
Hector Rondon <hrondon@teclib.com>
2018-03-03 15:00:40 -0400

    feat(users): create router for users
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/containers/Applications/ApplicationsItemList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import IconItemList from '../../components/IconItemList'
+
+export default class ApplicationsItemList extends Component {
+    render() {
+        let size = this.props.size
+        let image = "data:image/png;base64, " + this.props.itemList["PluginFlyvemdmPackage.icon"]
+        return (
+            <div>
+                <IconItemList 
+                    size={size} 
+                    image={image} 
+                    type="base64"
+                    backgroundColor="transparent"
+                />
+                <div style={{ marginLeft: 5, display: 'inline-block' }}>
+                    <div className="name">{this.props.itemList["PluginFlyvemdmPackage.alias"]}</div>
+                    <div className="detail">{this.props.itemList["PluginFlyvemdmPackage.name"]}</div>
+                </div>
+            </div>
+        )
+    }
+}
