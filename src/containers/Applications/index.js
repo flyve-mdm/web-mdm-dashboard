a9ea394a5c75db28bffda87ecdd234527b29bf9c
charlyjazz <carlosjazzc1@gmail.com>
2018-03-05 13:16:17 -0400

    refactor(itcss): refactor some styles
    Signed-off-by: charlyjazz <carlosjazzc1@gmail.com>

--- src/containers/Applications/index.js
+++ src/containers/Applications/index.js
@@ -54,7 +54,7 @@ export default class Applications extends Component {
         } else {
             let itemListPaneWidth = 320
             return (
-                <div className="flex-block">
+                <div className="flex-block --with-scroll --with-content-pane">
                     <ApplicationsList
                         itemListPaneWidth={itemListPaneWidth}
                         animation={this.state.animation}
