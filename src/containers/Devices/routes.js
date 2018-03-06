9fb5aee4c292009a0b2fbe7aa9984bddc60f141a
Hector Rondon <hrondon@teclib.com>
2018-03-06 16:49:14 -0400

    feat(agent): create router for init state
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/Devices/routes.js
+++ src/containers/Devices/routes.js
@@ -1,55 +1,12 @@
-import React from 'react'
-import { I18n } from 'react-i18nify'
-
-const MonkComponent1 = () => <div><h1> Mock1 </h1></div>
-const MonkComponent2 = () => <div><h1> Mock2 </h1></div>
-const MonkComponent3 = () => <div><h1> Mock3 </h1></div>
-const MonkComponent4 = () => <div><h1> Mock4 </h1></div>
-const MonkComponent5 = () => <div><h1> Mock5 </h1></div>
+import EmptyMessage from '../../components/EmptyMessage'
 
 const routes = [
   {
     path: '/',
-    name: I18n.t('about.overview'),
-    component: MonkComponent1,
+    name: 'No selected',
+    component: EmptyMessage,
     exact: true
-  },
-  // {
-  //   path: '/system',
-  //   name: I18n.t('about.system_information'),
-  //   component: MonkComponent3,
-  //   exact: false
-  // },
-  // {
-  //   path: '/help',
-  //   name: I18n.t('about.help_center'),
-  //   component: MonkComponent4,
-  //   exact: false
-  // },
-  // {
-  //   path: '/contact',
-  //   name: I18n.t('about.contact'),
-  //   component: MonkComponent1,
-  //   exact: false
-  // },
-  // {
-  //   path: '/release',
-  //   name: I18n.t('about.release_notes'),
-  //   component: MonkComponent2,
-  //   exact: false
-  // },
-  // {
-  //   path: '/term',
-  //   name: I18n.t('about.term_of_use'),
-  //   component: MonkComponent4,
-  //   exact: false
-  // },
-  // {
-  //   path: '/license',
-  //   name: I18n.t('about.license'),
-  //   component: MonkComponent5,
-  //   exact: false
-  // }
+  }
 ]
 
 export default routes
