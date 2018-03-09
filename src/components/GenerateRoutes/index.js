187f99e4942e50aa77bfbe6abb4ea4fb28c6e60c
Hector Rondon <hrondon@teclib.com>
2018-03-09 12:57:07 -0400

    refactor(routers): set props to components
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/components/GenerateRoutes/index.js
+++ src/components/GenerateRoutes/index.js
@@ -1,40 +1,40 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import PropsRoute from './PropsRoute'
 
 // TODO: Enable PrivateRoute if route if private
 
-const GenerateRoutes = ({routes, rootPath, withNotFound, data}) => {
-  let r = routes.map(({exact, path, component}, i) => {
-    if (typeof(data) === 'object') {
+const GenerateRoutes = ({ routes, rootPath, withNotFound, data }) => {
+  let r = routes.map(({ exact, path, component }, i) => {
+    if (typeof (data) === 'object') {
       return (
         <PropsRoute
-        exact={exact}
-        component={component}
-        key={i}
-        data={data}
-        path={
-          typeof(rootPath) === "string" 
-          ? path === '/'
-            ? rootPath
-            : rootPath + path
-          : path
-        }/>
+          exact={exact}
+          component={component}
+          key={i}
+          {...data}
+          path={
+            typeof (rootPath) === "string"
+              ? path === '/'
+                ? rootPath
+                : rootPath + path
+              : path
+          } />
       )
     }
     else {
       return (
         <Route
-        exact={exact}
-        component={component}
-        key={i}
-        path={
-          typeof(rootPath) === "string" 
-          ? path === '/'
-            ? rootPath
-            : rootPath + path
-          : path
-        }/>
+          exact={exact}
+          component={component}
+          key={i}
+          path={
+            typeof (rootPath) === "string"
+              ? path === '/'
+                ? rootPath
+                : rootPath + path
+              : path
+          } />
       )
     }
   });
@@ -43,7 +43,11 @@ const GenerateRoutes = ({routes, rootPath, withNotFound, data}) => {
   //   <Route key={routes.length + 1} render={() => <h1 style={{textAlign: 'center'}}>Not Found</h1>} />
   // )
 
-  return r
+  return (
+    <Switch>
+      {r}
+    </Switch>
+  )
 }
 
 export default GenerateRoutes
