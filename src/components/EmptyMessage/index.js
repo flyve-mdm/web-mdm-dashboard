4f60110640ca9457177a09955cd31c11f9162eca
Hector Rondon <hrondon@teclib.com>
2018-03-13 17:53:28 -0400

    refactor(EmptyMessage): add style to center content
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/components/EmptyMessage/index.js
+++ src/components/EmptyMessage/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Calc100PercentMinus from '../../shared/calc100PercentMinus'
 
 const EmptyMessage = props => (
-    <div className="contentPane" style={{ width: Calc100PercentMinus(props.itemListPaneWidth) }}>
-        <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+    <div className="center-block-content">
             <h1 className="win-h1" style={{ color: 'grey' }}> 
                 { props.message } 
             </h1>
-        </div>
     </div>
 )
 
