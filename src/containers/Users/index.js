eae10147b74416a05a95582e52f3d1c91a6537f7
Hector Rondon <hrondon@teclib.com>
2018-03-14 14:13:14 -0400

    refactor(users): handle responsive design
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import routes from './routes'
+import withGLPI from '../../hoc/withGLPI'
+import GenerateRoutes from '../../components/GenerateRoutes'
 import UsersList from './components/UsersList'
 import {
     uiTransactionStart,
@@ -7,13 +10,13 @@ import {
 } from '../../store/ui/actions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import GenerateRoutes from '../../components/GenerateRoutes'
-import withGLPI from '../../hoc/withGLPI'
-import routes from './routes'
+import getMode from '../../shared/getMode'
+import calc100PercentMinus from '../../shared/calc100PercentMinus'
 
 function mapStateToProps(state, props) {
     return {
-        isLoading: state.ui.loading
+        isLoading: state.ui.loading,
+        currentUser: state.auth.currentUser
     }
 }
 
@@ -31,19 +34,44 @@ class Users extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            mode: getMode(),
+            itemListPaneWidth: getMode() === 'small' ? '100%' : 320,
             selectionMode: false,
             action: null,
-            selectedItems: []            
+            selectedItems: []
         }
     }
 
-    changeAction = action => this.setState({action})
-    changeSelectionMode = selectionMode => this.setState({selectionMode})
-    changeSelectedItems = selectedItems => this.setState({ selectedItems })
+    handleResize = () => {
+        let nextMode = getMode()
+
+        if (nextMode === 'small') {
+            this.setState({
+                itemListPaneWidth: '100%'
+            })
+        } else {
+            this.setState({
+                itemListPaneWidth: 320
+            })
+        }
+
+        if (this.state.mode !== nextMode) {
+            this.setState({
+                mode: nextMode 
+            })
+        }
+    }
+
+    componentWillMount () {
+        window.addEventListener('resize', this.handleResize)
+    }
+
+    componentWillUnmount () {
+        window.removeEventListener('resize', this.handleResize)
+    }
 
     propsData = () => {
         return {
-            itemListPaneWidth: 320,
             changeSelectionMode: this.changeSelectionMode,
             selectionMode: this.state.selectionMode,
             selectedItems: this.state.selectedItems,
@@ -56,18 +84,82 @@ class Users extends Component {
         }
     }
 
+    changeSelectedItems = selectedItems => this.setState({ selectedItems })
+    changeAction = action => this.setState({ action })
+    changeSelectionMode = selectionMode => this.setState({ selectionMode })
+
+    stylesList = () => {
+
+        let styles = {
+            width: this.state.itemListPaneWidth   
+        }
+
+        if (this.state.mode === 'small') {
+            if (this.state.selectedItems.length === 0  || this.props.history.location.pathname === '/app/devices') {
+                styles.display = 'inline-block'
+            } else {
+                styles.display = 'none'
+            }
+
+        } else {
+            styles.display = 'inline-block'
+        } 
+
+        return styles
+    }
+
+    stylesContent = () => {
+
+        const validWidth = this.state.itemListPaneWidth === '100%' ? 0 : this.state.itemListPaneWidth
+        let styles = {
+            width: calc100PercentMinus(validWidth),
+            height:'100%'
+        }
+
+        if (this.state.mode === 'small') {
+            if (this.state.selectedItems.length === 0  || this.props.history.location.pathname === '/app/devices') {
+                styles.display = 'none'
+            } else {
+                styles.display = 'inline-flex'
+            }
+            
+        } else {
+            styles.display = 'inline-flex'
+        } 
+
+        return styles
+    }
+
     render() {
-        return (
-            <div className="flex-block --with-scroll --with-content-pane">
-                <UsersList 
+
+        let renderComponents = (
+
+            <React.Fragment>
+                <div className="listPane flex-block-list" style={{...this.stylesList()}}>
+                <UsersList
+                    key="list"
                     {...this.propsData()}
                 />
-                <GenerateRoutes routes={routes} rootPath={this.props.match.url} data={{...this.propsData()}} />
+                </div>
+                <div className="flex-block-content" style={{...this.stylesContent()}}>
+                <GenerateRoutes 
+                    key="content" 
+                    routes={routes} 
+                    rootPath={this.props.match.url} 
+                    data={{...this.propsData()}} 
+                />
+                </div>
+            </React.Fragment>
+
+        )
+
+        return (
+            <div className="flex-block --with-scroll">
+                {renderComponents}
             </div>
         )
     }
 }
-
 export default connect(
     mapStateToProps,
     mapDispatchToProps
