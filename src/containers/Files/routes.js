48b8d915bb0dd9d79813e2708f41609509952dd7
Hector Rondon <hrondon@teclib.com>
2018-03-16 17:28:04 -0400

    feat(files): add route to create new file
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/Files/routes.js
+++ src/containers/Files/routes.js
@@ -1,4 +1,5 @@
 import EmptyMessage from '../../components/EmptyMessage'
+import FilesAdd from './components/FilesAdd'
 
 const routes = [
     {
@@ -6,7 +7,13 @@ const routes = [
         name: 'No selected',
         component: EmptyMessage,
         exact: true
-    }
+    },
+    {
+        path: '/add',
+        name: 'Add',
+        component: FilesAdd,
+        exact: true
+    },
 ]
 
 export default routes
