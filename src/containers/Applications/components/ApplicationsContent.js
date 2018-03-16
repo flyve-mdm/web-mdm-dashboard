8fccd18833622241127be9093c346997fb8270e3
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-16 18:31:54 -0400

    refactor(applications): redirect to 'edit' in the content page
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/Applications/components/ApplicationsContent.js
+++ src/containers/Applications/components/ApplicationsContent.js
@@ -108,7 +108,7 @@ export default class ApplicationsContent extends Component {
                                 <div className="detail">{BytesToSize(this.state.data["filesize"])}</div>
                                 <span className="source">{this.state.data["source"]}</span>
                                 <br />
-                                <span className="editIcon" style={{ marginRight: '20px' }} onClick={() => this.props.changeAction('Edit')} />
+                                <span className="editIcon" style={{ marginRight: '20px' }} onClick={() => this.props.history.push(`/app/applications/${this.state.id}/edit`)} />
                                 <span className="deleteIcon" onClick={this.handleDelete} />
                             </div>
                         </div>
