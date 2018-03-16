02741f1d5c7764fed1dcabd19a5b9c7936adb18c
Hector Rondon <hrondon@teclib.com>
2018-03-16 16:53:24 -0400

    feat(files): create router main screen
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/containers/Files/routes.js
@@ -0,0 +1,12 @@
+import EmptyMessage from '../../components/EmptyMessage'
+
+const routes = [
+    {
+        path: '/',
+        name: 'No selected',
+        component: EmptyMessage,
+        exact: true
+    }
+]
+
+export default routes
