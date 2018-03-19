faa5195eaecd722b973cad132d6fdeeb7ff6139f
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-19 13:08:11 -0400

    feat(edit): validate required props
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/EditMultiple/index.js
+++ src/components/EditMultiple/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class EditMultiple extends Component {
     render() {
         return ""
     }
+}
+EditMultiple.propTypes = {
+    selectedItems: PropTypes.array.isRequired,
+    changeAction: PropTypes.func.isRequired,
+    changeSelectionMode: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
+    setNotification: PropTypes.func.isRequired,
+    glpi: PropTypes.object.isRequired
 }
