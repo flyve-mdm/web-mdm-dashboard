eb13e3edaa8830c5734452cd28c9d8a5226e382f
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-22 16:33:27 -0400

    fix(help-center): render correct article according to the route
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/About/components/HelpCenter/HelpCenterArticle.js
+++ src/containers/About/components/HelpCenter/HelpCenterArticle.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 
-import ArticleMock from './articles/article_1.md'
+class HelpCenterArticle extends Component {
+    render () {
+        const article = require(`./articles/${this.props.history.location.pathname.split("/help/")[1]}.md`)
+        return (
+            <div className="listPane" style={{ padding: 0 }}>
+                <ReactMarkdown source={article}/>
+            </div>
+        ) 
+    }
+}
 
-const HelpCenterArticle = (props) => {
-    return (
-        <div className="listPane" style={{ padding: 0 }}>
-            <ReactMarkdown source={ArticleMock}/>
-        </div>
-    )
+HelpCenterArticle.propTypes = {
+    history: PropTypes.object.isRequired
 }
 
 export default HelpCenterArticle
