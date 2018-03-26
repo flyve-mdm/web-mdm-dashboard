8cefb6479ac8aa48fda45efe02f2554447c232e8
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-26 15:18:19 -0400

    refactor(about): improve the titles of the help center
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/About/components/HelpCenter/index.js
+++ src/containers/About/components/HelpCenter/index.js
@@ -5,11 +5,8 @@ import routes from './routes'
 class HelpCenter extends Component {
     render() {
         return (
-            <div>                
-                <h2>Help Center</h2>
-                <div className="aboutPane">
-                    <GenerateRoutes routes={routes} rootPath={this.props.match.url}/>
-                </div>
+            <div className="aboutPane">
+                <GenerateRoutes routes={routes} rootPath={this.props.match.url}/>
             </div>
         )
     }
