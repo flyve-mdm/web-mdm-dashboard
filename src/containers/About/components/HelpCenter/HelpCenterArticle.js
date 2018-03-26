b309c469c58962f8050c229ab48657fb2f5a063d
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-26 14:21:32 -0400

    refactor(about): load the articles of the help center from glpi
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/About/components/HelpCenter/HelpCenterArticle.js
+++ src/containers/About/components/HelpCenter/HelpCenterArticle.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ReactMarkdown from 'react-markdown'
+import Loading from "../../../../components/Loading"
+import withGLPI from "../../../../hoc/withGLPI"
 
 class HelpCenterArticle extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            article: undefined,
+            id: this.props.history.location.pathname.split("/")[4],
+            isLoading: true
+        }
+    }
+
+    componentDidMount = async () => {
+        try {
+            this.setState({
+                article: await this.props.glpi.getAnItem({itemtype: "KnowbaseItem", id: this.state.id}),
+                isLoading: false
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    htmlDecode (input) {
+        const e = document.createElement('div')
+        e.innerHTML = input
+        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue
+    }
+      
     render () {
-        const article = require(`./articles/${this.props.history.location.pathname.split("/help/")[1]}.md`)
         return (
-            <div className="listPane" style={{ padding: 0 }}>
-                <ReactMarkdown source={article}/>
-            </div>
+            this.state.isLoading ? 
+                <Loading /> :
+                    <div className="listPane" style={{ padding: 0 }}>
+                        <h3>{this.state.article.name}</h3>
+                        <div dangerouslySetInnerHTML={{ __html: this.htmlDecode(this.state.article.answer) }} />
+                    </div>
         ) 
     }
 }
 
 HelpCenterArticle.propTypes = {
-    history: PropTypes.object.isRequired
+    history: PropTypes.object.isRequired,
+    glpi: PropTypes.object.isRequired
 }
 
-export default HelpCenterArticle
+export default withGLPI(HelpCenterArticle)
