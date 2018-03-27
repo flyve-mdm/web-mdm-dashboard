88f0a252e703204443f461f0d335b9a6ac5eaab6
Hector Rondon <hrondon@teclib.com>
2018-03-26 20:36:13 -0400

    feat(search): create search query builder component
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/containers/SearchEngine/components/SearchQueryBuilder.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import QueryBuilder from 'react-querybuilder'
+import PropTypes from 'prop-types'
+
+
+class SearchQueryBuilder extends Component {
+    render() {
+        return (
+            <div className="queryBuilder">
+                <QueryBuilder
+                    fields={this.props.fields}
+                    operators={[
+                        { name: 'contains', label: 'Contains' },
+                        { name: 'equal', label: 'Equals' }, // equal instead equals because work it
+                        { name: 'notequals', label: 'Not equals' },
+                        { name: 'lessthan', label: 'Less Than' },
+                        { name: 'morethan', label: 'More Than' },
+                        { name: 'under', label: 'Under' },
+                        { name: 'notunder', label: 'Not Under' }
+                    ]}
+                    onQueryChange={this.props.handleChangeQuery} />
+            </div>
+        )
+    }
+}
+
+SearchQueryBuilder.propTypes = {
+    fields: PropTypes.array.isRequired,
+    handleChangeQuery: PropTypes.func.isRequired
+}
+
+
+export default SearchQueryBuilder
