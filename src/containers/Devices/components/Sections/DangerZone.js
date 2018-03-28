13b81ae663bd07581b6c7d2afc7a2fa4f13ad820
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-28 13:15:08 -0400

    feat(devices): add DangerZone section to i18n
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/Devices/components/Sections/DangerZone.js
+++ src/containers/Devices/components/Sections/DangerZone.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Confirmation from '../../../../components/Confirmation'
+import { I18n } from 'react-i18nify'
 
 class DangerZone extends Component {
 
@@ -86,45 +87,57 @@ class DangerZone extends Component {
             <div>
                 <div className="listElement">
                     <div className="message">
-                        Wipe Device
-                        <div className="detail">Wipe will delete all data on the device</div>
+                        {I18n.t('devices.danger_zone.wipe')}
+                        <div className="detail">
+                            {I18n.t('devices.danger_zone.wipe_description')}
+                        </div>
                     </div>
                     <div className="controller">
-                        <button className="btn --primary" onClick={this.wipe}>Wipe</button>
+                        <button className="btn --primary" onClick={this.wipe}>
+                            {I18n.t('commons.wipe')}
+                        </button>
                     </div>
                     <Confirmation 
-                        title="This is the last warning"
-                        message="You're about to wipe the device. Are you really certain ? (the device is going to delete all its data next time it turns on)"
+                        title={I18n.t('devices.danger_zone.last_warning')}
+                        message={I18n.t('devices.danger_zone.last_warning_message')}
                         reference={el => this.wipeDevice = el} 
                     /> 
                 </div>
 
                 <div className="listElement">
                     <div className="message">
-                        Unenrollment Device
-                        <div className="detail">Unenrollment will change the state of the device (policies are unapplied, deployed files and app will be removed)</div>
+                        {I18n.t('devices.danger_zone.unenrollment')}
+                        <div className="detail">
+                            {I18n.t('devices.danger_zone.unenrollment_description')}
+                        </div>
                     </div>
                     <div className="controller">
-                        <button className="btn --primary" onClick={this.unenroll}>Unenroll</button>
+                        <button className="btn --primary" onClick={this.unenroll}>
+                            {I18n.t('commons.unenroll')}
+                        </button>
                     </div>
                     <Confirmation 
-                        title={'Unenroll device #' + this.props.id } 
-                        message={'You are going to unenroll device ' + this.props.id} 
+                        title={`${I18n.t('devices.danger_zone.unenroll_device')} # ${this.props.id}`} 
+                        message={`${I18n.t('devices.danger_zone.going_to_unenroll')} ${this.props.id}`} 
                         reference={el => this.unenrollmentDevice = el} 
                     /> 
                 </div>
 
                 <div className="listElement">
                     <div className="message">
-                        Delete Device
-                        <div className="detail">Once you delete a device, there is no going back. Please be certain</div>
+                        {I18n.t('devices.danger_zone.delete')}
+                        <div className="detail">
+                            {I18n.t('devices.danger_zone.delete_description')}
+                        </div>
                     </div>
                     <div className="controller">
-                        <button className="btn --primary" onClick={this.delete}>Delete</button>
+                        <button className="btn --primary" onClick={this.delete}>
+                            {I18n.t('commons.delete')}
+                        </button>
                     </div>
                     <Confirmation 
-                        title={'Delete device #' + this.props.id } 
-                        message={'You are going to delete device ' + this.props.id} 
+                        title={`${I18n.t('devices.danger_zone.delete')} # ${this.props.id}`} 
+                        message={`${I18n.t('devices.danger_zone.delete_message')} ${this.props.id}`} 
                         reference={el => this.deleteDevice = el} 
                     /> 
                 </div>
