62a2c029e3dbf83bea75d25a0e001137501c5f41
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-29 16:15:44 -0400

    feat(dashboard): add routes to i18n
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/applications/AdminDashboard/routes.js
+++ src/applications/AdminDashboard/routes.js
@@ -8,65 +8,66 @@ import SearchEngine from '../../containers/SearchEngine'
 import About from '../../containers/About'
 import Settings from '../../containers/Settings'
 import Fleets from '../../containers/Fleets'
+import { I18n } from 'react-i18nify'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: I18n.t('commons.home'),
     component: Dashboard,
     exact: true
   },
   {
     path: '/devices',
-    name: 'Devices',
+    name: I18n.t('commons.devices'),
     component: Devices,
     exact: false
   },
   {
     path: '/invitations',
-    name: 'Invitations',
+    name: I18n.t('commons.invitations'),
     component: Invitations,
     exact: false
   },
   {
     path: '/files',
-    name: 'Files',
+    name: I18n.t('commons.files'),
     component: Files,
     exact: false
   },
   {
     path: '/fleets',
-    name: 'Fleets',
+    name: I18n.t('commons.fleets'),
     component: Fleets,
     exact: false
   },
   {
     path: '/applications',
-    name: 'Applications',
+    name: I18n.t('commons.applications'),
     component: Applications,
     exact: false
   },
   {
     path: '/users',
-    name: 'Users',
+    name: I18n.t('commons.users'),
     component: Users,
     exact: false
   },
   {
     path: '/search',
-    name: 'Search',
+    name: I18n.t('commons.search'),
     component: SearchEngine,
     exact: false
   },
   {
     path: '/about',
-    name: 'About',
+    name: I18n.t('commons.about'),
     component: About,
     exact: false
   },
   {
     path: '/Settings',
-    name: 'Settings',
+    name: I18n.t('commons.settings'),
     component: Settings,
     exact: false
   }
