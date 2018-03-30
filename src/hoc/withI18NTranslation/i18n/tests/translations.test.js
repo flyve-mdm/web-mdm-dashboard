c9af5da36d421e5fb050b38b9ad8de21702c6f96
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-03-29 20:10:32 -0400

    feat(i18n): create test of login
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/hoc/withI18NTranslation/i18n/tests/translations.test.js
+++ src/hoc/withI18NTranslation/i18n/tests/translations.test.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { I18n, Translate } from 'react-i18nify';
-import source_file from '../source_file.json';
+import { I18n } from 'react-i18nify'
+import source_file from '../source_file.json'
 import {
   TRANSLATIONS_AVAILABLES,
   LANGUAGE_DEFAULT,
@@ -8,85 +7,27 @@ import {
   LANGUAGE_NAMES
 } from './constants'
 
-describe('Check if translations are available', () => {;
+describe('Check if translations are available', () => {
   beforeAll(()=> {
-    let count = 1
     let json  = {} 
 
-    json[LANGUAGE_DEFAULT] = source_file;
+    json[LANGUAGE_DEFAULT] = source_file
 
-    I18n.setTranslations(json);
-    I18n.setLocale(LANGUAGE_DEFAULT); 
+    I18n.setTranslations(json)
+    I18n.setLocale(LANGUAGE_DEFAULT)
     
-    // Useless meanwhile no exists translations
-    // for (let i in TRANSLATIONS_AVAILABLES) {
-    //   import(`../${TRANSLATION_FOLDER}/${TRANSLATIONS_AVAILABLES[i]}.json`).then(jsonModule => {
-    //     count++;
-    //     json[TRANSLATIONS_AVAILABLES[i]] = jsonModule;
-    //     if (count === LANGUAGE_NAMES.length) {
-    //       I18n.setTranslations(json)
-    //     }
-    //   });
-    // }
   })
 
   it('login page should have english translations', () => {
+    expect(I18n.t('login.title')).toBe('Sign in')
+    expect(I18n.t('login.username_not_registered')).toBe('The username entered is not registered. Try a different account or')
+    expect(I18n.t('login.create_an_new')).toBe('create an new')
     expect(I18n.t('login.use_your_account')).toBe('Use your Flyve MDM account')
     expect(I18n.t('login.what_is_this')).toBe('What\'s this?')
-    expect(I18n.t('login.username')).toBe('Username')
-    expect(I18n.t('login.next')).toBe('Next')
     expect(I18n.t('login.no_account')).toBe('No account?')
-    expect(I18n.t('login.')).toBe('Create one!')
+    expect(I18n.t('login.create_one')).toBe('Create one!')
     expect(I18n.t('login.enter_password')).toBe('Enter password')
     expect(I18n.t('login.enter_password_for')).toBe('Enter the password for')
     expect(I18n.t('login.forgot_my_password')).toBe('Forgot my password')
-  })
-
-  it('create account page should have english translations', () => {
-    expect(I18n.t('create_account.title')).toBe('Create account')
-    expect(I18n.t('create_account.enter_code_image')).toBe('Enter the code from the image')
-    expect(I18n.t('create_account.already_have_account')).toBe('Do you already have an account?')
-    expect(I18n.t('create_account.full_name')).toBe('Full name')
-    expect(I18n.t('commons.password_confirmation')).toBe('Password (confirmation)')
-    expect(I18n.t('create_account.register')).toBe('Register')
-  });
-
-  it('logout modal should have english translations', () => {
-    expect(I18n.t('logout.close_session')).toBe('Close session')
-  })
-
-  it('about page should have english translations', () => {
-    expect(I18n.t('about.overview.title')).toBe('Overview')
-    expect(I18n.t('about.system_information.title')).toBe('System information')
-    expect(I18n.t('about.help_center.title')).toBe('Help Center')
-    expect(I18n.t('about.contact.title')).toBe('Contact')
-    expect(I18n.t('about.release_notes.title')).toBe('Release notes')
-    expect(I18n.t('about.term_of_use.title')).toBe('Term of use')
-    expect(I18n.t('about.license.title')).toBe('License')
-  })
-
-  it('The common words are in the commons group', () => {
-    expect(I18n.t('commons.email')).toBe('Email')
-    expect(I18n.t('commons.name')).toBe('Name')
-    expect(I18n.t('commons.password')).toBe('Password')
-    expect(I18n.t('commons.back')).toBe('Back')
-    expect(I18n.t('commons.sign_in')).toBe('Sign in')
-    expect(I18n.t('commons.terms_and_conditions')).toBe('Terms and Conditions')
-    expect(I18n.t('commons.devices')).toBe('Devices')
-    expect(I18n.t('commons.applications')).toBe('Applications')
-    expect(I18n.t('commons.files')).toBe('Files')
-    expect(I18n.t('commons.fleets')).toBe('Fleets')
-    expect(I18n.t('commons.invitations')).toBe('Invitations')
-    expect(I18n.t('commons.users')).toBe('Users')
-    expect(I18n.t('commons.devices_by_plataform')).toBe('Devices by plataform')
-    expect(I18n.t('commons.pending_invitations')).toBe('Pending invitations')
-    expect(I18n.t('commons.dashboard')).toBe('Dashboard')
-    expect(I18n.t('commons.ok')).toBe('Ok')
-    expect(I18n.t('commons.cancel')).toBe('Cancel')
-    expect(I18n.t('commons.no_selection')).toBe('No Selection')
-    expect(I18n.t('commons.map')).toBe('Map')
-    expect(I18n.t('commons.call')).toBe('Call')
-    expect(I18n.t('commons.send')).toBe('Send')
-    expect(I18n.t('commons.sending')).toBe('Sending')
   })
 })
