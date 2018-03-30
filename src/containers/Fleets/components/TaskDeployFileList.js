d954b73c20ceffca0a1dd543cac1ce81adca6e01
Hector Rondon <hrondon@teclib.com>
2018-03-29 20:17:07 -0400

    feat(fleets): create render component files deploy
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ src/containers/Fleets/components/TaskDeployFileList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+class TasksDeployFileList extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderElements: undefined
+        }
+    }
+
+    componentDidMount() {
+        this.refreshRender()
+    }
+
+    refreshRender = () => {
+        return (
+            Array.isArray(this.props.data) ?
+                this.props.data.map(item => {
+                    return this.props.typeData.map((value, index) => {
+                        return item['items_id'] === value['id'] ?
+                            <div className='files-list' style={{ width: '320px' }} key={value['id']}>
+                                <div className='files-list-content'>
+                                    <div className='files-list-item'>
+                                        <div className='item-content-primary'>
+                                            <div className='content-text-primary'>{value['alias']}</div>
+                                            <div className='content-text-secondary'>{value['package_name']}</div>
+                                        </div>
+                                        <div className='item-content-secondary'>
+                                            <div className='icon item-icon'>
+                                                <span className='deleteIcon' style={{ fontSize: '18px' }} onClick={this.handleRemove.bind(this, item)}></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            : null
+                    })
+                })
+                : null
+        )
+    }
+
+    handleRemove = (task) => {
+        this.props.removeTask(task)
+    }
+
+    render() {
+        return this.refreshRender()
+
+    }
+}
+
+export default TasksDeployFileList
