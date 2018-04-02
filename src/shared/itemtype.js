eedc588c8b7492ae6621f26e620468045a713fca
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-02 16:06:30 -0400

    refactor(dashboard): create a file with all item types
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

---
+++ src/shared/itemtype.js
@@ -0,0 +1,9 @@
+import { itemtype } from '@glpi-project/javascript-library-glpi'
+
+export default {
+    ...itemtype,
+    PluginFlyvemdmAgent: "PluginFlyvemdmAgent",
+    PluginFlyvemdmFile: "PluginFlyvemdmFile",
+    PluginFlyvemdmInvitation: "PluginFlyvemdmInvitation",
+    PluginFlyvemdmPackage: "PluginFlyvemdmPackage"
+}
