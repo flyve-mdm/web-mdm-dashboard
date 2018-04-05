f84f9dbc01fe410bfb15d0162986abea50d6d2ce
Hector Rondon <hrondon@teclib.com>
2018-04-05 17:02:36 -0400

    refactor(routes): fix get ID from path
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/shared/getID.js
+++ src/shared/getID.js
@@ -1,7 +1,8 @@
+import location from './location'
 export default (path, position = 3) => {
     let id
-    if (process.env.PUBLIC_URL !== "") {
-        id = path.split(process.env.PUBLIC_URL)[1].split("/")[position]
+    if (location.pathname !== "") {
+        id = path.split(location.pathname)[1].split("/")[position]
     } else {
         id = path.split("/")[position]
     }
