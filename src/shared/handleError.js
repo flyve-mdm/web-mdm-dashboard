d57fe04523d3f6cbdee8c9d23ddf266b1667e930
Hector Rondon <hrondon@teclib.com>
2018-04-09 10:30:35 -0400

    refactor(errors): handle error code status
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/shared/handleError.js
+++ src/shared/handleError.js
@@ -1,9 +1,30 @@
-export default ({notification, type='info', error}) => {
-    console.log(error)
-    console.log(type)
-    notification({
-        title: error.data[0][0],
-        body: error.data[0][1],
-        type: type
-    })
+import config from '../config/config.json'
+
+export default ({type='info', error}) => {
+    let message = {
+        type: type,
+        title: config.appName,
+        body: error.statusText
+    }
+    switch (true) {
+        case (error.status === 0):
+            message.title = error.data[0][0]
+            message.body = 'No Internet Connection'
+            break;
+        case (error.status === 401):
+            message.title = error.data[0][0]
+            message.body = error.data[0][1] !== '' ? error.data[0][1] : error.statusText
+            break;
+        case (error.status === 404):
+            message.title = error.data[0][0]
+            message.body = error.data[0][1] !== '' ? error.data[0][1] : error.statusText
+            break;
+        case (error.status >= 400 && error.status < 500 && error.status !== 401):
+            message.title = error.data[0]
+            message.body = error.data[1][0].message ? error.data[0][1] : error.statusText
+            break;
+        default:
+            break;
+    }
+    return message
 }
