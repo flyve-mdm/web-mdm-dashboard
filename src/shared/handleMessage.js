ef52c1ca9d07ca142635ce60913537c42362c047
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-09 16:50:54 -0400

    refactor(dashboard): validate messages
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/handleMessage.js
+++ src/shared/handleMessage.js
@@ -1,30 +1,32 @@
 import config from '../config/config.json'
 
-export default ({type='info', error}) => {
-    let message = {
+export default ({type='info', message}) => {
+    let response = {
         type: type,
         title: config.appName,
-        body: error.statusText
+        body: message ? message.statusText : ''
     }
-    switch (true) {
-        case (error.status === 0):
-            message.title = error.data[0][0]
-            message.body = 'No Internet Connection'
-            break
-        case (error.status === 401):
-            message.title = error.data[0][0]
-            message.body = error.data[0][1] !== '' ? error.data[0][1] : error.statusText
-            break
-        case (error.status === 404):
-            message.title = error.data[0][0]
-            message.body = error.data[0][1] !== '' ? error.data[0][1] : error.statusText
-            break
-        case (error.status >= 400 && error.status < 500 && error.status !== 401):
-            message.title = error.data[0]
-            message.body = error.data[0][0].message ? error.data[0][1] : error.statusText
-            break
-        default:
-            break
+    if (message) {
+        switch (true) {
+            case (message.status === 0):
+                response.title = message.data[0][0]
+                response.body = 'No Internet Connection'
+                break
+            case (message.status === 401):
+                response.title = message.data[0][0]
+                response.body = message.data[0][1] !== '' ? message.data[0][1] : message.statusText
+                break
+            case (message.status === 404):
+                response.title = message.data[0][0]
+                response.body = message.data[0][1] !== '' ? message.data[0][1] : message.statusText
+                break
+            case (message.status >= 400 && message.status < 500 && message.status !== 401):
+                response.title = message.data[0]
+                response.body = message.data[0][1] ? message.data[0][1] : message.statusText
+                break
+            default:
+                break
+        }
     }
-    return message
+    return response
 }
