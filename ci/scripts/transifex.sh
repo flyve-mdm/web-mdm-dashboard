c94fb79fdd000344387e5da05e443c9b2e79a74f
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-13 13:19:48 -0400

    ci(i18n): automatic push of the source file
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/transifex.sh
+++ ci/scripts/transifex.sh
@@ -3,3 +3,4 @@
 sudo pip install transifex-client
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
 tx pull -a
+tx push -s
