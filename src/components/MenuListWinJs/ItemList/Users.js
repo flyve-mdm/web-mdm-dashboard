869bf06c0ca262baed2dea5968014c98afbe9e56
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-20 11:56:02 -0400

    feat(dashboard): request miniature images in the user list
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/MenuListWinJs/ItemList/Users.js
+++ src/components/MenuListWinJs/ItemList/Users.js
@@ -6,7 +6,11 @@ export default class UsersItemList extends Component {
         const imageProfile = this.props.itemList['User.picture'] ? this.props.itemList['User.picture'] : "profile.png"
         return (
             <div>
-                <IconItemList image={imageProfile} size={42} />
+                <IconItemList 
+                    image={imageProfile} 
+                    size={42} 
+                    isMin 
+                />
                 <div style={{ display: 'inline-block' }}>
                     <div className="name">{this.props.itemList['User.name']}</div>
                     <div className="detail">{this.props.itemList['User.realname']}</div>
