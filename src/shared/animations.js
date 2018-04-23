53bccda033c019d144f9cb9f60f86ff016a4798d
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-23 15:09:11 -0400

    feat(animations): disable animations by configuration
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/animations.js
+++ src/shared/animations.js
@@ -1,17 +1,21 @@
 import WinJS from 'winjs'
 import { AnimateFrom } from '@microsoft/fast-animation'
 
-const display = localStorage.getItem('display') ? JSON.parse(localStorage.getItem('display')) : {}
+const animate = () => localStorage.getItem('display') ? JSON.parse(localStorage.getItem('display')).animations : {}
 
-export default function (animate = display.animate) {
-    if (animate) {
+export default function (isAnimate = animate()) {
+    if (isAnimate) {
         WinJS.UI.enableAnimations()
     } else {
         WinJS.UI.disableAnimations()
     }
 }
 
-const slideTop = element => new AnimateFrom(element, { y: 20 }, { duration: 100 })
+const slideTop = (element) => {
+    const animation = new AnimateFrom(element, { y: 20 }, { duration: 100 })
+    if (!animate()) animation.play = () => {}
+    return animation
+}
 
 export {
     slideTop
