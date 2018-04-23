fa4a89a490bc001a002e8f5256ae4387f87d5218
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-23 14:38:27 -0400

    feat(animations): change animation of 'ContentPane'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/ContentPane/index.js
+++ src/components/ContentPane/index.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react'
+import { slideTop } from '../../shared/animations'
 
 class ContentPane extends Component {
 
-  constructor(props) {
-    super(props)
-    const display = localStorage.getItem('display') ? JSON.parse(localStorage.getItem('display')) : {}
-    this.state = {
-      updateAnimation: display.animations,
-      animate: display.animations ? "content-pane--animate": ""
-    }
-  }
-
   componentDidMount() {
-    this.handleAnimation()
-  }
-
-  handleAnimation = () => {
-    if (this.state.updateAnimation) {
-      this.setState({
-        animate: "content-pane--animate"
-      }, () => {
-        setTimeout(() => {
-          this.setState({
-            animate: ""
-          })
-        }, 2000)
-      })
-    }
+    slideTop(this.pane).play()
   }
 
   render() {
     return (
-      <div className={`content-pane ${this.state.animate}`}>
+      <div className="content-pane" ref={pane => this.pane = pane}>
         <div id="content-pane-block" className="content-pane-block">
         { this.props.children }
         </div>
