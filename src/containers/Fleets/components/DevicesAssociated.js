d8f3fb57ed6535075ac01292b87bb629b16390ed
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-26 13:58:28 -0400

    feat(fleets): create a list of associated devices
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/Fleets/components/DevicesAssociated.js
+++ src/containers/Fleets/components/DevicesAssociated.js
@@ -7,6 +7,8 @@ import withGLPI from '../../../hoc/withGLPI'
 import itemtype from '../../../shared/itemtype'
 import getID from '../../../shared/getID'
 import EmptyMessage from '../../../components/EmptyMessage'
+import WinJS from 'winjs'
+import ReactWinJS from 'react-winjs'
 
 class DevicesAssociated extends Component {
 
@@ -14,7 +16,10 @@ class DevicesAssociated extends Component {
         super(props)
         this.state = {
             devices: undefined,
-            isLoading: true
+            isLoading: true,
+            name: undefined,
+            layout: { type: WinJS.UI.ListLayout },
+            itemList: new WinJS.Binding.List([])
         }
     }
 
@@ -24,6 +29,10 @@ class DevicesAssociated extends Component {
             
             const devices = await this.props.glpi.searchItems({
                 itemtype: itemtype.PluginFlyvemdmAgent,
+                options: {
+                    uid_cols: true, 
+                    forcedisplay: [2, 3]
+                },
                 criteria: 
                 [
                     {
@@ -35,8 +44,10 @@ class DevicesAssociated extends Component {
                 ]
             })
             this.setState({ 
+                name,
                 devices,
-                isLoading: false
+                isLoading: false,
+                itemList: new WinJS.Binding.List(devices.data)
             })
         } catch (error) {
             this.setState({
@@ -45,9 +56,25 @@ class DevicesAssociated extends Component {
         }
     }
 
+    headerComponent = (
+        <React.Fragment>
+            <span className="id">#</span>
+            <span className="name">{I18n.t('commons.name')}</span>
+        </React.Fragment>
+    )
+
+    ItemListRenderer = ReactWinJS.reactRenderer((ItemList) => {
+        return (
+            <React.Fragment>
+                <span className="id">{ItemList.data[`${itemtype.PluginFlyvemdmAgent}.id`]}</span>
+                <span className="name">{ItemList.data[`${itemtype.PluginFlyvemdmAgent}.name`]}</span>
+            </React.Fragment>
+        )
+    })
+
     render () {
         return (
-            <ContentPane>
+            <ContentPane className="fleets">
                 {
                     this.state.isLoading ? 
                         <Loading message={`${I18n.t('commons.loading')}...`} /> :
@@ -55,7 +82,20 @@ class DevicesAssociated extends Component {
                             (!this.state.devices || this.state.devices.totalcount === 0) ? 
                                 <EmptyMessage message={I18n.t('fleets.no_associated_devices')}/> :
                                 (
-                                    ''
+                                    <React.Fragment>
+                                        <h2>{I18n.t('fleets.devices_of')} '{this.state.name}'</h2>
+                                        <div className="listPane">
+                                            <ReactWinJS.ListView
+                                                ref={(listView) => { this.listView = listView }}
+                                                className="contentListView win-selectionstylefilled"
+                                                itemDataSource={this.state.itemList.dataSource}
+                                                itemTemplate={this.ItemListRenderer}
+                                                headerComponent={this.headerComponent}
+                                                layout={this.state.layout}
+                                                selectionMode={'single'}
+                                            />
+                                        </div>
+                                    </React.Fragment>
                                 )
                         )
                 }
