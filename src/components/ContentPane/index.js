621c48f3d8aecbbba4b561d888694be5f6332d49
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-04-26 13:58:28 -0400

    feat(fleets): create a list of associated devices
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/ContentPane/index.js
+++ src/components/ContentPane/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { slideTop } from '../../shared/animations'
 
 class ContentPane extends Component {
@@ -9,13 +10,17 @@ class ContentPane extends Component {
 
   render() {
     return (
-      <div className="content-pane" ref={pane => this.pane = pane}>
+      <div className={`content-pane ${this.props.className}`} ref={pane => this.pane = pane}>
         <div id="content-pane-block" className="content-pane-block">
         { this.props.children }
         </div>
       </div>
     )
   }
+}
+
+ContentPane.propTypes = {
+  className: PropTypes.string
 }
 
 export default ContentPane
