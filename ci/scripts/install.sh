291802773be754d2038892f9a6ca2b94163ed8e3
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-03 15:27:51 -0400

    ci(test): validate HTML with 'html5validator'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -2,10 +2,15 @@
 
 # Install jq for json parse
 
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 apt-get -y update
-apt-get -y install jq python-pip zip unzip
+apt-get -y install jq python-pip zip unzip oracle-java8-installer oracle-java8-set-default
 npm rebuild node-sass
 yarn cache clean
 yarn global add jsdoc
 yarn global add gh-pages
 yarn install
+pip install html5validator
