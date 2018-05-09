dbe185cf9d64cfc991c76f2568af2016a6801e57
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-09 16:56:09 -0400

    test(snapshot): configure localStorage and sessionStorage to the jsdom
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { mount, render, shallow, configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon'
 
 configure({ adapter: new Adapter() })
 
@@ -9,6 +10,8 @@ process.on('unhandledRejection', err => {
 })
 
 global.expect = expect
+
+global.sinon = sinon
 
 global.mount = mount
 global.render = render
