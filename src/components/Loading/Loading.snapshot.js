110268b9be59a8951ee7a9795e5b81ec587b9bd3
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-09 14:12:39 -0400

    test(snapshot): add snapshots of the 'Loading' component
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

---
+++ src/components/Loading/Loading.snapshot.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Loading from "./index"
+
+describe('Loading', () => {
+    test('renders loading', () => {
+        const component = renderer.create(
+            <Loading />
+        )
+        let tree = component.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    test('renders small loading', () => {
+        const component = renderer.create(
+            <Loading small />
+        )
+        let tree = component.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
