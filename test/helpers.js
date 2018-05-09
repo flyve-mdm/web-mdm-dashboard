a2e23fea68e0fec551beef72381368213677dff5
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-09 11:21:38 -0400

    test(dashboard): configure enzyme
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,15 @@
 import { expect } from 'chai'
+import { mount, render, shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
 
 process.on('unhandledRejection', err => {
     throw err
 })
 
 global.expect = expect
+
+global.mount = mount
+global.render = render
+global.shallow = shallow
