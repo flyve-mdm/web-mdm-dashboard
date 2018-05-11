d0622b7f90569a6fd0d0ce0d24454a124b4cae05
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-11 11:36:06 -0400

    feat(devices): get software data
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/Devices/components/Sections/Applications/ApplicationDetail.js
+++ src/containers/Devices/components/Sections/Applications/ApplicationDetail.js
@@ -3,13 +3,28 @@ import PropTypes from 'prop-types'
 import itemtype from '../../../../../shared/itemtype'
 import Loading from '../../../../../components/Loading'
 import { I18n } from 'react-i18nify'
+import EmptyMessage from '../../../../../components/EmptyMessage'
 
 export default class Applications extends PureComponent {
 
     constructor(props) {
         super(props)
         this.state = {
-            isLoading: true
+            isLoading: true,
+            software: undefined
+        }
+    }
+
+    componentDidMount = async () => {
+        try {
+            this.setState({
+                software: await this.props.glpi.getAnItem({itemtype: itemtype.Software, id: this.props.id}),
+                isLoading: false
+            })
+        } catch (error) {
+            this.setState({
+                isLoading: false
+            })
         }
     }
 
@@ -17,7 +32,11 @@ export default class Applications extends PureComponent {
         return (
             this.state.isLoading ?
                 <Loading message={`${I18n.t('commons.loading')}...`}/> :
-                ''
+                (
+                    this.state.software ?
+                        "":
+                        <EmptyMessage message={I18n.t('commons.problems_loading_data')}/>
+                )
         )
     }
 }
