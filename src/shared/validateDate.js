1906cc84962a974ee2c544aa8f72e49377551205
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-15 11:36:59 -0400

    fix(devices): validate data of date filter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/validateDate.js
+++ src/shared/validateDate.js
@@ -1,6 +1,10 @@
 export default (value, min, max) => {
-    value.setHours(0)
-    min.setHours(0)
-    max.setHours(1)
-    return (value.getTime() >= min && value.getTime() <= max)
+    if (value instanceof Date && min instanceof Date && max instanceof Date) {
+        value.setHours(0)
+        min.setHours(0)
+        max.setHours(1)
+        return (value.getTime() >= min && value.getTime() <= max)
+    } else {
+        return false
+    }
 }
