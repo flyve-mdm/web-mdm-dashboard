dbdef3c1210d0d9ae20a83a67fa7d39e2a6a35a7
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-18 13:43:23 -0400

    build(dashboard): cover the application with 'ErrorBoundary'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/index.js
+++ src/index.js
@@ -5,31 +5,34 @@ import history from './shared/history'
 import RootApp from './applications/RootApp'
 import { unregister } from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import {
-    createStore,
-    applyMiddleware, 
-    compose
-} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './store'
 import './assets/styles/main.scss' // Global CSS Styles
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
+
+const bugsnagClient = bugsnag('API_KEY')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 
 ReactDOM.render(
     (
-        <Provider store={
-            createStore(rootReducer, (
-                (DevTool) => { // Enable Redux DevTool if are available
-                    return (process.env.NODE_ENV === 'development' && typeof(DevTool) === 'function')
-                        ? DevTool
-                        : compose
-                    })(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)(
-                applyMiddleware(thunk)
-            ))
-        }>
-            <Router history={history}>
-                <RootApp />
-            </Router>
-        </Provider>
+        <ErrorBoundary>
+            <Provider store={
+                createStore(rootReducer, (
+                    (DevTool) => { // Enable Redux DevTool if are available
+                        return (process.env.NODE_ENV === 'development' && typeof(DevTool) === 'function')
+                            ? DevTool
+                            : compose
+                        })(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)(
+                    applyMiddleware(thunk)
+                ))
+            }>
+                <Router history={history}>
+                    <RootApp />
+                </Router>
+            </Provider>
+        </ErrorBoundary>
     ),
     document.getElementById('root') 
 )
