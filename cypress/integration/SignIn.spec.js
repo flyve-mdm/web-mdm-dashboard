149513fdd13362ab6d7fa28db4baea0d3df6049b
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-23 14:22:54 -0400

    test(sign-in): validate successful login
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- cypress/integration/SignIn.spec.js
+++ cypress/integration/SignIn.spec.js
@@ -1,11 +1,13 @@
 describe('SignIn', () => {
   beforeEach(function () {
     cy.server()
+
     cy.route({
       method: 'GET',
       url: 'https://dev.flyve.org/glpi/apirest.php/initSession',
       response: { session_token: "12345678" }
     })
+
     cy.route({
       method: 'GET',
       url: 'https://dev.flyve.org/glpi/apirest.php/search/Plugin/?range=0-0&',
@@ -13,6 +15,7 @@ describe('SignIn', () => {
         totalcount: 1
       }
     })
+
     cy.route({
       method: 'GET',
       url: 'https://dev.flyve.org/glpi/apirest.php/Plugin/?range=0-1&',
@@ -20,13 +23,52 @@ describe('SignIn', () => {
         {"id":1,"directory":"flyvemdmdemo","name":"Flyve MDM Demo","version":"1.0.0-dev","state":1,"author":"<a href='http://www.teclib.com'>Teclib</a>","homepage":"","license":"AGPLv3+"},
       ]
     }).as('getFlyveDemo')
+
     cy.route({
       method: 'GET',
       url: 'https://dev.flyve.org/glpi/apirest.php/killSession',
       response: {}
     })
 
-    localStorage.setItem('display', JSON.stringify({
+    cy.route({
+      method: 'GET',
+      url: 'https://dev.flyve.org/glpi/apirest.php/getFullSession',
+      response: {
+        session: {
+          glpiID: 256
+        }
+      }
+    })
+
+    cy.route({
+      method: 'GET',
+      url: 'https://dev.flyve.org/glpi/apirest.php/User/256',
+      response: {}
+    })
+
+    cy.route({
+      method: 'GET',
+      url: 'https://dev.flyve.org/glpi/apirest.php/User/256/UserEmail',
+      response: {}
+    })
+
+    cy.route({
+      method: 'GET',
+      url: 'https://dev.flyve.org/glpi/apirest.php/getGlpiConfig',
+      response: {
+        "cfg_glpi":{
+          password_min_length: 10,
+          password_need_number: 1,
+          password_need_letter: 1,
+          password_need_caps: 1,
+          password_need_symbol: 1,
+          url_base: "https://your-url.com/glpi"
+        }  
+      }
+    })
+
+    localStorage.setItem('display',
+     JSON.stringify({
       applicationsUploaded: false,
       devicesByOperatingSystemVersion: false,
       devicesByUsers: false,
@@ -40,16 +82,16 @@ describe('SignIn', () => {
     }))
   })
 
-  it('should assert that true is equal to true', () => {
+  it('should log in without problems', () => {
     cy.visit('/')
     cy.wait('@getFlyveDemo')
-    cy.get('a[href="/signUp"]')
+    cy.get('a[href="/signUp"]').should('be.visible') 
     cy.get('.win-textbox')
       .type('example name')
       .type('{enter}')
     cy.get('.win-textbox')
       .type('12345678')
       .type('{enter}')
-    expect(true).to.equal(true)
+    cy.get('.empty-message').should('be.visible') 
   })
 })
