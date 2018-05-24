75cbbcf84af4913935cb7e909587630a0c48b22d
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-24 12:46:53 -0400

    refactor(dashboard): simplify 'Loading' component
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,46 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Loading = props => {
-    const headerSize = props.headerSize ? props.headerSize : 0
-
+    const loader = (
+        <div className='loader'>
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+        </div>  
+    )
     const loadComponent = props.small ? (
-        <div
-            className="loading"
-            style={{
-                display: 'inline-block',
-                verticalAlign: 'middle',
-                margin: '10px'
-            }}
-        >
-            <div>
-                <div className='small-loader'>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                </div>   
-            </div>
+        <div className="loading loading--small" style={props.style}>
+            <div> { loader } </div>
         </div>
     ) : (
-        <div className="loading" style={{
-                marginTop: - headerSize,
-                display: 'flex',
-                height: '100%',
-                width: '100%',
-                justifyContent: 'center',
-                textAlign: 'center',
-                alignItems: 'center',
-                flexDirection: 'column'
-        }}>
+        <div className="loading" style={{marginTop: - props.headerSize, ...props.style}}>
             <div>
-                <div className='loader'>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                    <div className='circle'></div>
-                </div>   
+                { loader }
                 <p>{props.message}</p>
             </div>
         </div>
@@ -50,10 +27,18 @@ const Loading = props => {
     return loadComponent
 }
 
+Loading.defaultProps = {
+    style: {},
+    small: false,
+    headerSize: 0,
+    message: ''
+}
+
 Loading.propTypes = {
     message: PropTypes.string,
     headerSize: PropTypes.number,
-    small: PropTypes.bool
+    small: PropTypes.bool,
+    style: PropTypes.object
 }
 
 export default Loading
