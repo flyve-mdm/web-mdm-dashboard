b9a0485baabb17246758e18a6b3d84813195e7c2
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-24 12:55:09 -0400

    test(dashboard): update the 'Loading' component tests
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/Loading/__tests__/Loading.spec.js
+++ src/components/Loading/__tests__/Loading.spec.js
@@ -4,10 +4,16 @@ import Loading from "../index"
 describe('Loading', () => {
     it('renders the loader', () => {
         const wrapper = shallow(<Loading />)
+        expect(wrapper.find('.loading')).toHaveLength(1)
+        expect(wrapper.find('.loading--small')).toHaveLength(0)
         expect(wrapper.find('.loader')).toHaveLength(1)
+        expect(wrapper.find('.circle')).toHaveLength(5)
     })
     it('renders the small loader', () => {
         const wrapper = shallow(<Loading small />)
-        expect(wrapper.find('.small-loader')).toHaveLength(1)
+        expect(wrapper.find('.loading')).toHaveLength(1)
+        expect(wrapper.find('.loading--small')).toHaveLength(1)
+        expect(wrapper.find('.loader')).toHaveLength(1)
+        expect(wrapper.find('.circle')).toHaveLength(5)
     })
 })
