786a465ed1b767d904ab815bee5ffafc490309c8
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-24 14:49:38 -0400

    refactor(dashboard): simplify the 'withGLPI' hoc
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/hoc/withGLPI/index.js
+++ src/hoc/withGLPI/index.js
@@ -7,34 +7,13 @@ import glpi from '../../shared/glpiApi'
  * @param { React Component } WrappedComponent 
  * @param { Object with method and parameters strings} fetch 
  */
-const withGLPI = (WrappedComponent, fetch) => {
+const withGLPI = (WrappedComponent) => {
   class GLPIClient extends PureComponent {
-    constructor (props) {
-      super(props);
-
-      this.state = {
-          data: null
-      }
-    }
-
-    componentDidMount() {
-        if (typeof(fetch) !== 'object') {
-
-        }
-    }
-
     render () { 
-      return <WrappedComponent {...this.props} /> 
+      return <WrappedComponent glpi={glpi} {...this.props} /> 
     }
   }
-
-  GLPIClient.defaultProps = {
-    glpi: glpi
-  }
-
   return withRouter(GLPIClient)
 }
-
-
 
 export default withGLPI
