41015b43bf33a443b50f6c6db70639a844354072
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-25 17:47:17 -0400

    test(glpiApi): should exist all methods of 'javascript-library-glpi'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/glpiApi/index.js
+++ src/shared/glpiApi/index.js
@@ -1,5 +1,5 @@
 import Glpi from '@glpi-project/javascript-library-glpi'
-import appConfig from '../../public/config.json'
+import appConfig from '../../../public/config.json'
 
 let glpi = new Glpi({ url: appConfig.glpiApiLink })
 glpi.sessionToken = localStorage.getItem('sessionToken')
