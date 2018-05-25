2f9c0d9a0e2b02b707bc5414fed78f92fc83f06c
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-25 16:04:15 -0400

    test(getMode): should return 'large'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/calc100PercentMinus/index.js
+++ src/shared/calc100PercentMinus/index.js
@@ -1,5 +1,4 @@
 export default function calc100PercentMinus(n) {
-    console.log(n)
     return n === '0' ?
         '100%' :
         `calc(100% - ${n}px)`
--- src/shared/changeState.js
+++
@@ -1,7 +0,0 @@
-const changeState = (ctx, event) => {
-  ctx.setState({
-      [event.target.name]: event.target.value
-  })
-}
-
-export default changeState
