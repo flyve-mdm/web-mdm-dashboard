776778aef59ce90ae01a9a45e49b45053dc4623d
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-25 17:10:20 -0400

    test(animations): should the 'slideLeft' animation return something
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/animations/__test__/itemtype.spec.js
+++ src/shared/animations/__test__/itemtype.spec.js
@@ -4,4 +4,8 @@ describe('animations', () => {
   it('should the "slideTop" animation return something', () => {
     expect(slideTop(document.createElement("div"))).toBeDefined()
   })
+
+  it('should the "slideLeft" animation return something', () => {
+    expect(slideLeft(document.createElement("div"))).toBeDefined()
+  })
 })
