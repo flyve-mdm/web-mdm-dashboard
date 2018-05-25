cad76b6e7319995c80d72bf6003423c5c8318726
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-25 10:50:53 -0400

    test(bytesToSize): should return '1 MB'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/bytesToSize/__test__/bytesToSize.spec.js
+++ src/shared/bytesToSize/__test__/bytesToSize.spec.js
@@ -8,4 +8,8 @@ describe('bytesToSize', () => {
   it('should return "1 Bytes"', () => {
     expect(bytesToSize(1)).toEqual('1 Bytes')
   })
+
+  it('should return "1 MB"', () => {
+    expect(bytesToSize(1048576)).toEqual('1 MB')
+  })
 })
