11a1c74710ed435034d6a0f35371bbeb3319639e
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-25 10:18:08 -0400

    test(authtype): should return 'mail_server'
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/authtype/__test__/authtype.spec.js
+++ src/shared/authtype/__test__/authtype.spec.js
@@ -4,4 +4,8 @@ describe('authtype', () => {
   it('should return "glpi_internal_database"', () => {
     expect(authtype(1)).toEqual('glpi_internal_database')
   })
+
+  it('should return "mail_server"', () => {
+    expect(authtype(2)).toEqual('mail_server')
+  })
 })
