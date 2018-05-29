be31613fce9f3c1a430a602b38cff17aa7cf5a22
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-29 18:11:22 -0400

    ci(deploy): delete unnecessary characters in the yarn scripts
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -11,7 +11,7 @@ git add docs -f
 # Create commit, NOTICE: this commit is not sent
 git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
 # Copy documentation to gh-pages branch
-yarn gh-pages-docs -- -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
+yarn gh-pages-docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
 # Genarate coverage
 yarn coverage
 # Add docs folder
@@ -19,11 +19,11 @@ git add coverage -f
 # Create commit, NOTICE: this commit is not sent
 git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
 # Copy code coverage to gh-pages branch
-yarn gh-pages-coverage -- -m "ci(docs): generate code coverage for version ${GIT_TAG}"
+yarn gh-pages-coverage -m "ci(docs): generate code coverage for version ${GIT_TAG}"
 
 GITHUB_COMMIT_MESSAGE=$(git log --format=oneline -n 1 ${CIRCLE_SHA1})
 
 if [[ $GITHUB_COMMIT_MESSAGE != *"ci(stats): generate stats.json for version"* ]]; then
     git add reports -f
-    yarn gh-pages-reports -- -m "ci(stats): generate stats.json for version ${GIT_TAG}"
+    yarn gh-pages-reports -m "ci(stats): generate stats.json for version ${GIT_TAG}"
 fi
--- ci/scripts/deploy_master.sh
+++ ci/scripts/deploy_master.sh
@@ -5,15 +5,15 @@ GITHUB_COMMIT_MESSAGE=$(git log --format=oneline -n 1 ${CIRCLE_SHA1})
 if [[ $GITHUB_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* ]]; then
 
     # Generate CHANGELOG.md and increment version
-    yarn release -- -m "ci(release): generate CHANGELOG.md for version %s"
+    yarn release -m "ci(release): generate CHANGELOG.md for version %s"
     # Get version number from package.json
     export GIT_TAG=$(jq -r ".version" package.json)
     # Copy CHANGELOG.md to gh-pages branch
-    yarn gh-pages-changelog -- -m "ci(docs): generate CHANGELOG.md for version ${GIT_TAG}"
+    yarn gh-pages-changelog -m "ci(docs): generate CHANGELOG.md for version ${GIT_TAG}"
     # Push commits and tags to origin branch
     git push --follow-tags origin $CIRCLE_BRANCH
     # Create release with conventional-github-releaser
-    yarn conventional-github-releaser -- -p angular -t $GITHUB_TOKEN
+    yarn conventional-github-releaser -p angular -t $GITHUB_TOKEN
 
     # Upload build file to release
     yarn github-release upload \
@@ -26,5 +26,5 @@ fi
 
 if [[ $GITHUB_COMMIT_MESSAGE != *"ci(stats): generate stats.json for version"* ]]; then
     git add reports -f
-    yarn gh-pages-reports -- -m "ci(stats): generate stats.json for version ${GIT_TAG}"
+    yarn gh-pages-reports -m "ci(stats): generate stats.json for version ${GIT_TAG}"
 fi
