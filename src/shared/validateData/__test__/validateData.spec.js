a55eb11682a595ad5e59fab3a033f616859a9279
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-30 14:47:35 -0400

    test(validateData): validate arrays
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/validateData/__test__/validateData.spec.js
+++ src/shared/validateData/__test__/validateData.spec.js
@@ -3,6 +3,7 @@ import validateData from '../index.js'
 describe('validateData', () => {
     it('should validate correct data', () => {
         expect(validateData('example test')).toEqual('example test')
+        expect(validateData(['text 1', 'text 2'])).toEqual(['text 1', 'text 2'])
     })
     it('should validate undefined data', () => {
         expect(validateData(undefined)).toEqual('')
