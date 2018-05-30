cb0b934055cf45d8f319b00ca6ea7d34dce7a374
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-30 13:39:13 -0400

    test(toDateInputValue): add more test data
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/toDateInputValue/__test__/toDateInputValue.spec.js
+++ src/shared/toDateInputValue/__test__/toDateInputValue.spec.js
@@ -2,6 +2,7 @@ import toDateInputValue from '../index.js'
 
 describe('toDateInputValue', () => {
   it('should convert date correctly', () => {
-     expect(toDateInputValue(new Date('06-10-2018'))).toEqual('2018-06-10')
+    expect(toDateInputValue(new Date('06-10-2018'))).toEqual('2018-06-10')
+    expect(toDateInputValue(new Date('12-01-2018'))).toEqual('2018-12-01')
   })
 })
