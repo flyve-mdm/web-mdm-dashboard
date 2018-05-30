39a29b65ff3fabcaa86e9f3a1d4843613531d2c7
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-30 14:53:34 -0400

    test(validateDate): should validate if a date is incorrect
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/validateDate/__test__/validateDate.spec.js
+++ src/shared/validateDate/__test__/validateDate.spec.js
@@ -7,4 +7,9 @@ describe('validateDate', () => {
     it('should validate if a date is out the range', () => {
         expect(validateDate(new Date('1-3-2018'), new Date('1-4-2018'), new Date('1-10-2018'))).toEqual(false)
     })
+    it('should validate if a date is incorrect', () => {
+        expect(validateDate('1-1-2018', new Date('1-1-2018'), new Date('1-1-2018'))).toEqual(false)
+        expect(validateDate(new Date('1-1-2018'), '1-1-2018', new Date('1-1-2018'))).toEqual(false)
+        expect(validateDate(new Date('1-1-2018'), new Date('1-1-2018'), '1-1-2018')).toEqual(false)
+    })
 })
