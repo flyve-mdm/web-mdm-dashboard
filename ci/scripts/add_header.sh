273027cf5d81fd10e45667ee5c554abbb5a44512
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-05-31 10:20:42 -0400

    ci(coverage): add Jekyll headers to coverage
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/add_header.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-directory='./coverage'
-header='---\
-layout: conatiner\
----\
-'
-find -d $directory -type f -name "*.html" -exec sed -i "1s/^/$header /" "{}" \;
--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -7,7 +7,7 @@ echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | te
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 apt-get -y update
-apt-get -y install jq python-pip zip unzip oracle-java8-installer oracle-java8-set-default wget xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+apt-get -y install jq python-pip zip unzip oracle-java8-installer oracle-java8-set-default wget xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 ruby-full
 npm rebuild node-sass
 yarn cache clean
 yarn global add jsdoc
