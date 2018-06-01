e3d0603484319382dcea6e30d089439b29fd2454
Hector Rondon <hrondon@teclib.com>
2018-06-01 12:57:11 -0400

    docs(components): add code documentation to async components
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/async/asyncValidateAccount.js
+++ src/async/asyncValidateAccount.js
@@ -26,8 +26,14 @@
 * ------------------------------------------------------------------------------
 */
 
+/** import dependencies */
 import withAsyncComponent from '../hoc/withAsyncComponent'
 
+/**
+ * Calls asynchronous ValidateAccount component
+ * @constant
+ * @type {component}
+ */
 const asyncValidateAccount = withAsyncComponent(() => {
   return import('../components/ValidateAccount')
 })
