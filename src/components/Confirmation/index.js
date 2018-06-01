e433e1f3b261b95ca3f438f7d7160bd183facd7e
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-06-01 12:28:51 -0400

    docs(dashboard): create documentation to 'Confirmation' component
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/Confirmation/index.js
+++ src/components/Confirmation/index.js
@@ -1,44 +1,22 @@
-/*
-*   Copyright © 2018 Teclib. All rights reserved.
-*
-*   This file is part of web-mdm-dashboard
-*
-* web-mdm-dashboard is a subproject of Flyve MDM. Flyve MDM is a mobile
-* device management software.
-*
-* Flyve MDM is free software: you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 3
-* of the License, or (at your option) any later version.
-*
-* Flyve MDM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* ------------------------------------------------------------------------------
-* @author     Gianfranco Manganiello (gmanganiello@teclib.com)
-* @author     Hector Rondon (hrondon@teclib.com)
-* @copyright  Copyright © 2018 Teclib. All rights reserved.
-* @license    GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-* @link       https://github.com/flyve-mdm/web-mdm-dashboard
-* @link       http://flyve.org/web-mdm-dashboard
-* @link       https://flyve-mdm.com
-* ------------------------------------------------------------------------------
-*/
-
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import ReactWinJS from 'react-winjs'
 import { I18n } from 'react-i18nify'
 
+/** Component with a dialogue box */
 class Confirmation extends PureComponent {
 
+    /** 
+     * Asynchronous function that waits for a user's response
+     * @return {boolean} User's response
+     */
     static isOK = async (contentDialog) => {
         return await contentDialog.winControl.show().then(({ result }) => {
             return result === "primary"
         })
     }
-
+    
+    /** Render component */
     render () {
         return (
             <ReactWinJS.ContentDialog
