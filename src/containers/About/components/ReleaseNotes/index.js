3166b48d12505d5dfc8a860c771f3fea34d24df9
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-06-05 15:28:09 -0400

    feat(about): get release info from the Github server
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/About/components/ReleaseNotes/index.js
+++ src/containers/About/components/ReleaseNotes/index.js
@@ -26,19 +26,86 @@
 * ------------------------------------------------------------------------------
 */
 
-import React from 'react'
+import React, { PureComponent } from 'react'
 import ReactMarkdown from 'react-markdown'
-import CHANGELOG from './CHANGELOG.md'
 import ContentPane from '../../../../components/ContentPane'
 import { I18n } from "react-i18nify"
+import Loading from "../../../../components/Loading"
+import EmptyMessage from "../../../../components/EmptyMessage"
+import withHandleMessages from '../../../../hoc/withHandleMessages'
+import { uiSetNotification } from '../../../../store/ui/actions'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-const ReleaseNotes = () => (
-  <ContentPane>
-    <h2 style={{ margin: '10px' }}>{I18n.t('about.release_notes.title')}</h2>
-    <div className="about-pane" style={{ margin: '10px' }}>
-      <ReactMarkdown source={CHANGELOG} />
-    </div>
-  </ContentPane>
-)
+function mapDispatchToProps(dispatch) {
+  const actions = {
+      setNotification: bindActionCreators(uiSetNotification, dispatch)
+  }
+  return { actions }
+}
 
-export default ReleaseNotes
+/**
+ * Component to show the release notes
+ * @class ReleaseNotes
+ * @extends PureComponent
+ */
+class ReleaseNotes extends PureComponent {
+  /** @constructor */
+  constructor(props) {
+    super(props)
+    this.state = {
+      release: undefined
+    }
+  }
+
+  /**
+   * Get release notes
+   * @function componentDidMount
+   * @async
+   */
+  componentDidMount = async () => {
+    try {
+      const response = await fetch ('https://raw.githubusercontent.com/flyve-mdm/web-mdm-dashboard/gh-pages/CHANGELOG.md')
+      this.setState({
+        release: await response.text()
+      })
+    } catch (error) {
+      this.props.actions.setNotification(this.props.handleMessage({ type: 'alert', message: error }))
+      this.setState({
+        release: 'no data'
+      })
+    }
+  }
+
+      
+  /** 
+   * Render component 
+   * @function render
+   */
+  render() {
+    let renderComponent 
+    if (this.state.release) {
+      if (this.state.release === 'no data') {
+        renderComponent = <EmptyMessage message={I18n.t('commons.no_data')}/>
+      } else {
+        renderComponent = (
+          <ContentPane>
+            <h2 style={{ margin: '10px' }}>{I18n.t('about.release_notes.title')}</h2>
+            <div className="about-pane" style={{ margin: '10px' }}>
+              <ReactMarkdown source={this.state.release} />
+            </div>
+          </ContentPane>
+        )
+      }
+    } else {
+      renderComponent = <Loading message={`${I18n.t('commons.loading')}...`}/>
+    }
+
+    return renderComponent
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(withHandleMessages(ReleaseNotes))
