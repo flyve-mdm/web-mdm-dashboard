efb94365aa189169aa9e7f71853bf6213bf150d6
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-06-25 17:54:57 -0400

    style(profiles): preparing code for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/Forms/Schemas/Schemas/user.js
+++ src/components/Forms/Schemas/Schemas/user.js
@@ -27,8 +27,8 @@
  */
 
 import {
-  I18n
-} from "react-i18nify"
+  I18n,
+} from 'react-i18nify'
 
 /**
  * User data schema
@@ -42,275 +42,297 @@ import {
  * @return {array}
  */
 export default function ({
-  state,
+  login,
+  realName,
+  firstName,
+  title,
+  location,
+  phone,
+  phone2,
+  validSince,
+  administrativeNumber,
+  passwordConfirmation,
+  created,
+  mobilePhone,
+  password,
+  lastLogin,
+  comments,
+  modified,
+  validUntil,
+  parametersToEvaluate,
+  authentication,
+  defaultEntity,
+  defaultProfile,
+  category,
+  emails,
   changeState,
   changeEmail,
   deleteEmail,
   changeSelect,
-  glpi
+  glpi,
 }) {
   const personalInformation = [
     [{
       label: I18n.t('commons.login'),
-      type: "text",
-      name: "login",
-      value: state.login,
+      type: 'text',
+      name: 'login',
+      value: login,
       placeholder: null,
       function: null,
       disabled: true,
       style: {
-        width: 'auto'
-      }
-    }, ],
-    [{
-        label: I18n.t('commons.realname'),
-        type: "text",
-        name: "realName",
-        value: state.realName,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
+        width: 'auto',
       },
-      {
-        label: I18n.t('commons.first_name'),
-        type: "text",
-        name: "firstName",
-        value: state.firstName,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
-      }
+    }],
+    [{
+      label: I18n.t('commons.realname'),
+      type: 'text',
+      name: 'realName',
+      value: realName,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
+    {
+      label: I18n.t('commons.first_name'),
+      type: 'text',
+      name: 'firstName',
+      value: firstName,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
     ],
     [{
-        label: I18n.t('commons.title'),
-        type: "select",
-        name: "title",
-        value: state.title.value,
-        options: [],
-        function: changeSelect,
-        request: state.title.request,
-        glpi
-      },
-      {
-        label: I18n.t('commons.location'),
-        type: "select",
-        name: "location",
-        value: state.location.value,
-        options: [],
-        function: changeSelect,
-        request: state.location.request,
-        glpi
-      },
-      {
-        label: I18n.t('commons.default_profile'),
-        type: "select",
-        name: "defaultProfile",
-        value: state.defaultProfile.value,
-        options: [],
-        function: changeSelect,
-        request: state.defaultProfile.request,
-        glpi
-      }
-    ]
+      label: I18n.t('commons.title'),
+      type: 'select',
+      name: 'title',
+      value: title.value,
+      options: [],
+      function: changeSelect,
+      request: title.request,
+      glpi,
+    },
+    {
+      label: I18n.t('commons.location'),
+      type: 'select',
+      name: 'location',
+      value: location.value,
+      options: [],
+      function: changeSelect,
+      request: location.request,
+      glpi,
+    },
+    {
+      label: I18n.t('commons.default_profile'),
+      type: 'select',
+      name: 'defaultProfile',
+      value: defaultProfile.value,
+      options: [],
+      function: changeSelect,
+      request: defaultProfile.request,
+      glpi,
+    },
+    ],
   ]
 
   const contactInformation = [
     [{
-        label: I18n.t('commons.phone'),
-        type: "text",
-        name: "phone",
-        value: state.phone,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
-      },
-      {
-        label: I18n.t('commons.mobile_phone'),
-        type: "text",
-        name: "mobilePhone",
-        value: state.mobilePhone,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
-      }
+      label: I18n.t('commons.phone'),
+      type: 'text',
+      name: 'phone',
+      value: phone,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
+    {
+      label: I18n.t('commons.mobile_phone'),
+      type: 'text',
+      name: 'mobilePhone',
+      value: mobilePhone,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
     ],
     [{
-        label: I18n.t('commons.phone_2'),
-        type: "text",
-        name: "phone2",
-        value: state.phone2,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
-      },
-      {
-        label: I18n.t('commons.administrative_number'),
-        type: "text",
-        name: "administrativeNumber",
-        value: state.administrativeNumber,
-        placeholder: null,
-        function: changeState,
-        disabled: false,
-        style: null
-      }
-    ]
+      label: I18n.t('commons.phone_2'),
+      type: 'text',
+      name: 'phone2',
+      value: phone2,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
+    {
+      label: I18n.t('commons.administrative_number'),
+      type: 'text',
+      name: 'administrativeNumber',
+      value: administrativeNumber,
+      placeholder: null,
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
+    ],
   ]
   const passwordInformation = [
     [{
-        label: I18n.t('commons.password'),
-        type: "password",
-        name: "password",
-        value: state.password,
-        placeholder: null,
-        function: changeState,
-        parametersToEvaluate: state.parametersToEvaluate,
-        disabled: false,
-        style: null
-      },
-      {
-        label: I18n.t('commons.password_confirmation'),
-        type: "password",
-        name: "passwordConfirmation",
-        value: state.passwordConfirmation,
-        placeholder: null,
-        parametersToEvaluate: {
-          ...state.parametersToEvaluate,
-          isEqualTo: {
-            value: state.password,
-            message: I18n.t('commons.passwords_not_match')
-          }
+      label: I18n.t('commons.password'),
+      type: 'password',
+      name: 'password',
+      value: password,
+      placeholder: null,
+      function: changeState,
+      parametersToEvaluate,
+      disabled: false,
+      style: null,
+    },
+    {
+      label: I18n.t('commons.password_confirmation'),
+      type: 'password',
+      name: 'passwordConfirmation',
+      value: passwordConfirmation,
+      placeholder: null,
+      parametersToEvaluate: {
+        ...parametersToEvaluate,
+        isEqualTo: {
+          value: password,
+          message: I18n.t('commons.passwords_not_match'),
         },
-        function: changeState,
-        disabled: false,
-        style: null
-      }
-    ]
+      },
+      function: changeState,
+      disabled: false,
+      style: null,
+    },
+    ],
   ]
 
   const activityInformation = [
     [{
-        label: I18n.t('commons.last_login'),
-        type: "text",
-        name: "lastLogin",
-        value: state.lastLogin,
-        placeholder: null,
-        function: changeState,
-        disabled: true,
-        style: {
-          width: '100%'
-        }
+      label: I18n.t('commons.last_login'),
+      type: 'text',
+      name: 'lastLogin',
+      value: lastLogin,
+      placeholder: null,
+      function: changeState,
+      disabled: true,
+      style: {
+        width: '100%',
       },
-      {
-        label: I18n.t('commons.created'),
-        type: "text",
-        name: "created",
-        value: state.created,
-        placeholder: null,
-        function: changeState,
-        disabled: true,
-        style: {
-          width: 'auto'
-        }
+    },
+    {
+      label: I18n.t('commons.created'),
+      type: 'text',
+      name: 'created',
+      value: created,
+      placeholder: null,
+      function: changeState,
+      disabled: true,
+      style: {
+        width: 'auto',
       },
-      {
-        label: I18n.t('commons.modified'),
-        type: "text",
-        name: "modified",
-        value: state.modified,
-        placeholder: null,
-        function: changeState,
-        disabled: true,
-        style: {
-          width: 'auto'
-        }
-      }
-    ]
+    },
+    {
+      label: I18n.t('commons.modified'),
+      type: 'text',
+      name: 'modified',
+      value: modified,
+      placeholder: null,
+      function: changeState,
+      disabled: true,
+      style: {
+        width: 'auto',
+      },
+    },
+    ],
 
   ]
 
   const validDatesInformation = [
     [{
       label: I18n.t('commons.valid_since'),
-      type: "date",
-      name: "validSince",
-      value: state.validSince,
-      function: changeState
+      type: 'date',
+      name: 'validSince',
+      value: validSince,
+      function: changeState,
     }],
     [{
       label: I18n.t('commons.valid_until'),
-      type: "date",
-      name: "validUntil",
-      value: state.validUntil,
-      function: changeState
-    }]
+      type: 'date',
+      name: 'validUntil',
+      value: validUntil,
+      function: changeState,
+    }],
   ]
 
   const moreInformation = [
     [{
       label: I18n.t('commons.authentication'),
-      type: "text",
-      name: "authentication",
-      value: state.authentication,
+      type: 'text',
+      name: 'authentication',
+      value: authentication,
       placeholder: null,
       function: changeState,
       disabled: true,
-      style: null
+      style: null,
     }],
     [{
-        label: I18n.t('commons.category'),
-        type: "select",
-        name: "category",
-        value: state.category.value,
-        options: [],
-        function: changeSelect,
-        request: state.category.request,
-        glpi
-      },
-      {
-        label: I18n.t('commons.default_entity'),
-        type: "select",
-        name: "defaultEntity",
-        value: state.defaultEntity.value,
-        options: [],
-        function: changeSelect,
-        request: state.defaultEntity.request,
-        glpi
-      }
+      label: I18n.t('commons.category'),
+      type: 'select',
+      name: 'category',
+      value: category.value,
+      options: [],
+      function: changeSelect,
+      request: category.request,
+      glpi,
+    },
+    {
+      label: I18n.t('commons.default_entity'),
+      type: 'select',
+      name: 'defaultEntity',
+      value: defaultEntity.value,
+      options: [],
+      function: changeSelect,
+      request: defaultEntity.request,
+      glpi,
+    },
     ],
     [{
       label: I18n.t('commons.comments'),
-      type: "textArea",
-      name: "comments",
-      value: state.comments,
+      type: 'textArea',
+      name: 'comments',
+      value: comments,
       placeholder: null,
       function: changeState,
       disabled: false,
-      style: null
-    }]
+      style: null,
+    }],
   ]
 
   let emailsInformation = [
-    []
+    [],
   ]
 
-  for (let index = 0; index < state.emails.length; index++) {
+  for (let index = 0; index < emails.length; index += 1) {
     emailsInformation = [
       ...emailsInformation, [{
         name: index,
         label: `${I18n.t('commons.email')} ${index + 1}`,
-        type: "email",
-        value: state.emails[index].email,
+        type: 'email',
+        value: emails[index].email,
         placeholder: null,
         function: changeEmail,
         disabled: false,
         style: null,
-        delete: deleteEmail
-      }]
+        delete: deleteEmail,
+      }],
     ]
   }
 
@@ -321,6 +343,6 @@ export default function ({
     activityInformation,
     validDatesInformation,
     moreInformation,
-    emailsInformation
+    emailsInformation,
   }
 }
