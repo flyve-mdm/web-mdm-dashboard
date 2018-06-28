c208f02311d0ab535e63d33ec193f0cf2ab74b5f
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-06-28 18:01:07 -0400

    style(system-report): preparing code for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/containers/Devices/components/Sections/SystemReport.js
+++ src/containers/Devices/components/Sections/SystemReport.js
@@ -28,14 +28,14 @@
 
 /** import dependencies */
 import React, {
-  PureComponent
+  PureComponent,
 } from 'react'
 import PropTypes from 'prop-types'
+import {
+  I18n,
+} from 'react-i18nify'
 import Inventory from './Inventory'
 import Loader from '../../../../components/Loader'
-import {
-  I18n
-} from "react-i18nify"
 import itemtype from '../../../../shared/itemtype'
 import Loading from '../../../../components/Loading'
 
@@ -47,12 +47,32 @@ export default class SystemReport extends PureComponent {
   /** @constructor */
   constructor(props) {
     super(props)
+    const {
+      id,
+      update,
+    } = this.props
+
     this.state = {
-      id: this.props.id,
-      update: this.props.update,
+      id,
+      update,
       data: undefined,
       isLoading: true,
-      requestingInventory: false
+      requestingInventory: false,
+    }
+  }
+
+  componentDidMount() {
+    this.handleRefresh()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {
+      id,
+      update,
+    } = this.state
+
+    if (prevState.id !== id || prevState.update !== update) {
+      this.handleRefresh()
     }
   }
 
@@ -63,23 +83,12 @@ export default class SystemReport extends PureComponent {
         id: nextProps.id,
         update: nextProps.update,
         data: undefined,
-        isLoading: true
-      }
-    } else {
-      return {
-        ...prevState
+        isLoading: true,
       }
     }
-  }
-
-  componentDidUpdate(prevProps, prevState, prevContext) {
-    if (prevState.id !== this.state.id || prevState.update !== this.state.update) {
-      this.handleRefresh()
+    return {
+      ...prevState,
     }
-  }
-
-  componentDidMount() {
-    this.handleRefresh()
   }
 
   /**
@@ -87,23 +96,33 @@ export default class SystemReport extends PureComponent {
    * @function handleRefresh
    */
   handleRefresh = () => {
-    if (this.state.update) {
+    const {
+      update,
+      id,
+    } = this.state
+    const {
+      glpi,
+      setNotification,
+      handleMessage,
+    } = this.props
+
+    if (update) {
       this.setState({
         isLoading: true,
-        requestingInventory: false
+        requestingInventory: false,
       }, async () => {
         try {
           this.setState({
             isLoading: false,
-            data: await this.props.glpi.getAnItem({
+            data: await glpi.getAnItem({
               itemtype: itemtype.PluginFlyvemdmAgent,
-              id: this.state.id
-            })
+              id,
+            }),
           })
         } catch (error) {
-          this.props.setNotification(this.props.handleMessage({
+          setNotification(handleMessage({
             type: 'alert',
-            message: error
+            message: error,
           }))
         }
       })
@@ -115,55 +134,73 @@ export default class SystemReport extends PureComponent {
    * @function requestInventory
    */
   requestInventory = () => {
+    const {
+      glpi,
+      setNotification,
+      handleMessage,
+    } = this.props
+    const { id } = this.state
+
     this.setState({
-      requestingInventory: true
+      requestingInventory: true,
     }, async () => {
       try {
-        const response = await this.props.glpi.genericRequest({
-          path: `${itemtype.PluginFlyvemdmAgent}/${this.state.id}`,
+        const response = await glpi.genericRequest({
+          path: `${itemtype.PluginFlyvemdmAgent}/${id}`,
           requestParams: {
             method: 'PUT',
             body: JSON.stringify({
-              "input": {
-                "_inventory": ""
-              }
-            })
-          }
+              input: {
+                _inventory: '',
+              },
+            }),
+          },
         })
-        this.props.setNotification({
+        setNotification({
           title: I18n.t('commons.success'),
           body: response[0].message,
-          type: 'success'
+          type: 'success',
         })
         this.handleRefresh()
       } catch (error) {
-        this.props.setNotification(this.props.handleMessage({
+        setNotification(handleMessage({
           type: 'alert',
-          message: error
+          message: error,
         }))
         this.setState({
-          requestingInventory: false
+          requestingInventory: false,
         })
       }
     })
   }
 
   render() {
-    if (this.state.isLoading && !this.state.data) {
+    const {
+      isLoading,
+      data,
+      requestingInventory,
+    } = this.state
+    const { glpi } = this.props
+
+    if (isLoading && !data) {
       return (
-        <div style={{padding:'20px'}}>
-          <Loader type="content"/>
+        <div style={{ padding: '20px' }}>
+          <Loader type="content" />
         </div>
       )
-    } else if (!this.state.isLoading && this.state.data) {
+    } if (!isLoading && data) {
       return (
         <div className="devices">
           <div className="system-report">
             <div className="request-inventory">
-              <button className="btn btn--secondary" onClick={this.requestInventory}>
+              <button
+                className="btn btn--secondary"
+                onClick={this.requestInventory}
+                type="button"
+              >
                 {I18n.t('devices.system_report.request_inventory')}
               </button>
-              {this.state.requestingInventory ? <Loading small style={{paddingTop: '6px'}}/> : ""}
+              {requestingInventory ? <Loading small style={{ paddingTop: '6px' }} /> : ''}
             </div>
             <div className="title">
               {I18n.t('commons.agent')}
@@ -172,45 +209,55 @@ export default class SystemReport extends PureComponent {
               <div className="list-col">
                 {I18n.t('commons.id')}
               </div>
-              <div className="list-col">{this.state.data['id']}</div>
+              <div className="list-col">
+                {data.id}
+              </div>
             </div>
             <div className="list-content">
               <div className="list-col">
                 {I18n.t('commons.name')}
               </div>
-              <div className="list-col">{this.state.data['name']}</div>
+              <div className="list-col">
+                {data.name}
+              </div>
             </div>
             <div className="list-content">
               <div className="list-col">
                 {I18n.t('commons.version')}
               </div>
-              <div className="list-col">{this.state.data['version']}</div>
+              <div className="list-col">
+                {data.version}
+              </div>
             </div>
             <div className="list-content">
               <div className="list-col">
                 {I18n.t('commons.last_contact')}
               </div>
-              <div className="list-col">{this.state.data['last_contact']}</div>
+              <div className="list-col">
+                {data.last_contact}
+              </div>
             </div>
             <div className="list-content">
               <div className="list-col">
                 {I18n.t('commons.last_report')}
               </div>
-              <div className="list-col">{this.state.data['last_report'] ? this.state.data['last_report'] : 'N/A'}</div>
+              <div className="list-col">
+                {data.last_report ? data.last_report : 'N/A'}
+              </div>
             </div>
 
             <Inventory
               title={I18n.t('commons.fleet')}
-              itemType='PluginFlyvemdmFleet'
-              itemID={this.state.data['plugin_flyvemdm_fleets_id']}
-              fields={{id: 'ID', name: 'Name'}}
-              glpi={this.props.glpi}
+              itemType="PluginFlyvemdmFleet"
+              itemID={data.plugin_flyvemdm_fleets_id}
+              fields={{ id: 'ID', name: 'Name' }}
+              glpi={glpi}
             />
 
             <Inventory
               title={I18n.t('commons.device')}
-              itemType='Computer'
-              itemID={this.state.data['computers_id']}
+              itemType="Computer"
+              itemID={data.computers_id}
               fields={{
                 id: 'ID',
                 name: 'Name',
@@ -220,7 +267,7 @@ export default class SystemReport extends PureComponent {
                 computermodels_id: 'Model',
                 computertypes_id: 'Type',
                 manufacturers_id: 'Manufacturer',
-                serial: 'Serial'
+                serial: 'Serial',
               }}
               parameters={{
                 expand_dropdowns: true,
@@ -228,16 +275,15 @@ export default class SystemReport extends PureComponent {
                 with_disks: true,
                 with_softwares: true,
                 with_connections: true,
-                with_networkports: true
+                with_networkports: true,
               }}
-              glpi={this.props.glpi}
+              glpi={glpi}
             />
           </div>
         </div>
       )
-    } else {
-      return ""
     }
+    return ''
   }
 }
 /** SystemReport propTypes */
@@ -245,5 +291,6 @@ SystemReport.propTypes = {
   id: PropTypes.string.isRequired,
   glpi: PropTypes.object.isRequired,
   setNotification: PropTypes.func.isRequired,
-  update: PropTypes.bool.isRequired
+  update: PropTypes.bool.isRequired,
+  handleMessage: PropTypes.func.isRequired,
 }
