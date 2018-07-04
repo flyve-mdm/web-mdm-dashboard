aec46abf3004c15e693cf2dafaf910ebc8ab54b2
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-04 11:44:27 -0400

    style(validate-date): preparing for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/validateDate/__test__/validateDate.spec.js
+++ src/shared/validateDate/__test__/validateDate.spec.js
@@ -26,20 +26,20 @@
  * ------------------------------------------------------------------------------
  */
 
-import validateDate from '../index.js'
+import validateDate from '../index'
 
 describe('validateDate', () => {
-    it('should validate if a date is within the range', () => {
-        expect(validateDate(new Date('1-1-2018'), new Date('1-1-2018'), new Date('1-1-2018')))
-          .toEqual(true)
-    })
-    it('should validate if a date is out the range', () => {
-        expect(validateDate(new Date('1-3-2018'), new Date('1-4-2018'), new Date('1-10-2018')))
-          .toEqual(false)
-    })
-    it('should validate if a date is incorrect', () => {
-        expect(validateDate('1-1-2018', new Date('1-1-2018'), new Date('1-1-2018'))).toEqual(false)
-        expect(validateDate(new Date('1-1-2018'), '1-1-2018', new Date('1-1-2018'))).toEqual(false)
-        expect(validateDate(new Date('1-1-2018'), new Date('1-1-2018'), '1-1-2018')).toEqual(false)
-    })
+  it('should validate if a date is within the range', () => {
+    expect(validateDate(new Date('1-1-2018'), new Date('1-1-2018'), new Date('1-1-2018')))
+      .toEqual(true)
+  })
+  it('should validate if a date is out the range', () => {
+    expect(validateDate(new Date('1-3-2018'), new Date('1-4-2018'), new Date('1-10-2018')))
+      .toEqual(false)
+  })
+  it('should validate if a date is incorrect', () => {
+    expect(validateDate('1-1-2018', new Date('1-1-2018'), new Date('1-1-2018'))).toEqual(false)
+    expect(validateDate(new Date('1-1-2018'), '1-1-2018', new Date('1-1-2018'))).toEqual(false)
+    expect(validateDate(new Date('1-1-2018'), new Date('1-1-2018'), '1-1-2018')).toEqual(false)
+  })
 })
