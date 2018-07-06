dd4c7821911b536148c3032bafc599b7bcba66fe
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-06 12:23:01 -0400

    style(schemas): preparing for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/Forms/Schemas/index.js
+++ src/components/Forms/Schemas/index.js
@@ -26,12 +26,12 @@
  * ------------------------------------------------------------------------------
  */
 
-import usersScheme from "./Schemas/user"
-import supervisionScheme from "./Schemas/supervision"
-import agentScheme from "./Schemas/agent"
+import usersScheme from './Schemas/user'
+import supervisionScheme from './Schemas/supervision'
+import agentScheme from './Schemas/agent'
 
 export {
   usersScheme,
   supervisionScheme,
-  agentScheme
+  agentScheme,
 }
