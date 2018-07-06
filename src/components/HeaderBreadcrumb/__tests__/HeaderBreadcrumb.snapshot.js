f4da2ad24a1178d134f370968759ff66c172d016
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-06 12:56:21 -0400

    style(breadcrumb): preparing for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/HeaderBreadcrumb/__tests__/HeaderBreadcrumb.snapshot.js
+++ src/components/HeaderBreadcrumb/__tests__/HeaderBreadcrumb.snapshot.js
@@ -29,23 +29,23 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import {
-  Router
+  Router,
 } from 'react-router-dom'
 import history from '../../../shared/history'
 
-import HeaderBreadcrumb from "../index"
+import HeaderBreadcrumb from '../index'
 
 describe('HeaderBreadcrumb', () => {
   test('renders HeaderBreadcrumb', () => {
     const component = renderer.create(
       <Router history={history}>
         <HeaderBreadcrumb
-          location={{pathname: '/app/devices/309'}}
+          location={{ pathname: '/app/devices/309' }}
           handleToggleExpand={() => {}}
         />
-      </Router>
+      </Router>,
     )
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
--- src/components/HeaderBreadcrumb/__tests__/HeaderBreadcrumb.spec.js
+++ src/components/HeaderBreadcrumb/__tests__/HeaderBreadcrumb.spec.js
@@ -27,16 +27,58 @@
  */
 
 import React from 'react'
-import HeaderBreadcrumb from "../index"
+import HeaderBreadcrumb from '../index'
 
 describe('HeaderBreadcrumb', () => {
   it('should create breadcrumb menu', () => {
     const wrapper = shallow(
       <HeaderBreadcrumb
-        location={{pathname: '/app/devices/309'}}
-        handleToggleExpand={()=>{}}
-      />
+        location={{ pathname: '/app/devices/309' }}
+        handleToggleExpand={() => {}}
+      />,
     )
-    expect(JSON.parse(JSON.stringify(wrapper.instance().breadcrumbs()))).toEqual([{"_owner": null, "_store": {}, "key": "/app/devices", "props": {"children": [{"_owner": null, "_store": {}, "key": null, "props": {"children": "/", "className": "header-breadcrumb-separator"}, "ref": null, "type": "span"}, {"_owner": null, "_store": {}, "key": null, "props": {"children": {"_owner": null, "_store": {},"key": null, "props": {"children": "Devices", "replace": false, "to": "/app/devices"}, "ref": null}}, "ref":null, "type": "span"}]}, "ref": null}, {"_owner": null, "_store": {}, "key": "/app/devices/309", "props": {"children": [{"_owner": null, "_store": {}, "key": null, "props": {"children": "/", "className": "header-breadcrumb-separator"}, "ref": null, "type": "span"}, {"_owner": null, "_store": {}, "key": null, "props": {"children": {"_owner": null, "_store": {}, "key": null, "props": {"children": "309", "replace": false, "to": "/app/devices/309"}, "ref": null}}, "ref": null, "type": "span"}]}, "ref": null}])
+    expect(JSON.parse(JSON.stringify(wrapper.instance().breadcrumbs()))).toEqual([{
+      _owner: null,
+      _store: {},
+      key: '/app/devices',
+      props: {
+        children: [{
+          _owner: null, _store: {}, key: null, props: { children: '/', className: 'header-breadcrumb-separator' }, ref: null, type: 'span',
+        }, {
+          _owner: null,
+          _store: {},
+          key: null,
+          props: {
+            children: {
+              _owner: null, _store: {}, key: null, props: { children: 'Devices', replace: false, to: '/app/devices' }, ref: null,
+            },
+          },
+          ref: null,
+          type: 'span',
+        }],
+      },
+      ref: null,
+    }, {
+      _owner: null,
+      _store: {},
+      key: '/app/devices/309',
+      props: {
+        children: [{
+          _owner: null, _store: {}, key: null, props: { children: '/', className: 'header-breadcrumb-separator' }, ref: null, type: 'span',
+        }, {
+          _owner: null,
+          _store: {},
+          key: null,
+          props: {
+            children: {
+              _owner: null, _store: {}, key: null, props: { children: '309', replace: false, to: '/app/devices/309' }, ref: null,
+            },
+          },
+          ref: null,
+          type: 'span',
+        }],
+      },
+      ref: null,
+    }])
   })
 })
