6c099452a053e5df32a37ac85dd99a034cd832e3
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-06 11:05:22 -0400

    style(error-validation): preparing for the integration with the linter
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/ErrorValidation/__tests__/ErrorValidation.snapshot.js
+++ src/components/ErrorValidation/__tests__/ErrorValidation.snapshot.js
@@ -29,14 +29,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import ErrorValidation from "../index"
+import ErrorValidation from '../index'
 
 describe('ErrorValidation', () => {
   test('renders ErrorValidation', () => {
     const component = renderer.create(
-      <ErrorValidation errors={['error 1', 'error 2']}/>
+      <ErrorValidation errors={['error 1', 'error 2']} />,
     )
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
