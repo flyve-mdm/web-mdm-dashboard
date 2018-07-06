46be1ae189cf085f20ecc8468f0271435e8d3eb7
Hector Rondon <hrondon@teclib.com>
2018-07-06 15:15:20 -0400

    refactor(auth): use context to handle authentication layout
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- src/containers/SignIn/components/UsernameFieldset.js
+++ src/containers/SignIn/components/UsernameFieldset.js
@@ -34,9 +34,7 @@ import PropTypes from 'prop-types'
 import {
   Link,
 } from 'react-router-dom'
-import {
-  I18n,
-} from 'react-i18nify'
+import I18n from '../../../shared/i18n'
 import publicURL from '../../../shared/publicURL'
 import appConfig from '../../../../public/config.json'
 import withGLPI from '../../../hoc/withGLPI'
