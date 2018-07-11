8420e8da624b7879e9f5f45983dee035e1c6dbb3
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-11 09:47:45 -0400

    build(dashboard): disable htaccess constructor
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- gitlabRunner.sh
+++ gitlabRunner.sh
@@ -25,9 +25,9 @@ source ci/scripts/sitemap_generator.sh
 rm -r ${webroot}
 cp -r ./build ${webroot}
 
-# set the production htaccess for public web site
-cp -f ./htaccess_prod ${webroot}/.htaccess
+# # set the production htaccess for public web site
+# cp -f ./htaccess_prod ${webroot}/.htaccess
 
-# change web server options for development environment
-sed -i -- 's/DASHBOARD_WEBROOT \//DASHBOARD_WEBROOT \/dashboard\//g' ${webroot}/.htaccess
-cat ./htaccess_dev >> ${webroot}/.htaccess
+# # change web server options for development environment
+# sed -i -- 's/DASHBOARD_WEBROOT \//DASHBOARD_WEBROOT \/dashboard\//g' ${webroot}/.htaccess
+# cat ./htaccess_dev >> ${webroot}/.htaccess
