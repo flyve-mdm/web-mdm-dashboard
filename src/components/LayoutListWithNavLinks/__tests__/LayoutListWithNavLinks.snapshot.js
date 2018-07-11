7ec0469da513b10e9f681a2b60566368156a7fbe
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-11 18:03:54 -0400

    test(dashboard): mock history to tests
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/components/LayoutListWithNavLinks/__tests__/LayoutListWithNavLinks.snapshot.js
+++ src/components/LayoutListWithNavLinks/__tests__/LayoutListWithNavLinks.snapshot.js
@@ -52,7 +52,7 @@ describe('LayoutListWithNavLinks', () => {
     const component = renderer.create(
       <Router history={history}>
         <LayoutListWithNavLinks
-          rootPath="test/"
+          rootPath=""
           history={history}
           routes={[
             {
