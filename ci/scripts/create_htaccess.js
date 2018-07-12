9bd4948deaa308386090a4e5fd65985ab4d84292
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-07-12 16:35:40 -0400

    refactor(dashboard): create htaccess to development
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/create_htaccess.js
+++ ci/scripts/create_htaccess.js
@@ -16,9 +16,18 @@ function getWebRoot (argv) {
   return '/'
 }
 
+function getDev (argv) {
+  if(argv.filter(element => element === '--dev').length > 0) {
+    return true
+  }
+  return false
+}
+
 const DASHBOARD_WEBROOT = getWebRoot(process.argv.slice(2))
 
-fs.writeFile('build/.htaccess', `# .htaccess for production server
+fs.writeFile('build/.htaccess',
+
+`# .htaccess for production server
 
 # url relative to web root dir for example /dashboard/
 # if you url is http://www.example.com/dashboard/
@@ -43,5 +52,40 @@ ErrorDocument 500 ${DASHBOARD_WEBROOT}error?code=500
 
 `, function (err) {
   if (err) throw err
-  console.log('Saved!')
+  if (getDev(process.argv.slice(2))){
+    fs.appendFile('build/.htaccess',
+
+`# DISABLE ALL CACHING WHILE DEVELOPING
+<FilesMatch "\.(html|htm|js|css|json)$">
+  FileETag None
+
+  <IfModule mod_headers.c>
+    Header unset ETag
+    Header set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"
+    Header set Pragma "no-cache"
+    Header set Note "CACHING IS DISABLED ON LOCALHOST OR DEVELOP"
+    Header set Expires "Wed, 11 Jan 1984 05:00:00 GMT"
+  </IfModule>
+</FilesMatch>
+
+# EXPIRES CACHING
+<IfModule mod_expires.c>
+  ExpiresActive On
+  ExpiresByType image/jpg "access 1 year"
+  ExpiresByType image/jpeg "access 1 year"
+  ExpiresByType image/gif "access 1 year"
+  ExpiresByType image/png "access 1 year"
+  ExpiresByType text/css "access 1 month"
+  ExpiresByType text/html "access 1 month"
+  ExpiresByType application/pdf "access 1 month"
+  ExpiresByType text/x-javascript "access 1 month"
+  ExpiresByType application/x-shockwave-flash "access 1 month"
+  ExpiresByType image/x-icon "access 1 year"
+  ExpiresDefault "access 1 month"
+</IfModule>`
+
+    , function (err) {
+      if (err) throw err
+    })
+  }
 })
