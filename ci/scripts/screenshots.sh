5fe163d13b4c11cb67a537130018dc12142b44d2
Hector Rondon <hrondon@teclib.com>
2018-07-23 10:12:26 -0400

    ci(deploy): generate screenshots
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ ci/scripts/screenshots.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Generate CHANGELOG.md to increment version
+yarn release
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+# Copy screenshots to gh-pages branch
+yarn gh-pages-screenshots -m "ci(docs): take dashboard screenshots for version ${GIT_TAG}"
