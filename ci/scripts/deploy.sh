a50f89f3bd3a4a4a4b786551c7403c99ba5fe7e3
Hector Rondon <hrondon@teclib.com>
2018-07-23 17:25:38 -0400

    ci(release): update deploy branchs from release
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $CIRCLE_BRANCH = "develop" ]]; then
-    source ci/scripts/deploy_develop.sh
-elif [[ $CIRCLE_BRANCH = "master" ]]; then
-    source ci/scripts/deploy_master.sh
-fi
