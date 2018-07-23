6d8aea492ac15dd12f47a0bd2d3e3f17e43c7516
Hector Rondon <hrondon@teclib.com>
2018-07-23 10:16:07 -0400

    ci(deploy): generate documentation
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ ci/scripts/documentation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Generate CHANGELOG.md and increment version
+yarn release
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+# Generate documentation with jsdoc
+yarn docs
+# Add docs folder
+git add docs -f
+# Create commit, NOTICE: this commit is not sent
+git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
+# Copy documentation to gh-pages branch
+yarn gh-pages-docs -m "ci(docs): generate documentation with jsdoc for version ${GIT_TAG}"
