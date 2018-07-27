b65f94eebc4d7c22c715823beb884d36808f9374
Luis F. Gonzalez <lgonzalez@teclib.com>
2018-07-27 17:47:24 -0400

    build(docker): fix incorrect format
    add dos2unix dependency
    add fix to command eslint
    
    Signed-off-by: Luis F. Gonzalez <lgonzalez@teclib.com>

--- docker/docker/node/cliinstall.sh
+++ docker/docker/node/cliinstall.sh
@@ -2,8 +2,10 @@
 #
 # Instalation / Configuration Dashboad WEB
 #
-if [ ! -d $APPPATH/node_modules ] && [ ! "$(ls -A $APPPATH/node_modules)" ]; then
+if [ ! -d $APPPATH/node_modules ] || [ ! "$(ls -A $APPPATH/node_modules)" ]; then
     yarn install
+fi
+if [ ! -d $APPPATH/build ] || [ ! "$(ls -A $APPPATH/build)" ]; then
     yarn build
 fi
 yarn start
