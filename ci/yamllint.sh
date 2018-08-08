fdfcdf7a8b3c3194fcf03f43434cc8ac9daf5782
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-08-08 11:36:03 -0400

    docs(linter): update the list of the yamllint
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/yamllint.sh
+++ ci/yamllint.sh
@@ -1,4 +1,5 @@
 ./node_modules/.bin/yamllint _config.yml
-./node_modules/.bin/yamllint .eslintrc.yml
 ./node_modules/.bin/yamllint .sass-lint.yml
 ./node_modules/.bin/yamllint .circleci/config.yml
+./node_modules/.bin/yamllint _data/config.yml
+./node_modules/.bin/yamllint _data/whitelist_GP.yml
