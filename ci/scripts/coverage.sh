bf58c9befebe564fcbee66bdc9199cf6bb010f1f
Hector Rondon <hrondon@teclib.com>
2018-09-05 17:28:41 -0400

    ci(documentation): update version on develop branch only
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/coverage.sh
+++ ci/scripts/coverage.sh
@@ -3,20 +3,28 @@
 export LC_ALL=C.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
-# Generate CHANGELOG.md and increment version
-IS_PRERELEASE="$( cut -d '-' -f 2 <<< "$CIRCLE_BRANCH" )";
 
-if [[ $CIRCLE_BRANCH != "$IS_PRERELEASE" ]]; then
+if [[ $CIRCLE_BRANCH != "develop" ]]; then
 
-  PREFIX_PRERELEASE="$( cut -d '.' -f 1 <<< "$IS_PRERELEASE" )";
-  yarn release -m "ci(release): generate CHANGELOG.md for version %s" --prerelease "$PREFIX_PRERELEASE"
+  # Get old version number from package.json
+  export GIT_OLD_TAG=$(jq -r ".version" package.json)
+  # Generate CHANGELOG.md and increment version
+  IS_PRERELEASE="$( cut -d '-' -f 2 <<< "$GIT_OLD_TAG" )";
 
-else
+  if [[ $GIT_OLD_TAG != "$IS_PRERELEASE" ]]; then
 
-  yarn release -m "ci(release): generate CHANGELOG.md for version %s"
+    PREFIX_PRERELEASE="$( cut -d '.' -f 1 <<< "$IS_PRERELEASE" )";
+    yarn release -m "ci(release): generate CHANGELOG.md for version %s" --prerelease "$PREFIX_PRERELEASE"
+
+  else
+
+    yarn release -m "ci(release): generate CHANGELOG.md for version %s"
+
+  fi
 
 fi
-# Get version number from package.json
+
+# Get new version number from package.json
 export GIT_TAG=$(jq -r ".version" package.json)
 # Genarate coverage
 yarn coverage
