4a150ba236ee729f9862a997d0e2011297ed56ed
Hector Rondon <hrondon@teclib.com>
2018-09-06 16:28:49 -0400

    ci(docs): increment version on develop branch only
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/documentation.sh
+++ ci/scripts/documentation.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-if [[ $CIRCLE_BRANCH != "develop" ]]; then
+if [[ $CIRCLE_BRANCH == "develop" ]]; then
 
   # Get old version number from package.json
   export GIT_OLD_TAG=$(jq -r ".version" package.json)
