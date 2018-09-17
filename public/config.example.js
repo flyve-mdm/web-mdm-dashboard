1edeb5f4de04ecc573f323fc52cc0ad9e151c4f7
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-09-17 12:15:01 -0400

    fix(search-engine): use correct values for the combiner
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- public/config.example.js
+++ public/config.example.js
@@ -1,7 +1,7 @@
 window.appConfig = {
-  glpiApiLink: "your_URL",
-  demoToken: "your_token",
-  pluginToken: "your_token",
-  appName: "MDM Dashboard",
-  bugsnag: "your_bugsnag_key",
+  glpiApiLink: 'your_URL',
+  demoToken: 'your_token',
+  pluginToken: 'your_token',
+  appName: 'MDM Dashboard',
+  bugsnag: 'your_bugsnag_key',
 }
