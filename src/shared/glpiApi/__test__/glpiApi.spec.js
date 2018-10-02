0c55c2a4319213870f776c7a2e4b5554e0071a3e
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-10-02 18:16:58 -0400

    test(dashboard): fix integration with new react scripts
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/shared/glpiApi/__test__/glpiApi.spec.js
+++ src/shared/glpiApi/__test__/glpiApi.spec.js
@@ -28,9 +28,29 @@
 
 import glpiApi from '../index'
 
+
+
 /* eslint no-underscore-dangle: ["error", { "allow": ["_parser", "_makeRequest"] }] */
 describe('glpiApi', () => {
+  beforeAll(() => {
+    global.appConfig = {
+      glpiApiLink: 'https://demo-api.flyve.org/apirest.php',
+      demoToken: 'pe90W27ebPXIeYXsj2mYE0nMrRz9q1AlguKhbhcI',
+      pluginToken: '2JoAOqcPcqkMZDf2QeeQBAVjERO2LamlZghkvWPB',
+      appName: 'MDM Dashboard',
+      bugsnag: 'fba7440f31b8f7a7e72495cf000b2c42',
+    }
+  })
+
   it('should exist all methods of "javascript-library-glpi"', () => {
+    global.appConfig = {
+      glpiApiLink: 'https://demo-api.flyve.org/apirest.php',
+      demoToken: 'pe90W27ebPXIeYXsj2mYE0nMrRz9q1AlguKhbhcI',
+      pluginToken: '2JoAOqcPcqkMZDf2QeeQBAVjERO2LamlZghkvWPB',
+      appName: 'MDM Dashboard',
+      bugsnag: 'fba7440f31b8f7a7e72495cf000b2c42',
+    }
+
     expect(glpiApi).toBeDefined()
     expect(glpiApi._parser).toBeDefined()
     expect(glpiApi._makeRequest).toBeDefined()
