e9e2615815dd56d660a5cd7cec94c68830baa4ff
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-10-05 17:51:54 -0400

    docs(contact): format and send email
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* eslint-env browser */
+
 $(() => {
   // On mouse click and touch, add a class to <button> and <a> that removes focus rectangle
   $(document).on('mousedown touchstart', 'a, button', () => {
@@ -211,3 +213,45 @@ $(() => {
   // Enable jQuery Lazy plugin
   $('.lazy').lazy()
 })
+
+const sendContactEmail = () => {
+  const contactData = {}
+
+  $('form').serializeArray().forEach((item) => {
+    if (contactData[item.name]) {
+      if (typeof (contactData[item.name]) === 'string') {
+        contactData[item.name] = [contactData[item.name]]
+      }
+
+      contactData[item.name].push(item.value)
+    } else {
+      contactData[item.name] = item.value
+    }
+  })
+
+  const link = `mailto:sales@teclib.com?subject=${escape(contactData.contactSubject)}`
+  const body = escape(`
+  URL: ${window.location.href}
+
+  Name: ${contactData.contactName}
+
+  Email: ${contactData.contactEmail}
+
+  Company: ${contactData.contactCompany}
+
+  Job position: ${contactData.contactJobPosition}
+
+  Motive:
+    - Pricing: ${contactData.contactMotivePricing ? 'Yes' : 'No'}
+    - Partnership: ${contactData.contactMotivePartnership ? 'Yes' : 'No'}
+    - Support: ${contactData.contactMotiveSupport ? 'Yes' : 'No'}
+    - Other: ${contactData.contactMotiveOther}
+
+  Subject: ${contactData.contactSubject}
+
+  Message:
+    ${contactData.contactMessage}
+  `)
+
+  window.location.href = `${link}&body=${body}`
+}
