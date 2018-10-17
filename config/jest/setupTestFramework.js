dd8a0179ccc13085bb7496a152181afebb8645ec
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-10-17 15:30:28 -0400

    refactor(dashboard): remove self-registration
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- config/jest/setupTestFramework.js
+++ config/jest/setupTestFramework.js
@@ -15,8 +15,6 @@ window.sessionStorage = storage
 
 window.appConfig = {
   glpiApiLink: 'your_URL',
-  demoToken: 'your_token',
-  pluginToken: 'your_token',
   appName: 'MDM Dashboard',
   bugsnag: 'your_bugsnag_key',
 }
