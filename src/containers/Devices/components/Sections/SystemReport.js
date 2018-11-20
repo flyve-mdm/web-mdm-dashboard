105ab2b889fe29a2454fdf9c742c8d55f78806a0
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2018-11-20 11:47:29 -0400

    refactor(system-report): simplify and unify the code
    Signed-off-by: Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>

--- src/containers/Devices/components/Sections/SystemReport.js
+++ src/containers/Devices/components/Sections/SystemReport.js
@@ -33,7 +33,6 @@ import React, {
 import PropTypes from 'prop-types'
 import I18n from 'shared/i18n'
 import itemtype from 'shared/itemtype'
-import Loader from 'components/Loader'
 import Loading from 'components/Loading'
 import Inventory from './Inventory'
 
@@ -48,14 +47,10 @@ export default class SystemReport extends PureComponent {
     this.state = {
       id: this.props.id,
       update: this.props.update,
-      data: undefined,
-      isLoading: true,
       requestingInventory: false,
+      fleetID: undefined,
+      computersID: undefined,
     }
-  }
-
-  componentDidMount() {
-    this.handleRefresh()
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -70,8 +65,6 @@ export default class SystemReport extends PureComponent {
         ...prevState,
         id: nextProps.id,
         update: nextProps.update,
-        data: undefined,
-        isLoading: true,
       }
     }
     return {
@@ -80,33 +73,16 @@ export default class SystemReport extends PureComponent {
   }
 
   /**
-   * handle refresh system report
+   * handle refresh of the inventory
    * @function handleRefresh
    */
   handleRefresh = () => {
-    if (this.state.update) {
-      this.setState({
-        isLoading: true,
-        requestingInventory: false,
-      }, async () => {
-        try {
-          const { id } = this.state
-          const data = await this.props.glpi.getAnItem({
-            itemtype: itemtype.PluginFlyvemdmAgent,
-            id,
-          })
-          this.setState({
-            isLoading: false,
-            data,
-          })
-        } catch (error) {
-          this.props.toast.setNotification(this.props.handleMessage({
-            type: 'alert',
-            message: error,
-          }))
-        }
-      })
-    }
+    this.setState({
+      fleetID: undefined,
+      computersID: undefined,
+      requestingInventory: false,
+    })
+    this.inventory.handleRefresh()
   }
 
   /**
@@ -149,124 +125,87 @@ export default class SystemReport extends PureComponent {
   }
 
   render() {
-    if (this.state.isLoading && !this.state.data) {
-      return (
-        <div style={{ padding: '20px' }}>
-          <Loader type="content" />
-        </div>
-      )
-    } if (!this.state.isLoading && this.state.data) {
-      return (
-        <div className="devices">
-          <div className="system-report">
-            <div className="request-inventory">
-              <button
-                className="btn btn--secondary"
-                onClick={this.requestInventory}
-                type="button"
-              >
-                {I18n.t('devices.system_report.request_inventory')}
-              </button>
-              {this.state.requestingInventory ? <Loading small style={{ paddingTop: '6px' }} /> : ''}
-            </div>
-            <div className="title">
-              {I18n.t('commons.agent')}
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.id')}
-              </div>
-              <div className="list-col">
-                {this.state.data.id}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.name')}
-              </div>
-              <div className="list-col">
-                {this.state.data.name}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.version')}
-              </div>
-              <div className="list-col">
-                {this.state.data.version}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.mdm_type')}
-              </div>
-              <div className="list-col">
-                {this.state.data.mdm_type}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.enroll_status')}
-              </div>
-              <div className="list-col">
-                {this.state.data.enroll_status}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.last_contact')}
-              </div>
-              <div className="list-col">
-                {this.state.data.last_contact}
-              </div>
-            </div>
-            <div className="list-content">
-              <div className="list-col">
-                {I18n.t('commons.last_report')}
-              </div>
-              <div className="list-col">
-                {this.state.data.last_report ? this.state.data.last_report : 'N/A'}
-              </div>
-            </div>
-
-            <Inventory
-              title={I18n.t('commons.fleet')}
-              itemType="PluginFlyvemdmFleet"
-              itemID={this.state.data.plugin_flyvemdm_fleets_id}
-              fields={{ id: 'ID', name: 'Name' }}
-              glpi={this.props.glpi}
-            />
-
-            <Inventory
-              title={I18n.t('commons.device')}
-              itemType="Computer"
-              itemID={this.state.data.computers_id}
-              fields={{
-                id: 'ID',
-                name: 'Name',
-                uuid: 'UUID',
-                date_creation: 'Creation',
-                date_mod: 'Modification',
-                computermodels_id: 'Model',
-                computertypes_id: 'Type',
-                manufacturers_id: 'Manufacturer',
-                serial: 'Serial',
-              }}
-              parameters={{
-                expand_dropdowns: true,
-                with_devices: true,
-                with_disks: true,
-                with_softwares: true,
-                with_connections: true,
-                with_networkports: true,
-              }}
-              glpi={this.props.glpi}
-            />
+    return (
+      <div className="devices">
+        <div className="system-report">
+          <div className="request-inventory">
+            <button
+              className="btn btn--secondary"
+              onClick={this.requestInventory}
+              type="button"
+            >
+              {I18n.t('devices.system_report.request_inventory')}
+            </button>
+            {this.state.requestingInventory ? <Loading small style={{ paddingTop: '6px' }} /> : ''}
           </div>
+
+          <Inventory
+            title={I18n.t('commons.agent')}
+            itemType="PluginFlyvemdmAgent"
+            itemID={this.state.id}
+            fields={{
+              id: 'id',
+              name: 'name',
+              version: 'version',
+              mdm_type: 'mdm_type',
+              enroll_status: 'enroll_status',
+              last_contact: 'last_contact',
+              last_report: 'last_report',
+            }}
+            glpi={this.props.glpi}
+            afterLoading={(fleetID, computersID) => {
+              this.setState({
+                fleetID,
+                computersID,
+              })
+            }}
+            ref={(inventory) => { this.inventory = inventory }}
+          />
+
+          {
+            this.state.fleetID && (
+              <Inventory
+                title={I18n.t('commons.fleet')}
+                itemType="PluginFlyvemdmFleet"
+                itemID={this.state.fleetID}
+                fields={{ id: 'id', name: 'name' }}
+                glpi={this.props.glpi}
+              />
+            )
+          }
+
+          {
+            this.state.computersID && (
+              <Inventory
+                title={I18n.t('commons.device')}
+                itemType="Computer"
+                itemID={this.state.computersID}
+                fields={{
+                  id: 'id',
+                  name: 'name',
+                  uuid: 'uuid',
+                  date_creation: 'creation',
+                  date_mod: 'modification',
+                  computermodels_id: 'model',
+                  computertypes_id: 'type',
+                  manufacturers_id: 'manufacturer',
+                  serial: 'serial',
+                }}
+                parameters={{
+                  expand_dropdowns: true,
+                  with_devices: true,
+                  with_disks: true,
+                  with_softwares: true,
+                  with_connections: true,
+                  with_networkports: true,
+                }}
+                glpi={this.props.glpi}
+              />
+            )
+          }
         </div>
-      )
-    }
-    return ''
+      </div>
+    )
   }
 }
 /** SystemReport propTypes */
