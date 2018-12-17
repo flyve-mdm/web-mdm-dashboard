4d7aa43ce8ff2206852aaa25c22b68e13e1312aa
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2018-12-17 13:28:51 -0400

    ci(config): add release information
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com

--- ci/scripts/create_config_file.sh
+++ ci/scripts/create_config_file.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+
+# Get the current date
+export DATE=`date '+%Y-%m-%d %H:%M:%S'`
+
 cat <<EOF >public/config.js
 window.appConfig = {
-  glpiApiLink: "$API_LINK",
-  appName: "MDM Dashboard",
-  bugsnag: "$BUGSNAG_KEY"
+  glpiApiLink: '$API_LINK',
+  appName: 'MDM Dashboard',
+  bugsnag: '$BUGSNAG_KEY',
+  release: {
+    lastCommit: '$CIRCLE_SHA1',
+    currentVersion: '$GIT_TAG',
+    buildNumber: '$CIRCLE_BUILD_NUM',
+    deployDate: '$DATE',
+    buildUrl: '$CIRCLE_BUILD_URL',
+  },
 }
 EOF
