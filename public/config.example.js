97a3e480a4860ac6acfeab3dd54351b03aa5d562
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2018-12-17 13:28:18 -0400

    build(config): add release information
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- public/config.example.js
+++ public/config.example.js
@@ -2,4 +2,10 @@ window.appConfig = {
   glpiApiLink: 'your_URL',
   appName: 'MDM Dashboard',
   bugsnag: 'your_bugsnag_key',
+  release: {
+    lastCommit: 'your_last_commit',
+    currentVersion: 'current_version_of_the_application',
+    buildNumber: 'the_last_build_number',
+    buildUrl: 'build_url',
+  },
 }
