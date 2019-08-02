e2e53409db85035f61290984737bd97aac357f76
Gianfranco Gesu Manganiello Tiso <gianfrancomanganiello@hotmail.com>
2019-08-02 11:10:47 +0200

    ci(install): fix jessie url
    Signed-off-by: Gianfranco Gesu Manganiello Tiso <gianfrancomanganiello@hotmail.com>

--- ci/scripts/system_install.sh
+++ ci/scripts/system_install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # System dependencies
-echo "deb http://ftp.us.debian.org/debian/ jessie main contrib non-free" > /etc/apt/sources.list
-echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://ftp.us.debian.org/debian/ jessie-updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://ftp.us.debian.org/debian/ jessie-backports main contrib non-free" >> /etc/apt/sources.list
-apt-get -y update
-apt-get -y install -t jessie-backports  openjdk-8-jre-headless ca-certificates-java
-apt-get -y install jq python-pip zip unzip wget xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 ruby-full locales
+apt-get clean
+rm /etc/apt/sources.list
+echo 'deb http://archive.debian.org/debian/ wheezy main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://archive.debian.org/debian/ jessie-backports main' >> /etc/apt/sources.list.d/openjdk.list
+echo 'deb http://archive.debian.org/debian/ jessie main' >> /etc/apt/sources.list.d/openjdk.list
+apt-get update -o Acquire::Check-Valid-Until=false
+apt-get install -y -t jessie-backports openjdk-8-jre-headless
+rm /etc/apt/sources.list.d/openjdk.list
+apt-get update
+
+echo '++++++++++++++++++++++++++++++++++++++++'
+
+apt-get -y install python-pip zip unzip wget xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 ruby-full locales jq
 
 # Configure encoding for ruby scripts
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
